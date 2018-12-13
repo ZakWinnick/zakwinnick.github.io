bbb7a2447602717d9462df5d21e4636806808434
Zak Winnick <zakwinnick@protonmail.com>
2018-12-13 16:10:50 -0600

    Added 2 tweets

--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2018_12.js",
 		"year": 2018,
 		"var_name": "tweets_2018_12",
-		"tweet_count": 345,
+		"tweet_count": 347,
 		"month": 12
 	},
 	{
