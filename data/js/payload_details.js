62a9434d39dd3e9225cf0aa584cd62048aad9157
Zak Winnick <zakwinnick@protonmail.com>
2018-12-25 22:10:53 -0600

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 9731,
-	"created_at": "Wed Dec 26 03:18:49 +0000 2018",
+	"tweets": 9734,
+	"created_at": "Wed Dec 26 04:10:52 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2018_12.js",
 		"year": 2018,
 		"var_name": "tweets_2018_12",
-		"tweet_count": 631,
+		"tweet_count": 634,
 		"month": 12
 	},
 	{
