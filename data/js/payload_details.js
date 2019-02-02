4e58edb88c0684d3358e06e2c0f1665bd2800741
Zak Winnick <zakwinnick@protonmail.com>
2019-02-01 22:18:45 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10162,
-	"created_at": "Thu Jan 31 15:10:48 +0000 2019",
+	"tweets": 10163,
+	"created_at": "Sat Feb 02 04:18:44 +0000 2019",
 	"lang": "en"
 }
---
+++ data/js/tweets/2019_02.js
@@ -0,0 +1,361 @@
+Grailbird.data.tweets_2019_02 = 
+[
+	{
+		"created_at": "Sat Feb 02 04:12:01 +0000 2019",
+		"id": 1091549776896839700,
+		"id_str": "1091549776896839681",
+		"full_text": "You never go Full Mingo https://t.co/M9P34mznfN",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			23
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/M9P34mznfN",
+					"expanded_url": "https://twitter.com/VengefulWalnut/status/1091541374862684160",
+					"display_url": "twitter.com/VengefulWalnut…",
+					"indices": [
+						24,
+						47
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 9586552,
+			"id_str": "9586552",
+			"name": "Zak Winnick",
+			"screen_name": "ZakWinnick",
+			"location": "Nashville, TN",
+			"description": "Hotel Technology Guru | Electric Vehicle Enthusiast | Founder @SurfChaosMedia",
+			"url": "https://t.co/mVEZMWANl3",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/mVEZMWANl3",
+							"expanded_url": "https://www.zakwinnick.com",
+							"display_url": "zakwinnick.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 57,
+			"friends_count": 188,
+			"listed_count": 18,
+			"created_at": "Sun Oct 21 22:28:31 +0000 2007",
+			"favourites_count": 119,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9108,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "3F5B66",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1091153949976084480/5dMFj49f_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1091153949976084480/5dMFj49f_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/9586552/1548453841",
+			"profile_link_color": "483D8B",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "858080",
+			"profile_text_color": "086660",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1091541374862684200,
+		"quoted_status_id_str": "1091541374862684160",
+		"quoted_status_permalink": {
+			"url": "https://t.co/M9P34mznfN",
+			"expanded": "https://twitter.com/VengefulWalnut/status/1091541374862684160",
+			"display": "twitter.com/VengefulWalnut…"
+		},
+		"quoted_status": {
+			"created_at": "Sat Feb 02 03:38:38 +0000 2019",
+			"id": 1091541374862684200,
+			"id_str": "1091541374862684160",
+			"full_text": "A better love story than Twilight. https://t.co/5GxBnAOoyC",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				34
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1091541371175854100,
+						"id_str": "1091541371175854081",
+						"indices": [
+							35,
+							58
+						],
+						"media_url": "http://pbs.twimg.com/media/DyXvGAaUYAEofM5.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DyXvGAaUYAEofM5.jpg",
+						"url": "https://t.co/5GxBnAOoyC",
+						"display_url": "pic.twitter.com/5GxBnAOoyC",
+						"expanded_url": "https://twitter.com/VengefulWalnut/status/1091541374862684160/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 581,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1080,
+								"h": 922,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1080,
+								"h": 922,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1091541371175854100,
+						"id_str": "1091541371175854081",
+						"indices": [
+							35,
+							58
+						],
+						"media_url": "http://pbs.twimg.com/media/DyXvGAaUYAEofM5.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DyXvGAaUYAEofM5.jpg",
+						"url": "https://t.co/5GxBnAOoyC",
+						"display_url": "pic.twitter.com/5GxBnAOoyC",
+						"expanded_url": "https://twitter.com/VengefulWalnut/status/1091541374862684160/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 581,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1080,
+								"h": 922,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1080,
+								"h": 922,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 1091541371171758100,
+						"id_str": "1091541371171758080",
+						"indices": [
+							35,
+							58
+						],
+						"media_url": "http://pbs.twimg.com/media/DyXvGAZV4AABblN.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DyXvGAZV4AABblN.jpg",
+						"url": "https://t.co/5GxBnAOoyC",
+						"display_url": "pic.twitter.com/5GxBnAOoyC",
+						"expanded_url": "https://twitter.com/VengefulWalnut/status/1091541374862684160/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 1080,
+								"h": 918,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1080,
+								"h": 918,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 578,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 1091541371167502300,
+						"id_str": "1091541371167502338",
+						"indices": [
+							35,
+							58
+						],
+						"media_url": "http://pbs.twimg.com/media/DyXvGAYU8AIVL5z.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DyXvGAYU8AIVL5z.jpg",
+						"url": "https://t.co/5GxBnAOoyC",
+						"display_url": "pic.twitter.com/5GxBnAOoyC",
+						"expanded_url": "https://twitter.com/VengefulWalnut/status/1091541374862684160/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 524,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1080,
+								"h": 832,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1080,
+								"h": 832,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 3009376464,
+				"id_str": "3009376464",
+				"name": "James Cervantes",
+				"screen_name": "VengefulWalnut",
+				"location": "Hollywood, Los Angeles",
+				"description": "not terrible writer person. has approximate knowledge of many things. possibly a velociraptor. pygmy ferret herder.",
+				"url": "https://t.co/9u3SfAphGT",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/9u3SfAphGT",
+								"expanded_url": "http://www.jamescervantes.com",
+								"display_url": "jamescervantes.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 277,
+				"friends_count": 611,
+				"listed_count": 7,
+				"created_at": "Tue Feb 03 16:03:31 +0000 2015",
+				"favourites_count": 16415,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 20297,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/846404685829365760/X6f9jR83_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/846404685829365760/X6f9jR83_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/3009376464/1483719901",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 2,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "You never go Full Mingo https://t.co/M9P34mznfN"
+	}
+]
