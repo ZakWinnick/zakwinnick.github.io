30b621571cd92c3e9b13eddb3c3020a2e4aef281
Zak Winnick <zakwinnick@protonmail.com>
2019-03-02 19:10:47 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10180,
-	"created_at": "Thu Feb 28 21:10:46 +0000 2019",
+	"tweets": 10181,
+	"created_at": "Sun Mar 03 01:10:46 +0000 2019",
 	"lang": "en"
 }
---
+++ data/js/tweets/2019_03.js
@@ -0,0 +1,309 @@
+Grailbird.data.tweets_2019_03 = 
+[
+	{
+		"created_at": "Sun Mar 03 01:03:37 +0000 2019",
+		"id": 1102011614306492400,
+		"id_str": "1102011614306492417",
+		"full_text": "Well, patience comes to those who wait… https://t.co/4xdkjW5MA5",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			39
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/4xdkjW5MA5",
+					"expanded_url": "https://twitter.com/SpaceflightNow/status/1101738342851510272",
+					"display_url": "twitter.com/SpaceflightNow…",
+					"indices": [
+						40,
+						63
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 9586552,
+			"id_str": "9586552",
+			"name": "Zak Winnick",
+			"screen_name": "ZakWinnick",
+			"location": "Nashville, TN",
+			"description": "Hotel Technology Guru | Electric Vehicle Enthusiast | Founder @SurfChaosMedia | Podcaster @ https://t.co/OGCcNBr1wH",
+			"url": "https://t.co/mVEZMWANl3",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/mVEZMWANl3",
+							"expanded_url": "https://www.zakwinnick.com",
+							"display_url": "zakwinnick.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/OGCcNBr1wH",
+							"expanded_url": "https://techkaffe.com",
+							"display_url": "techkaffe.com",
+							"indices": [
+								92,
+								115
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 58,
+			"friends_count": 200,
+			"listed_count": 18,
+			"created_at": "Sun Oct 21 22:28:31 +0000 2007",
+			"favourites_count": 131,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9117,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "3F5B66",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1102011692022722566/gLpFBZjz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1102011692022722566/gLpFBZjz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/9586552/1550435595",
+			"profile_link_color": "483D8B",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "858080",
+			"profile_text_color": "086660",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1101738342851510300,
+		"quoted_status_id_str": "1101738342851510272",
+		"quoted_status_permalink": {
+			"url": "https://t.co/4xdkjW5MA5",
+			"expanded": "https://twitter.com/SpaceflightNow/status/1101738342851510272",
+			"display": "twitter.com/SpaceflightNow…"
+		},
+		"quoted_status": {
+			"created_at": "Sat Mar 02 06:57:44 +0000 2019",
+			"id": 1101738342851510300,
+			"id_str": "1101738342851510272",
+			"full_text": "The SpaceX launch director is verifying that all stations are “go” for propellant loading at this time. The Falcon 9 will soon be filled with kerosene and liquid oxygen propellants for liftoff at 2:49am EST (0749 GMT). Live coverage: https://t.co/DcjwNciGlg https://t.co/XJgt8p33dk",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				257
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/DcjwNciGlg",
+						"expanded_url": "https://spaceflightnow.com/2019/03/01/falcon-9-crew-dragon-demo-1-mission-status-center/",
+						"display_url": "spaceflightnow.com/2019/03/01/fal…",
+						"indices": [
+							234,
+							257
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1101738109086126100,
+						"id_str": "1101738109086126080",
+						"indices": [
+							258,
+							281
+						],
+						"media_url": "http://pbs.twimg.com/media/D0oo-ROW0AAW4f4.png",
+						"media_url_https": "https://pbs.twimg.com/media/D0oo-ROW0AAW4f4.png",
+						"url": "https://t.co/XJgt8p33dk",
+						"display_url": "pic.twitter.com/XJgt8p33dk",
+						"expanded_url": "https://twitter.com/SpaceflightNow/status/1101738342851510272/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 735,
+								"h": 718,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 735,
+								"h": 718,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 664,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1101738109086126100,
+						"id_str": "1101738109086126080",
+						"indices": [
+							258,
+							281
+						],
+						"media_url": "http://pbs.twimg.com/media/D0oo-ROW0AAW4f4.png",
+						"media_url_https": "https://pbs.twimg.com/media/D0oo-ROW0AAW4f4.png",
+						"url": "https://t.co/XJgt8p33dk",
+						"display_url": "pic.twitter.com/XJgt8p33dk",
+						"expanded_url": "https://twitter.com/SpaceflightNow/status/1101738342851510272/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 735,
+								"h": 718,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 735,
+								"h": 718,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 664,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 17217640,
+				"id_str": "17217640",
+				"name": "Spaceflight Now",
+				"screen_name": "SpaceflightNow",
+				"location": "Cape Canaveral, Florida",
+				"description": "The best source for space news",
+				"url": "http://t.co/bwQfSeZCPB",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/bwQfSeZCPB",
+								"expanded_url": "http://spaceflightnow.com",
+								"display_url": "spaceflightnow.com",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 405907,
+				"friends_count": 128,
+				"listed_count": 5477,
+				"created_at": "Thu Nov 06 19:44:31 +0000 2008",
+				"favourites_count": 57,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 21431,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/63871979/twittericon_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/63871979/twittericon_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/17217640/1430874494",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 49,
+			"favorite_count": 155,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Well, patience comes to those who wait… https://t.co/4xdkjW5MA5"
+	}
+]
