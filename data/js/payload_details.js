ba33df7c5bc53274f40380d0f95e533403be9641
Zak Winnick <zakwinnick@protonmail.com>
2019-03-06 17:10:47 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10181,
-	"created_at": "Sun Mar 03 01:10:46 +0000 2019",
+	"tweets": 10182,
+	"created_at": "Wed Mar 06 23:10:46 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,336 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Wed Mar 06 23:04:00 +0000 2019",
+		"id": 1103431061676142600,
+		"id_str": "1103431061676142598",
+		"full_text": "I’m not going to lie…I cried. \n\nBTW, do you think he used the 3 P’s? https://t.co/zflHhJyOkP",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			68
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/zflHhJyOkP",
+					"expanded_url": "https://twitter.com/Jeopardy/status/1103416223331569664",
+					"display_url": "twitter.com/Jeopardy/statu…",
+					"indices": [
+						69,
+						92
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 9586552,
+			"id_str": "9586552",
+			"name": "Zak Winnick",
+			"screen_name": "ZakWinnick",
+			"location": "Nashville, TN",
+			"description": "Hotel Technology Guru | Electric Vehicle Enthusiast | Founder @SurfChaosMedia | Podcaster @ https://t.co/OGCcNBr1wH",
+			"url": "https://t.co/mVEZMWANl3",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/mVEZMWANl3",
+							"expanded_url": "https://www.zakwinnick.com",
+							"display_url": "zakwinnick.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/OGCcNBr1wH",
+							"expanded_url": "https://techkaffe.com",
+							"display_url": "techkaffe.com",
+							"indices": [
+								92,
+								115
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 58,
+			"friends_count": 179,
+			"listed_count": 18,
+			"created_at": "Sun Oct 21 22:28:31 +0000 2007",
+			"favourites_count": 132,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9118,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "3F5B66",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1102663936837586946/d8TspB6c_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1102663936837586946/d8TspB6c_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/9586552/1551576813",
+			"profile_link_color": "483D8B",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "858080",
+			"profile_text_color": "086660",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1103416223331569700,
+		"quoted_status_id_str": "1103416223331569664",
+		"quoted_status_permalink": {
+			"url": "https://t.co/zflHhJyOkP",
+			"expanded": "https://twitter.com/Jeopardy/status/1103416223331569664",
+			"display": "twitter.com/Jeopardy/statu…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Mar 06 22:05:02 +0000 2019",
+			"id": 1103416223331569700,
+			"id_str": "1103416223331569664",
+			"full_text": "A Message from Alex Trebek: https://t.co/LbxcIyeTCF",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				27
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1103415357232934900,
+						"id_str": "1103415357232934912",
+						"indices": [
+							28,
+							51
+						],
+						"media_url": "http://pbs.twimg.com/media/D1AfFtzUwAIl5x4.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/D1AfFtzUwAIl5x4.jpg",
+						"url": "https://t.co/LbxcIyeTCF",
+						"display_url": "pic.twitter.com/LbxcIyeTCF",
+						"expanded_url": "https://twitter.com/Jeopardy/status/1103416223331569664/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1600,
+								"h": 900,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1103415357232934900,
+						"id_str": "1103415357232934912",
+						"indices": [
+							28,
+							51
+						],
+						"media_url": "http://pbs.twimg.com/media/D1AfFtzUwAIl5x4.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/D1AfFtzUwAIl5x4.jpg",
+						"url": "https://t.co/LbxcIyeTCF",
+						"display_url": "pic.twitter.com/LbxcIyeTCF",
+						"expanded_url": "https://twitter.com/Jeopardy/status/1103416223331569664/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1600,
+								"h": 900,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								16,
+								9
+							],
+							"duration_millis": 72973,
+							"variants": [
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/amplify_video/1103415357232934912/pl/cBcm3bFF2O9g9Cz8.m3u8?tag=9"
+								},
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/amplify_video/1103415357232934912/vid/640x360/qHjRvdMX18OpR8pJ.mp4?tag=9"
+								},
+								{
+									"bitrate": 288000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/amplify_video/1103415357232934912/vid/320x180/76eT7DAJJB9nTFac.mp4?tag=9"
+								},
+								{
+									"bitrate": 2176000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/amplify_video/1103415357232934912/vid/1280x720/ywnT6Y8MLR0r24Ri.mp4?tag=9"
+								}
+							]
+						},
+						"additional_media_info": {
+							"title": "",
+							"description": "",
+							"embeddable": true,
+							"monetizable": false
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://studio.twitter.com\" rel=\"nofollow\">Twitter Media Studio</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 52554306,
+				"id_str": "52554306",
+				"name": "Jeopardy!",
+				"screen_name": "Jeopardy",
+				"location": "Los Angeles, CA",
+				"description": "What does Jeopardy! mean to you? We'd love to hear your stories! 📺#JEffect",
+				"url": "https://t.co/vvb6GdTo3z",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/vvb6GdTo3z",
+								"expanded_url": "https://www.jeopardy.com/jeffect",
+								"display_url": "jeopardy.com/jeffect",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 158810,
+				"friends_count": 331,
+				"listed_count": 783,
+				"created_at": "Tue Jun 30 23:09:10 +0000 2009",
+				"favourites_count": 7298,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 8420,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1103366921041276928/5DIn8I4H_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1103366921041276928/5DIn8I4H_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/52554306/1551898103",
+				"profile_link_color": "4100A3",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "CEEDED",
+				"profile_text_color": "000000",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 30421,
+			"favorite_count": 87891,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I’m not going to lie…I cried. \n\nBTW, do you think he used the 3 P’s? https://t.co/zflHhJyOkP"
+	},
+	{
 		"created_at": "Sun Mar 03 01:03:37 +0000 2019",
 		"id": 1102011614306492400,
 		"id_str": "1102011614306492417",
