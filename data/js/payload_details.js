e1f3080f71cfb2b71cb470f42041c89b46b190ab
Zak Winnick <zakwinnick@protonmail.com>
2019-04-20 14:10:47 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10224,
-	"created_at": "Sat Apr 20 13:18:42 +0000 2019",
+	"tweets": 10225,
+	"created_at": "Sat Apr 20 19:10:46 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,10 +1,264 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Sat Apr 20 18:44:51 +0000 2019",
+		"id": 1119673297678020600,
+		"id_str": "1119673297678020608",
+		"full_text": "This is brilliant. https://t.co/VV9exZk6Xk",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			18
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/VV9exZk6Xk",
+					"expanded_url": "https://twitter.com/PDFAssociation/status/1119227925474291713",
+					"display_url": "twitter.com/PDFAssociation…",
+					"indices": [
+						19,
+						42
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 9586552,
+			"id_str": "9586552",
+			"name": "Zak Winnick",
+			"screen_name": "ZakWinnick",
+			"location": "Nashville, TN",
+			"description": "Hotel Technology Guru | Electric Vehicle Enthusiast | Founder @SurfChaosMedia",
+			"url": "https://t.co/h6N0tCU1zj",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/h6N0tCU1zj",
+							"expanded_url": "http://ZakWinnick.com",
+							"display_url": "ZakWinnick.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 58,
+			"friends_count": 175,
+			"listed_count": 18,
+			"created_at": "Sun Oct 21 22:28:31 +0000 2007",
+			"favourites_count": 139,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9133,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "3F5B66",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1117910108032442368/PuYwWPMX_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1117910108032442368/PuYwWPMX_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/9586552/1555366317",
+			"profile_link_color": "483D8B",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "858080",
+			"profile_text_color": "086660",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1119227925474291700,
+		"quoted_status_id_str": "1119227925474291713",
+		"quoted_status_permalink": {
+			"url": "https://t.co/VV9exZk6Xk",
+			"expanded": "https://twitter.com/PDFAssociation/status/1119227925474291713",
+			"display": "twitter.com/PDFAssociation…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Apr 19 13:15:06 +0000 2019",
+			"id": 1119227925474291700,
+			"id_str": "1119227925474291713",
+			"full_text": "A Technical and Cultural Analysis of the #MuellerReport #PDF from the @PDFAssociation https://t.co/YC1VQB1HUM",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				109
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "MuellerReport",
+						"indices": [
+							41,
+							55
+						]
+					},
+					{
+						"text": "PDF",
+						"indices": [
+							56,
+							60
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "PDFAssociation",
+						"name": "PDF Association",
+						"id": 375301497,
+						"id_str": "375301497",
+						"indices": [
+							70,
+							85
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/YC1VQB1HUM",
+						"expanded_url": "https://www.pdfa.org/a-technical-and-cultural-assessment-of-the-mueller-report-pdf/",
+						"display_url": "pdfa.org/a-technical-an…",
+						"indices": [
+							86,
+							109
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 375301497,
+				"id_str": "375301497",
+				"name": "PDF Association",
+				"screen_name": "PDFAssociation",
+				"location": "",
+				"description": "Founded in 2006 the PDF Association promotes the adoption and implementation of International Standards for PDF technology. Imprint: https://t.co/E4O2yMPGQ5",
+				"url": "https://t.co/cflQUgJY1q",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/cflQUgJY1q",
+								"expanded_url": "http://www.pdfa.org",
+								"display_url": "pdfa.org",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/E4O2yMPGQ5",
+								"expanded_url": "http://bit.ly/2gLzrNd",
+								"display_url": "bit.ly/2gLzrNd",
+								"indices": [
+									133,
+									156
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 886,
+				"friends_count": 330,
+				"listed_count": 32,
+				"created_at": "Sat Sep 17 21:48:47 +0000 2011",
+				"favourites_count": 402,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 1592,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1090229703124889600/QcOIZ-_S_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1090229703124889600/QcOIZ-_S_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/375301497/1548865795",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 29,
+			"favorite_count": 33,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "This is brilliant. https://t.co/VV9exZk6Xk"
+	},
+	{
 		"created_at": "Sat Apr 20 13:13:27 +0000 2019",
 		"id": 1119589899471130600,
 		"id_str": "1119589899471130625",
-		"full_text": "People still use those passwords…in 2019. 🤦🏻‍♂️ https://t.co/tiyGvxNLuU",
+		"full_text": "People still use those passwords…in 2019. 🤦🏻♂️ https://t.co/tiyGvxNLuU",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -313,7 +567,7 @@ Grailbird.data.tweets_2019_04 =
 		"retweeted": false,
 		"possibly_sensitive": false,
 		"lang": "en",
-		"text": "People still use those passwords…in 2019. 🤦🏻‍♂️ https://t.co/tiyGvxNLuU"
+		"text": "People still use those passwords…in 2019. 🤦🏻♂️ https://t.co/tiyGvxNLuU"
 	},
 	{
 		"created_at": "Thu Apr 18 22:18:53 +0000 2019",
