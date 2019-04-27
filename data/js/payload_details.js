8518b81c2ca381587270fc769366e4855b9600fe
Zak Winnick <zakwinnick@protonmail.com>
2019-04-26 21:10:47 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10238,
-	"created_at": "Fri Apr 26 13:18:43 +0000 2019",
+	"tweets": 10239,
+	"created_at": "Sat Apr 27 02:10:46 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,302 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Sat Apr 27 01:54:46 +0000 2019",
+		"id": 1121955818251878400,
+		"id_str": "1121955818251878407",
+		"full_text": "I’m not sure how I feel about this one… https://t.co/rxuSvxtGtw",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			39
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/rxuSvxtGtw",
+					"expanded_url": "https://twitter.com/CameronCrowe/status/1121942650733510657",
+					"display_url": "twitter.com/CameronCrowe/s…",
+					"indices": [
+						40,
+						63
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 9586552,
+			"id_str": "9586552",
+			"name": "Zak Winnick",
+			"screen_name": "ZakWinnick",
+			"location": "Nashville, TN",
+			"description": "Hotel Technology Guru | Electric Vehicle Enthusiast | Founder @SurfChaosMedia",
+			"url": "https://t.co/h6N0tCU1zj",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/h6N0tCU1zj",
+							"expanded_url": "http://ZakWinnick.com",
+							"display_url": "ZakWinnick.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 56,
+			"friends_count": 172,
+			"listed_count": 18,
+			"created_at": "Sun Oct 21 22:28:31 +0000 2007",
+			"favourites_count": 140,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9146,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "3F5B66",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1117910108032442368/PuYwWPMX_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1117910108032442368/PuYwWPMX_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/9586552/1555366317",
+			"profile_link_color": "483D8B",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "858080",
+			"profile_text_color": "086660",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1121942650733510700,
+		"quoted_status_id_str": "1121942650733510657",
+		"quoted_status_permalink": {
+			"url": "https://t.co/rxuSvxtGtw",
+			"expanded": "https://twitter.com/CameronCrowe/status/1121942650733510657",
+			"display": "twitter.com/CameronCrowe/s…"
+		},
+		"quoted_status": {
+			"created_at": "Sat Apr 27 01:02:27 +0000 2019",
+			"id": 1121942650733510700,
+			"id_str": "1121942650733510657",
+			"full_text": "The Almost Famous musical world premiere is happening this Fall in San Diego. https://t.co/QIQtHgpdbb https://t.co/9uo67kPMQI",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				101
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/QIQtHgpdbb",
+						"expanded_url": "http://www.theuncool.com/2019/04/26/its-all-happening-almost-famous-the-musical/",
+						"display_url": "theuncool.com/2019/04/26/its…",
+						"indices": [
+							78,
+							101
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1121942473276702700,
+						"id_str": "1121942473276702721",
+						"indices": [
+							102,
+							125
+						],
+						"media_url": "http://pbs.twimg.com/media/D5HwvDIUwAENtUg.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/D5HwvDIUwAENtUg.jpg",
+						"url": "https://t.co/9uo67kPMQI",
+						"display_url": "pic.twitter.com/9uo67kPMQI",
+						"expanded_url": "https://twitter.com/CameronCrowe/status/1121942650733510657/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 820,
+								"h": 410,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 820,
+								"h": 410,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 340,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1121942473276702700,
+						"id_str": "1121942473276702721",
+						"indices": [
+							102,
+							125
+						],
+						"media_url": "http://pbs.twimg.com/media/D5HwvDIUwAENtUg.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/D5HwvDIUwAENtUg.jpg",
+						"url": "https://t.co/9uo67kPMQI",
+						"display_url": "pic.twitter.com/9uo67kPMQI",
+						"expanded_url": "https://twitter.com/CameronCrowe/status/1121942650733510657/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 820,
+								"h": 410,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 820,
+								"h": 410,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 340,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 275197798,
+				"id_str": "275197798",
+				"name": "Cameron Crowe",
+				"screen_name": "CameronCrowe",
+				"location": "",
+				"description": "Writer-director.",
+				"url": "https://t.co/c0RknEmBA3",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/c0RknEmBA3",
+								"expanded_url": "http://www.theuncool.com",
+								"display_url": "theuncool.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 149917,
+				"friends_count": 1024,
+				"listed_count": 2012,
+				"created_at": "Thu Mar 31 20:31:06 +0000 2011",
+				"favourites_count": 2470,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 3802,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "21C4B6",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/642532549198090241/veRDzO6U_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/642532549198090241/veRDzO6U_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/275197798/1444631856",
+				"profile_link_color": "5C9191",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 41,
+			"favorite_count": 195,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I’m not sure how I feel about this one… https://t.co/rxuSvxtGtw"
+	},
+	{
 		"created_at": "Fri Apr 26 13:12:31 +0000 2019",
 		"id": 1121763990785466400,
 		"id_str": "1121763990785466369",
