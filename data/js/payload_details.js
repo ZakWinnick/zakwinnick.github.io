c280d056889b9bdb160b1057e4606313ac818a71
Zak Winnick <zakwinnick@protonmail.com>
2019-04-28 13:10:47 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10244,
-	"created_at": "Sun Apr 28 15:10:46 +0000 2019",
+	"tweets": 10245,
+	"created_at": "Sun Apr 28 18:10:46 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,308 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Sun Apr 28 17:28:59 +0000 2019",
+		"id": 1122553307774496800,
+		"id_str": "1122553307774496774",
+		"full_text": "Everyone should subscribe and check out Indy as the Cybercat of the Week! \nhttps://t.co/ndfcWBcNQz https://t.co/DTWB2xVfL2",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			98
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/ndfcWBcNQz",
+					"expanded_url": "https://twitter.com/zackwhittaker/status/1122509642083573761",
+					"display_url": "twitter.com/zackwhittaker/…",
+					"indices": [
+						75,
+						98
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1122553285469143000,
+					"id_str": "1122553285469143040",
+					"indices": [
+						99,
+						122
+					],
+					"media_url": "http://pbs.twimg.com/media/D5QcQ_3WAAA7hj9.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D5QcQ_3WAAA7hj9.jpg",
+					"url": "https://t.co/DTWB2xVfL2",
+					"display_url": "pic.twitter.com/DTWB2xVfL2",
+					"expanded_url": "https://twitter.com/ZakWinnick/status/1122553307774496774/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 554,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 946,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 314,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1122553285469143000,
+					"id_str": "1122553285469143040",
+					"indices": [
+						99,
+						122
+					],
+					"media_url": "http://pbs.twimg.com/media/D5QcQ_3WAAA7hj9.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D5QcQ_3WAAA7hj9.jpg",
+					"url": "https://t.co/DTWB2xVfL2",
+					"display_url": "pic.twitter.com/DTWB2xVfL2",
+					"expanded_url": "https://twitter.com/ZakWinnick/status/1122553307774496774/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 554,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 946,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 314,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 9586552,
+			"id_str": "9586552",
+			"name": "Zak Winnick",
+			"screen_name": "ZakWinnick",
+			"location": "Nashville, TN",
+			"description": "Hotel Technology Guru | Electric Vehicle Enthusiast | Founder @SurfChaosMedia",
+			"url": "https://t.co/h6N0tCU1zj",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/h6N0tCU1zj",
+							"expanded_url": "http://ZakWinnick.com",
+							"display_url": "ZakWinnick.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 56,
+			"friends_count": 173,
+			"listed_count": 18,
+			"created_at": "Sun Oct 21 22:28:31 +0000 2007",
+			"favourites_count": 141,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9152,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "3F5B66",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1117910108032442368/PuYwWPMX_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1117910108032442368/PuYwWPMX_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/9586552/1555366317",
+			"profile_link_color": "483D8B",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "858080",
+			"profile_text_color": "086660",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1122509642083573800,
+		"quoted_status_id_str": "1122509642083573761",
+		"quoted_status_permalink": {
+			"url": "https://t.co/ndfcWBcNQz",
+			"expanded": "https://twitter.com/zackwhittaker/status/1122509642083573761",
+			"display": "twitter.com/zackwhittaker/…"
+		},
+		"quoted_status": {
+			"created_at": "Sun Apr 28 14:35:28 +0000 2019",
+			"id": 1122509642083573800,
+			"id_str": "1122509642083573761",
+			"full_text": "Happy Sunday! My weekly cybersecurity newsletter is out to 2,845 subscribers. This week we have more ransomware attacks, car halting hacks, cryptocurrency thefts and a lot more. Plus, a new cybercat.\n\nSign up: https://t.co/mtLrEIJEJn — or read online. https://t.co/FSW3tYmSkA",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				275
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/mtLrEIJEJn",
+						"expanded_url": "http://eepurl.com/dBQdJ5",
+						"display_url": "eepurl.com/dBQdJ5",
+						"indices": [
+							210,
+							233
+						]
+					},
+					{
+						"url": "https://t.co/FSW3tYmSkA",
+						"expanded_url": "https://mailchi.mp/zackwhittaker/this-week-in-security-april-28-edition",
+						"display_url": "mailchi.mp/zackwhittaker/…",
+						"indices": [
+							252,
+							275
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 15752235,
+				"id_str": "15752235",
+				"name": "Zack Whittaker",
+				"screen_name": "zackwhittaker",
+				"location": "New York, NY",
+				"description": "Security editor, @TechCrunch. CBS alum. Email: zack.whittaker@gmail.com. WhatsApp, Signal: +1.646.755.8849. Newsletter: https://t.co/0aSHFyliQz",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/0aSHFyliQz",
+								"expanded_url": "http://eepurl.com/dBQdJ5",
+								"display_url": "eepurl.com/dBQdJ5",
+								"indices": [
+									120,
+									143
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 42166,
+				"friends_count": 999,
+				"listed_count": 1474,
+				"created_at": "Wed Aug 06 17:00:05 +0000 2008",
+				"favourites_count": 49385,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 3017,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": true,
+				"profile_background_color": "131516",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1011798454866235392/-WKpbT-d_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1011798454866235392/-WKpbT-d_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/15752235/1453743768",
+				"profile_link_color": "36345C",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "EFEFEF",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "regular"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 5,
+			"favorite_count": 12,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Everyone should subscribe and check out Indy as the Cybercat of the Week! \nhttps://t.co/ndfcWBcNQz https://t.co/DTWB2xVfL2"
+	},
+	{
 		"created_at": "Sun Apr 28 14:33:15 +0000 2019",
 		"id": 1122509083804864500,
 		"id_str": "1122509083804864512",
