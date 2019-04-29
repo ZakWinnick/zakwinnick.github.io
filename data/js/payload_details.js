b21090afc19e607d360f1d904485dd40df6f69ec
Zak Winnick <zakwinnick@protonmail.com>
2019-04-29 12:10:47 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10246,
-	"created_at": "Mon Apr 29 13:10:46 +0000 2019",
+	"tweets": 10247,
+	"created_at": "Mon Apr 29 17:10:46 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,224 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Mon Apr 29 16:22:19 +0000 2019",
+		"id": 1122898920659615700,
+		"id_str": "1122898920659615744",
+		"full_text": "Daily Source Code by @adamcurry.\n\nIt remains the only podcast I listened to religiously. https://t.co/D463L2UJG7",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			88
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "adamcurry",
+					"name": "Adam Curry",
+					"id": 2062801,
+					"id_str": "2062801",
+					"indices": [
+						21,
+						31
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/D463L2UJG7",
+					"expanded_url": "https://twitter.com/stalman/status/1122897188361625600",
+					"display_url": "twitter.com/stalman/status…",
+					"indices": [
+						89,
+						112
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 9586552,
+			"id_str": "9586552",
+			"name": "Zak Winnick",
+			"screen_name": "ZakWinnick",
+			"location": "Nashville, TN",
+			"description": "Hotel Technology Guru | Electric Vehicle Enthusiast | Founder @SurfChaosMedia",
+			"url": "https://t.co/h6N0tCU1zj",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/h6N0tCU1zj",
+							"expanded_url": "http://ZakWinnick.com",
+							"display_url": "ZakWinnick.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 58,
+			"friends_count": 173,
+			"listed_count": 18,
+			"created_at": "Sun Oct 21 22:28:31 +0000 2007",
+			"favourites_count": 143,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9154,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "3F5B66",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1117910108032442368/PuYwWPMX_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1117910108032442368/PuYwWPMX_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/9586552/1555366317",
+			"profile_link_color": "483D8B",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "858080",
+			"profile_text_color": "086660",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1122897188361625600,
+		"quoted_status_id_str": "1122897188361625600",
+		"quoted_status_permalink": {
+			"url": "https://t.co/D463L2UJG7",
+			"expanded": "https://twitter.com/stalman/status/1122897188361625600",
+			"display": "twitter.com/stalman/status…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Apr 29 16:15:26 +0000 2019",
+			"id": 1122897188361625600,
+			"id_str": "1122897188361625600",
+			"full_text": "What’s a podcast that you’ve listened to (pretty much) every episode of?",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				72
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 10307712,
+				"id_str": "10307712",
+				"name": "Tyler Stalman",
+				"screen_name": "stalman",
+				"location": "Canada",
+				"description": "Full stack creator",
+				"url": "https://t.co/jf4Gh4oWxC",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/jf4Gh4oWxC",
+								"expanded_url": "http://www.stalmanpodcast.com",
+								"display_url": "stalmanpodcast.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 10893,
+				"friends_count": 173,
+				"listed_count": 241,
+				"created_at": "Fri Nov 16 16:51:34 +0000 2007",
+				"favourites_count": 6267,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 7017,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "B8C3BF",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1034325920604028928/tpB7N6JI_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1034325920604028928/tpB7N6JI_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/10307712/1513547977",
+				"profile_link_color": "ABB8C2",
+				"profile_sidebar_border_color": "7CA1AF",
+				"profile_sidebar_fill_color": "D3ECF5",
+				"profile_text_color": "000000",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "regular"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 20,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Daily Source Code by @adamcurry.\n\nIt remains the only podcast I listened to religiously. https://t.co/D463L2UJG7"
+	},
+	{
 		"created_at": "Mon Apr 29 13:02:31 +0000 2019",
 		"id": 1122848638559035400,
 		"id_str": "1122848638559035392",
