68b0fe7194e121edc6b8469a932b5cadbb660dcc
Zak Winnick <zakwinnick@protonmail.com>
2019-05-15 08:10:49 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10260,
-	"created_at": "Fri May 10 15:10:48 +0000 2019",
+	"tweets": 10261,
+	"created_at": "Wed May 15 13:10:47 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,344 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Wed May 15 13:09:13 +0000 2019",
+		"id": 1128648530011271200,
+		"id_str": "1128648530011271168",
+		"full_text": "Ok, @GoPro. Your turn… https://t.co/2GtLLEefso",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			22
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "GoPro",
+					"name": "GoPro",
+					"id": 32340023,
+					"id_str": "32340023",
+					"indices": [
+						4,
+						10
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/2GtLLEefso",
+					"expanded_url": "https://twitter.com/DJISupport/status/1128646518133657600",
+					"display_url": "twitter.com/DJISupport/sta…",
+					"indices": [
+						23,
+						46
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 9586552,
+			"id_str": "9586552",
+			"name": "Zak Winnick",
+			"screen_name": "ZakWinnick",
+			"location": "Nashville, TN",
+			"description": "Hotel Technology Guru | Electric Vehicle Enthusiast | Founder @SurfChaosMedia",
+			"url": "https://t.co/h6N0tCU1zj",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/h6N0tCU1zj",
+							"expanded_url": "http://ZakWinnick.com",
+							"display_url": "ZakWinnick.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 57,
+			"friends_count": 177,
+			"listed_count": 18,
+			"created_at": "Sun Oct 21 22:28:31 +0000 2007",
+			"favourites_count": 146,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9157,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "3F5B66",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1117910108032442368/PuYwWPMX_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1117910108032442368/PuYwWPMX_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/9586552/1555366317",
+			"profile_link_color": "483D8B",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "858080",
+			"profile_text_color": "086660",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1128646518133657600,
+		"quoted_status_id_str": "1128646518133657600",
+		"quoted_status_permalink": {
+			"url": "https://t.co/2GtLLEefso",
+			"expanded": "https://twitter.com/DJISupport/status/1128646518133657600",
+			"display": "twitter.com/DJISupport/sta…"
+		},
+		"quoted_status": {
+			"created_at": "Wed May 15 13:01:13 +0000 2019",
+			"id": 1128646518133657600,
+			"id_str": "1128646518133657600",
+			"full_text": "This is Osmo Action.\nShake it, dunk it, dive it, and most importantly - stay in the action. Screens on both sides mean you'll always frame your shot perfectly, no matter what you're shooting.\n\nGet more information or buy your own today at: https://t.co/2iXssf7xfz https://t.co/QU3O6oBULB",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				263
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/2iXssf7xfz",
+						"expanded_url": "https://buff.ly/2JGGLKm",
+						"display_url": "buff.ly/2JGGLKm",
+						"indices": [
+							240,
+							263
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1128646338953076700,
+						"id_str": "1128646338953076737",
+						"indices": [
+							264,
+							287
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1128646338953076737/pu/img/B-htsotObRvyrsPQ.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1128646338953076737/pu/img/B-htsotObRvyrsPQ.jpg",
+						"url": "https://t.co/QU3O6oBULB",
+						"display_url": "pic.twitter.com/QU3O6oBULB",
+						"expanded_url": "https://twitter.com/DJISupport/status/1128646518133657600/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 720,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1128646338953076700,
+						"id_str": "1128646338953076737",
+						"indices": [
+							264,
+							287
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1128646338953076737/pu/img/B-htsotObRvyrsPQ.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1128646338953076737/pu/img/B-htsotObRvyrsPQ.jpg",
+						"url": "https://t.co/QU3O6oBULB",
+						"display_url": "pic.twitter.com/QU3O6oBULB",
+						"expanded_url": "https://twitter.com/DJISupport/status/1128646518133657600/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 720,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								16,
+								9
+							],
+							"duration_millis": 113834,
+							"variants": [
+								{
+									"bitrate": 2176000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1128646338953076737/pu/vid/1280x720/6AMj9sSGQEkCZFHN.mp4?tag=9"
+								},
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/ext_tw_video/1128646338953076737/pu/pl/e4NVBsGNPZi1ZUkm.m3u8?tag=9"
+								},
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1128646338953076737/pu/vid/640x360/XxLqZPVwTT0kVmxo.mp4?tag=9"
+								},
+								{
+									"bitrate": 256000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1128646338953076737/pu/vid/320x180/8MBq1JoHZHsDM9fL.mp4?tag=9"
+								}
+							]
+						},
+						"additional_media_info": {
+							"monetizable": false
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://buffer.com\" rel=\"nofollow\">Buffer</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 712857593530683400,
+				"id_str": "712857593530683392",
+				"name": "DJI Support",
+				"screen_name": "DJISupport",
+				"location": "",
+				"description": "We're here to help. The official Customer Support account of #DJI.",
+				"url": "https://t.co/tHLSlrTVGc",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/tHLSlrTVGc",
+								"expanded_url": "http://www.dji.com/support",
+								"display_url": "dji.com/support",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 16544,
+				"friends_count": 154,
+				"listed_count": 78,
+				"created_at": "Thu Mar 24 04:24:29 +0000 2016",
+				"favourites_count": 1060,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 20390,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/839369423907532800/n5Ot6I4Z_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/839369423907532800/n5Ot6I4Z_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/712857593530683392/1557371287",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 7,
+			"favorite_count": 11,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Ok, @GoPro. Your turn… https://t.co/2GtLLEefso"
+	},
+	{
 		"created_at": "Fri May 10 14:31:09 +0000 2019",
 		"id": 1126857208862576600,
 		"id_str": "1126857208862576640",
