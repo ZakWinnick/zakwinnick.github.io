49d80c488633e578d53b8c077f4ba7945558aa08
Zak Winnick <zakwinnick@protonmail.com>
2019-05-16 13:10:48 -0500

    Added 1 tweets

--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,326 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Thu May 16 17:27:10 +0000 2019",
+		"id": 1129075831115046900,
+		"id_str": "1129075831115046917",
+		"full_text": "Teach me. https://t.co/Fz5u1rLp65",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			9
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Fz5u1rLp65",
+					"expanded_url": "https://twitter.com/firstwefeast/status/1128797204401885185",
+					"display_url": "twitter.com/firstwefeast/s…",
+					"indices": [
+						10,
+						33
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 9586552,
+			"id_str": "9586552",
+			"name": "Zak Winnick",
+			"screen_name": "ZakWinnick",
+			"location": "Nashville, TN",
+			"description": "Hotel Technology Guru | Electric Vehicle Enthusiast | Founder @SurfChaosMedia",
+			"url": "https://t.co/h6N0tCU1zj",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/h6N0tCU1zj",
+							"expanded_url": "http://ZakWinnick.com",
+							"display_url": "ZakWinnick.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 56,
+			"friends_count": 177,
+			"listed_count": 18,
+			"created_at": "Sun Oct 21 22:28:31 +0000 2007",
+			"favourites_count": 137,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9161,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "3F5B66",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1117910108032442368/PuYwWPMX_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1117910108032442368/PuYwWPMX_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/9586552/1555366317",
+			"profile_link_color": "483D8B",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "858080",
+			"profile_text_color": "086660",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1128797204401885200,
+		"quoted_status_id_str": "1128797204401885185",
+		"quoted_status_permalink": {
+			"url": "https://t.co/Fz5u1rLp65",
+			"expanded": "https://twitter.com/firstwefeast/status/1128797204401885185",
+			"display": "twitter.com/firstwefeast/s…"
+		},
+		"quoted_status": {
+			"created_at": "Wed May 15 23:00:00 +0000 2019",
+			"id": 1128797204401885200,
+			"id_str": "1128797204401885185",
+			"full_text": "This dudes chopping skills are A1 🔥 https://t.co/WLAaxhKS7Z",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				35
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 822497590235140100,
+						"id_str": "822497590235140096",
+						"indices": [
+							36,
+							59
+						],
+						"media_url": "http://pbs.twimg.com/amplify_video_thumb/822497590235140096/img/RoL_qnkRNWO5eRD7.jpg",
+						"media_url_https": "https://pbs.twimg.com/amplify_video_thumb/822497590235140096/img/RoL_qnkRNWO5eRD7.jpg",
+						"url": "https://t.co/WLAaxhKS7Z",
+						"display_url": "pic.twitter.com/WLAaxhKS7Z",
+						"expanded_url": "https://twitter.com/firstwefeast/status/1128797204401885185/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 383,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 720,
+								"h": 1280,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 675,
+								"h": 1200,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 822497590235140100,
+						"id_str": "822497590235140096",
+						"indices": [
+							36,
+							59
+						],
+						"media_url": "http://pbs.twimg.com/amplify_video_thumb/822497590235140096/img/RoL_qnkRNWO5eRD7.jpg",
+						"media_url_https": "https://pbs.twimg.com/amplify_video_thumb/822497590235140096/img/RoL_qnkRNWO5eRD7.jpg",
+						"url": "https://t.co/WLAaxhKS7Z",
+						"display_url": "pic.twitter.com/WLAaxhKS7Z",
+						"expanded_url": "https://twitter.com/firstwefeast/status/1128797204401885185/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 383,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 720,
+								"h": 1280,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 675,
+								"h": 1200,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								9,
+								16
+							],
+							"duration_millis": 40941,
+							"variants": [
+								{
+									"bitrate": 320000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/amplify_video/822497590235140096/vid/180x320/4GwDoeGhwyRTSdJh.mp4"
+								},
+								{
+									"bitrate": 2176000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/amplify_video/822497590235140096/vid/720x1280/8tCYFpBho6jc6Bux.mp4"
+								},
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/amplify_video/822497590235140096/vid/360x640/tXxxyjDoHN5pBIaT.mp4"
+								},
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/amplify_video/822497590235140096/pl/H30F8eqECAhoLkte.m3u8"
+								}
+							]
+						},
+						"additional_media_info": {
+							"title": "",
+							"description": "",
+							"embeddable": true,
+							"monetizable": true
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://studio.twitter.com\" rel=\"nofollow\">Twitter Media Studio</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 770352762,
+				"id_str": "770352762",
+				"name": "First We Feast",
+				"screen_name": "firstwefeast",
+				"location": "New York, NY",
+				"description": "Bridging the gap between food, drinks & pop culture. #FirstWeFeast",
+				"url": "https://t.co/afhl0ZlKA2",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/afhl0ZlKA2",
+								"expanded_url": "http://www.firstwefeast.com",
+								"display_url": "firstwefeast.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 180147,
+				"friends_count": 1069,
+				"listed_count": 1163,
+				"created_at": "Mon Aug 20 22:38:44 +0000 2012",
+				"favourites_count": 13977,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 36312,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "FFD200",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1022929414261297153/zhWl6cz5_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1022929414261297153/zhWl6cz5_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/770352762/1558020293",
+				"profile_link_color": "ABB8C2",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 28,
+			"favorite_count": 244,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Teach me. https://t.co/Fz5u1rLp65"
+	},
+	{
 		"created_at": "Thu May 16 13:15:56 +0000 2019",
 		"id": 1129012606168182800,
 		"id_str": "1129012606168182785",
