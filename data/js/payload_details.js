bbded098f6dab37d58be7f57a8c21879ce6b133b
Zak Winnick <zakwinnick@protonmail.com>
2019-05-22 17:10:47 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10272,
-	"created_at": "Wed May 22 02:10:46 +0000 2019",
+	"tweets": 10273,
+	"created_at": "Wed May 22 22:10:46 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,302 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Wed May 22 22:01:36 +0000 2019",
+		"id": 1131319223383408600,
+		"id_str": "1131319223383408645",
+		"full_text": "My 3rd DMB show ever (the 2 shows before this one were my first 2). \n\nWhat a weird experience that show was…\n\nFlying home for the weekend from SF. Dodger Stadium for a concert. Macy Gray as the opener. John Popper guesting on WWYS. https://t.co/8IZW89hGzN",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			231
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/8IZW89hGzN",
+					"expanded_url": "https://twitter.com/antsmarching/status/1131273533844123649",
+					"display_url": "twitter.com/antsmarching/s…",
+					"indices": [
+						232,
+						255
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 9586552,
+			"id_str": "9586552",
+			"name": "Zak Winnick",
+			"screen_name": "ZakWinnick",
+			"location": "Nashville, TN",
+			"description": "Hotel Technology Guru | Electric Vehicle Enthusiast | Founder @SurfChaosMedia",
+			"url": "https://t.co/h6N0tCU1zj",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/h6N0tCU1zj",
+							"expanded_url": "http://ZakWinnick.com",
+							"display_url": "ZakWinnick.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 56,
+			"friends_count": 176,
+			"listed_count": 18,
+			"created_at": "Sun Oct 21 22:28:31 +0000 2007",
+			"favourites_count": 139,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9165,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "3F5B66",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1117910108032442368/PuYwWPMX_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1117910108032442368/PuYwWPMX_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/9586552/1555366317",
+			"profile_link_color": "483D8B",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "858080",
+			"profile_text_color": "086660",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1131273533844123600,
+		"quoted_status_id_str": "1131273533844123649",
+		"quoted_status_permalink": {
+			"url": "https://t.co/8IZW89hGzN",
+			"expanded": "https://twitter.com/antsmarching/status/1131273533844123649",
+			"display": "twitter.com/antsmarching/s…"
+		},
+		"quoted_status": {
+			"created_at": "Wed May 22 19:00:03 +0000 2019",
+			"id": 1131273533844123600,
+			"id_str": "1131273533844123649",
+			"full_text": "On this day in DMB History: DMB2001-05-22 @ Los Angeles, CA  https://t.co/jnr3Gm4ypy https://t.co/5Cp4mzsAES",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				84
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/jnr3Gm4ypy",
+						"expanded_url": "http://bit.ly/1vNpkcv",
+						"display_url": "bit.ly/1vNpkcv",
+						"indices": [
+							61,
+							84
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1131273532619481100,
+						"id_str": "1131273532619481088",
+						"indices": [
+							85,
+							108
+						],
+						"media_url": "http://pbs.twimg.com/media/D7MXSL-V4AA_8iD.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/D7MXSL-V4AA_8iD.jpg",
+						"url": "https://t.co/5Cp4mzsAES",
+						"display_url": "pic.twitter.com/5Cp4mzsAES",
+						"expanded_url": "https://twitter.com/antsmarching/status/1131273533844123649/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1024,
+								"h": 1680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 731,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 414,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1131273532619481100,
+						"id_str": "1131273532619481088",
+						"indices": [
+							85,
+							108
+						],
+						"media_url": "http://pbs.twimg.com/media/D7MXSL-V4AA_8iD.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/D7MXSL-V4AA_8iD.jpg",
+						"url": "https://t.co/5Cp4mzsAES",
+						"display_url": "pic.twitter.com/5Cp4mzsAES",
+						"expanded_url": "https://twitter.com/antsmarching/status/1131273533844123649/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1024,
+								"h": 1680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 731,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 414,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://www.antsmarching.org\" rel=\"nofollow\">antsmarching.org</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 16629394,
+				"id_str": "16629394",
+				"name": "antsmarching.org",
+				"screen_name": "antsmarching",
+				"location": "People in Every Direction",
+				"description": "The premier Dave Matthews Band fansite, with over a quarter million registered members. Follow @dmblive for live setlist updates all tour long!",
+				"url": "http://t.co/O9KcHOlvmY",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/O9KcHOlvmY",
+								"expanded_url": "http://www.antsmarching.org",
+								"display_url": "antsmarching.org",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 45971,
+				"friends_count": 17475,
+				"listed_count": 586,
+				"created_at": "Tue Oct 07 13:26:06 +0000 2008",
+				"favourites_count": 2303,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 17367,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "1B2D41",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/692442260038746112/Zfm0hHZo_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/692442260038746112/Zfm0hHZo_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/16629394/1400101616",
+				"profile_link_color": "1B2D41",
+				"profile_sidebar_border_color": "D0D3C0",
+				"profile_sidebar_fill_color": "9BAF29",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 0,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "My 3rd DMB show ever (the 2 shows before this one were my first 2). \n\nWhat a weird experience that show was…\n\nFlying home for the weekend from SF. Dodger Stadium for a concert. Macy Gray as the opener. John Popper guesting on WWYS. https://t.co/8IZW89hGzN"
+	},
+	{
 		"created_at": "Wed May 22 01:40:56 +0000 2019",
 		"id": 1131012033070227500,
 		"id_str": "1131012033070227457",
