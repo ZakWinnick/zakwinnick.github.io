7c15891c77b0e6809796ef61fcefa50f8f3338ae
Zak Winnick <zakwinnick@protonmail.com>
2019-05-30 08:10:49 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10280,
-	"created_at": "Tue May 28 23:10:46 +0000 2019",
+	"tweets": 10281,
+	"created_at": "Thu May 30 13:10:46 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,167 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Thu May 30 12:48:27 +0000 2019",
+		"id": 1133912736876945400,
+		"id_str": "1134079123075358720",
+		"full_text": "So there. https://t.co/zoexwDDmZY",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			9
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1133912731797598200,
+					"id_str": "1133912731797598209",
+					"indices": [
+						10,
+						33
+					],
+					"media_url": "http://pbs.twimg.com/media/D7x3nzEWsAEmksl.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D7x3nzEWsAEmksl.jpg",
+					"url": "https://t.co/zoexwDDmZY",
+					"display_url": "pic.twitter.com/zoexwDDmZY",
+					"expanded_url": "https://twitter.com/bradmeltzer/status/1133912736876945411/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 609,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1125,
+							"h": 1257,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1074,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1133912731797598200,
+					"id_str": "1133912731797598209",
+					"indices": [
+						10,
+						33
+					],
+					"media_url": "http://pbs.twimg.com/media/D7x3nzEWsAEmksl.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D7x3nzEWsAEmksl.jpg",
+					"url": "https://t.co/zoexwDDmZY",
+					"display_url": "pic.twitter.com/zoexwDDmZY",
+					"expanded_url": "https://twitter.com/bradmeltzer/status/1133912736876945411/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 609,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1125,
+							"h": 1257,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1074,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 34386708,
+			"id_str": "34386708",
+			"name": "Brad Meltzer",
+			"screen_name": "bradmeltzer",
+			"location": "",
+			"description": "The greatest criminal mind of our time. Bestselling author. Host of Lost #History. #Books. #Thrillers. #Comics. But really, greatest criminal mind.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 60733,
+			"friends_count": 915,
+			"listed_count": 1545,
+			"created_at": "Wed Apr 22 20:27:46 +0000 2009",
+			"favourites_count": 38359,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 23686,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/864091396415684608/onpE4Ccj_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/864091396415684608/onpE4Ccj_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/34386708/1557228778",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDFFCC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 257,
+		"favorite_count": 793,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "So there. https://t.co/zoexwDDmZY"
+	},
+	{
 		"created_at": "Tue May 28 22:51:40 +0000 2019",
 		"id": 1133506152409522200,
 		"id_str": "1133506152409522177",
