f00f10a4da47e35b2947d8bffe14df983da6cef8
Zak Winnick <zakwinnick@protonmail.com>
2019-06-12 20:10:46 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10288,
-	"created_at": "Thu Jun 13 00:18:43 +0000 2019",
+	"tweets": 10289,
+	"created_at": "Thu Jun 13 01:10:45 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_06.js
+++ data/js/tweets/2019_06.js
@@ -1,6 +1,180 @@
 Grailbird.data.tweets_2019_06 = 
 [
 	{
+		"created_at": "Thu Jun 13 00:19:01 +0000 2019",
+		"id": 1138832019851493400,
+		"id_str": "1138963949813227523",
+		"full_text": "IKEA has joined in poking fun at the new Mac Pro… 🧀 https://t.co/G6lGEv7dWT",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			51
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1138831741349744600,
+					"id_str": "1138831741349744640",
+					"indices": [
+						52,
+						75
+					],
+					"media_url": "http://pbs.twimg.com/media/D83xb1oXUAA6bD-.png",
+					"media_url_https": "https://pbs.twimg.com/media/D83xb1oXUAA6bD-.png",
+					"url": "https://t.co/G6lGEv7dWT",
+					"display_url": "pic.twitter.com/G6lGEv7dWT",
+					"expanded_url": "https://twitter.com/MacRumors/status/1138832019851493376/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 650,
+							"h": 866,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 650,
+							"h": 866,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1138831741349744600,
+					"id_str": "1138831741349744640",
+					"indices": [
+						52,
+						75
+					],
+					"media_url": "http://pbs.twimg.com/media/D83xb1oXUAA6bD-.png",
+					"media_url_https": "https://pbs.twimg.com/media/D83xb1oXUAA6bD-.png",
+					"url": "https://t.co/G6lGEv7dWT",
+					"display_url": "pic.twitter.com/G6lGEv7dWT",
+					"expanded_url": "https://twitter.com/MacRumors/status/1138832019851493376/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 650,
+							"h": 866,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 650,
+							"h": 866,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14861285,
+			"id_str": "14861285",
+			"name": "MacRumors.com",
+			"screen_name": "MacRumors",
+			"location": "",
+			"description": "Apple, iPhone, iPad and Mac rumors, news, tips and videos. Follow us to keep up with the latest! Message us with questions / comments.",
+			"url": "https://t.co/qq2P8QLVoq",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qq2P8QLVoq",
+							"expanded_url": "https://www.macrumors.com",
+							"display_url": "macrumors.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 878636,
+			"friends_count": 122,
+			"listed_count": 18650,
+			"created_at": "Wed May 21 20:01:19 +0000 2008",
+			"favourites_count": 804,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 35914,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C2D6F2",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1057301750854680576/liXw_2jb_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1057301750854680576/liXw_2jb_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14861285/1399010969",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 434,
+		"favorite_count": 1539,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "IKEA has joined in poking fun at the new Mac Pro… 🧀 https://t.co/G6lGEv7dWT"
+	},
+	{
 		"created_at": "Thu Jun 13 00:16:49 +0000 2019",
 		"id": 1138963397855404000,
 		"id_str": "1138963397855404033",
