82a035130fb3637efadd22d575482bf7cbd35750
Zak Winnick <zakwinnick@protonmail.com>
2019-06-12 19:18:44 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10287,
-	"created_at": "Sun Jun 09 17:10:45 +0000 2019",
+	"tweets": 10288,
+	"created_at": "Thu Jun 13 00:18:43 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_06.js
+++ data/js/tweets/2019_06.js
@@ -1,6 +1,275 @@
 Grailbird.data.tweets_2019_06 = 
 [
 	{
+		"created_at": "Thu Jun 13 00:16:49 +0000 2019",
+		"id": 1138963397855404000,
+		"id_str": "1138963397855404033",
+		"full_text": "Well, I know what I’m doing Friday… https://t.co/AIslSVfYib",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			35
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/AIslSVfYib",
+					"expanded_url": "https://twitter.com/xjoshbaileyx/status/1138870146548752385",
+					"display_url": "twitter.com/xjoshbaileyx/s…",
+					"indices": [
+						36,
+						59
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 9586552,
+			"id_str": "9586552",
+			"name": "Zak Winnick",
+			"screen_name": "ZakWinnick",
+			"location": "Nashville, TN",
+			"description": "Hotel Technology Guru | Electric Vehicle Enthusiast | Founder @SurfChaosMedia",
+			"url": "https://t.co/h6N0tCU1zj",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/h6N0tCU1zj",
+							"expanded_url": "http://ZakWinnick.com",
+							"display_url": "ZakWinnick.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 55,
+			"friends_count": 178,
+			"listed_count": 18,
+			"created_at": "Sun Oct 21 22:28:31 +0000 2007",
+			"favourites_count": 144,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9178,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "3F5B66",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1117910108032442368/PuYwWPMX_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1117910108032442368/PuYwWPMX_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/9586552/1555366317",
+			"profile_link_color": "483D8B",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "858080",
+			"profile_text_color": "086660",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1138870146548752400,
+		"quoted_status_id_str": "1138870146548752385",
+		"quoted_status_permalink": {
+			"url": "https://t.co/AIslSVfYib",
+			"expanded": "https://twitter.com/xjoshbaileyx/status/1138870146548752385",
+			"display": "twitter.com/xjoshbaileyx/s…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Jun 12 18:06:16 +0000 2019",
+			"id": 1138870146548752400,
+			"id_str": "1138870146548752385",
+			"full_text": "Here’s the reveal of the beginning of an ongoing collection I’m doing with @SuperYakiStuff starting with Back to the Future: The Ride. So happy they’re letting me create some silly theme park designs. Very happy with how these turned out. They drop on Friday! https://t.co/AGuuTMx2xi",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				259
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "SuperYakiStuff",
+						"name": "Super Yaki",
+						"id": 859565533900963800,
+						"id_str": "859565533900963844",
+						"indices": [
+							75,
+							90
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/AGuuTMx2xi",
+						"expanded_url": "https://twitter.com/superyakistuff/status/1138855402760802305",
+						"display_url": "twitter.com/superyakistuff…",
+						"indices": [
+							260,
+							283
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 2447076657,
+				"id_str": "2447076657",
+				"name": "Josh Bailey",
+				"screen_name": "xjoshbaileyx",
+				"location": "Austin, TX",
+				"description": "Graphic Designer @RoosterTeeth ——————Theme Park Fan",
+				"url": "https://t.co/LO2tvWZZlt",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/LO2tvWZZlt",
+								"expanded_url": "http://joshbaileydesign.com",
+								"display_url": "joshbaileydesign.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 811,
+				"friends_count": 633,
+				"listed_count": 9,
+				"created_at": "Sun Mar 30 12:54:56 +0000 2014",
+				"favourites_count": 20600,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 4528,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1000070356512604161/shlUiGRj_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1000070356512604161/shlUiGRj_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/2447076657/1551038924",
+				"profile_link_color": "ED4B19",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": {
+				"id": "c3f37afa9efcf94b",
+				"url": "https://api.twitter.com/1.1/geo/id/c3f37afa9efcf94b.json",
+				"place_type": "city",
+				"name": "Austin",
+				"full_name": "Austin, TX",
+				"country_code": "US",
+				"country": "United States",
+				"contained_within": [],
+				"bounding_box": {
+					"type": "Polygon",
+					"coordinates": [
+						[
+							[
+								-97.928935,
+								30.127892
+							],
+							[
+								-97.5805133,
+								30.127892
+							],
+							[
+								-97.5805133,
+								30.5187994
+							],
+							[
+								-97.928935,
+								30.5187994
+							]
+						]
+					]
+				},
+				"attributes": {}
+			},
+			"contributors": null,
+			"is_quote_status": true,
+			"quoted_status_id": 1138855402760802300,
+			"quoted_status_id_str": "1138855402760802305",
+			"quoted_status_permalink": {
+				"url": "https://t.co/AGuuTMx2xi",
+				"expanded": "https://twitter.com/superyakistuff/status/1138855402760802305",
+				"display": "twitter.com/superyakistuff…"
+			},
+			"retweet_count": 3,
+			"favorite_count": 18,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Well, I know what I’m doing Friday… https://t.co/AIslSVfYib"
+	},
+	{
 		"created_at": "Sun Jun 09 16:42:12 +0000 2019",
 		"id": 1137761826068947000,
 		"id_str": "1137761826068946945",
