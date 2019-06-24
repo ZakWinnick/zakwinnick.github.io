b186d2ed65b4c8a592c082149a9ac5492054e598
Zak Winnick <zakwinnick@protonmail.com>
2019-06-24 08:10:47 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10296,
-	"created_at": "Fri Jun 21 18:10:45 +0000 2019",
+	"tweets": 10297,
+	"created_at": "Mon Jun 24 13:10:46 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_06.js
+++ data/js/tweets/2019_06.js
@@ -1,6 +1,221 @@
 Grailbird.data.tweets_2019_06 = 
 [
 	{
+		"created_at": "Mon Jun 24 12:49:43 +0000 2019",
+		"id": 1143139138650169300,
+		"id_str": "1143139138650169344",
+		"full_text": "Ooh! This is a fun one...\n\nKelly Hrudey vs. Dominik Hasek. \n\nKings lost 5-3, but that's been the only LAK game I've ever been to where they've lost, so I've got that going for me... https://t.co/BrwYIWXBXY",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			181
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/BrwYIWXBXY",
+					"expanded_url": "https://twitter.com/GeoffMags5490/status/1142936673669529601",
+					"display_url": "twitter.com/GeoffMags5490/…",
+					"indices": [
+						182,
+						205
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 9586552,
+			"id_str": "9586552",
+			"name": "Zak Winnick",
+			"screen_name": "ZakWinnick",
+			"location": "Nashville, TN",
+			"description": "Hotel Technology Guru | Electric Vehicle Enthusiast | Founder @SurfChaosMedia",
+			"url": "https://t.co/h6N0tCU1zj",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/h6N0tCU1zj",
+							"expanded_url": "http://ZakWinnick.com",
+							"display_url": "ZakWinnick.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 55,
+			"friends_count": 178,
+			"listed_count": 18,
+			"created_at": "Sun Oct 21 22:28:31 +0000 2007",
+			"favourites_count": 128,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9188,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "3F5B66",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1117910108032442368/PuYwWPMX_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1117910108032442368/PuYwWPMX_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/9586552/1555366317",
+			"profile_link_color": "483D8B",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "858080",
+			"profile_text_color": "086660",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1142936673669529600,
+		"quoted_status_id_str": "1142936673669529601",
+		"quoted_status_permalink": {
+			"url": "https://t.co/BrwYIWXBXY",
+			"expanded": "https://twitter.com/GeoffMags5490/status/1142936673669529601",
+			"display": "twitter.com/GeoffMags5490/…"
+		},
+		"quoted_status": {
+			"created_at": "Sun Jun 23 23:25:12 +0000 2019",
+			"id": 1142936673669529600,
+			"id_str": "1142936673669529601",
+			"full_text": "Social experiment: Name the starting goaltending matchup from the first #NHL game that you attended:\n\nMike Richter vs. Craig Billington",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				135
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "NHL",
+						"indices": [
+							72,
+							76
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 202394728,
+				"id_str": "202394728",
+				"name": "Geoff Magliocchetti",
+				"screen_name": "GeoffMags5490",
+				"location": "Wayne, NJ",
+				"description": "Marist Grad, Ex-Long Snapper, Current NY Liberty/NY Jets//College Football beat writer for @EliteSportsNY; PFWA Member; Host of #JetsAllTheWay podcast",
+				"url": "https://t.co/IJbv96gyV7",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/IJbv96gyV7",
+								"expanded_url": "http://www.linkedin.com/in/geoffmags",
+								"display_url": "linkedin.com/in/geoffmags",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 2645,
+				"friends_count": 3919,
+				"listed_count": 58,
+				"created_at": "Wed Oct 13 23:25:25 +0000 2010",
+				"favourites_count": 47821,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 48774,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1058178941129678848/hKDZV-Tv_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1058178941129678848/hKDZV-Tv_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/202394728/1560217464",
+				"profile_link_color": "E81C4F",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 4,
+			"favorite_count": 40,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Ooh! This is a fun one...\n\nKelly Hrudey vs. Dominik Hasek. \n\nKings lost 5-3, but that's been the only LAK game I've ever been to where they've lost, so I've got that going for me... https://t.co/BrwYIWXBXY"
+	},
+	{
 		"created_at": "Fri Jun 21 17:20:48 +0000 2019",
 		"id": 1142120195584122900,
 		"id_str": "1142120195584122884",
