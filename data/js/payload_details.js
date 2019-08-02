b85b51c3a5b8c829bc72e09d3998b08b0b59b180
Zak Winnick <zakwinnick@protonmail.com>
2019-08-02 08:10:47 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10328,
-	"created_at": "Sun Jul 28 20:10:46 +0000 2019",
+	"tweets": 10329,
+	"created_at": "Fri Aug 02 13:10:46 +0000 2019",
 	"lang": "en"
 }
---
+++ data/js/tweets/2019_08.js
@@ -0,0 +1,384 @@
+Grailbird.data.tweets_2019_08 = 
+[
+	{
+		"created_at": "Fri Aug 02 12:46:32 +0000 2019",
+		"id": 1157271464208453600,
+		"id_str": "1157271464208453639",
+		"full_text": "TIL Bullwinkle’s still exist!\n\nI used to love that place when I was a kid.\n\nThey turned the one I always used to goto into a Chili’s or something a long time ago. https://t.co/P8bKBwQXci",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			162
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/P8bKBwQXci",
+					"expanded_url": "https://twitter.com/cabel/status/1157117832825409536",
+					"display_url": "twitter.com/cabel/status/1…",
+					"indices": [
+						163,
+						186
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 9586552,
+			"id_str": "9586552",
+			"name": "Zak Winnick",
+			"screen_name": "ZakWinnick",
+			"location": "Nashville, TN",
+			"description": "Hotel Technology Guru | Electric Vehicle Enthusiast | Founder @SurfChaosMedia",
+			"url": "https://t.co/vGZZOYs8dO",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vGZZOYs8dO",
+							"expanded_url": "https://ZakWinnick.com",
+							"display_url": "ZakWinnick.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 56,
+			"friends_count": 184,
+			"listed_count": 18,
+			"created_at": "Sun Oct 21 22:28:31 +0000 2007",
+			"favourites_count": 142,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9224,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "3F5B66",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1149479819937103872/14NI-n9s_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1149479819937103872/14NI-n9s_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/9586552/1555366317",
+			"profile_link_color": "483D8B",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "858080",
+			"profile_text_color": "086660",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1157117832825409500,
+		"quoted_status_id_str": "1157117832825409536",
+		"quoted_status_permalink": {
+			"url": "https://t.co/P8bKBwQXci",
+			"expanded": "https://twitter.com/cabel/status/1157117832825409536",
+			"display": "twitter.com/cabel/status/1…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Aug 02 02:36:04 +0000 2019",
+			"id": 1157117832825409500,
+			"id_str": "1157117832825409536",
+			"full_text": "Oh no, the Bullwinkle’s in Wilsonville, OR got rid of its pizza restaurant and animatronic Bullwinkle show. In its place, a “Badenov’s” bar and a bowling alley. It’s trying to be too cool now. But bits of the original theme are still here and there... https://t.co/AKPmDcZ2co",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				251
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1157117827549032400,
+						"id_str": "1157117827549032450",
+						"indices": [
+							252,
+							275
+						],
+						"media_url": "http://pbs.twimg.com/media/EA7oiJJU8AI0ii5.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EA7oiJJU8AI0ii5.jpg",
+						"url": "https://t.co/AKPmDcZ2co",
+						"display_url": "pic.twitter.com/AKPmDcZ2co",
+						"expanded_url": "https://twitter.com/cabel/status/1157117832825409536/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 510,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1536,
+								"h": 2048,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 900,
+								"h": 1200,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1157117827549032400,
+						"id_str": "1157117827549032450",
+						"indices": [
+							252,
+							275
+						],
+						"media_url": "http://pbs.twimg.com/media/EA7oiJJU8AI0ii5.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EA7oiJJU8AI0ii5.jpg",
+						"url": "https://t.co/AKPmDcZ2co",
+						"display_url": "pic.twitter.com/AKPmDcZ2co",
+						"expanded_url": "https://twitter.com/cabel/status/1157117832825409536/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 510,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1536,
+								"h": 2048,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 900,
+								"h": 1200,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 1157117827544825900,
+						"id_str": "1157117827544825858",
+						"indices": [
+							252,
+							275
+						],
+						"media_url": "http://pbs.twimg.com/media/EA7oiJIUwAIEDU6.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EA7oiJIUwAIEDU6.jpg",
+						"url": "https://t.co/AKPmDcZ2co",
+						"display_url": "pic.twitter.com/AKPmDcZ2co",
+						"expanded_url": "https://twitter.com/cabel/status/1157117832825409536/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 900,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 510,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1536,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 1157117827548999700,
+						"id_str": "1157117827548999681",
+						"indices": [
+							252,
+							275
+						],
+						"media_url": "http://pbs.twimg.com/media/EA7oiJJUcAEyNUN.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EA7oiJJUcAEyNUN.jpg",
+						"url": "https://t.co/AKPmDcZ2co",
+						"display_url": "pic.twitter.com/AKPmDcZ2co",
+						"expanded_url": "https://twitter.com/cabel/status/1157117832825409536/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 510,
+								"h": 680,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 900,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1536,
+								"h": 2047,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 1157117827544830000,
+						"id_str": "1157117827544829952",
+						"indices": [
+							252,
+							275
+						],
+						"media_url": "http://pbs.twimg.com/media/EA7oiJIU0AA-KeQ.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EA7oiJIU0AA-KeQ.jpg",
+						"url": "https://t.co/AKPmDcZ2co",
+						"display_url": "pic.twitter.com/AKPmDcZ2co",
+						"expanded_url": "https://twitter.com/cabel/status/1157117832825409536/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1536,
+								"h": 2048,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 510,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 900,
+								"h": 1200,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1919231,
+				"id_str": "1919231",
+				"name": "Cabel",
+				"screen_name": "cabel",
+				"location": "Portland, OR",
+				"description": "Employee of Panic (Mac/iOS apps like Coda, Transmit, Prompt, etc) ⋆ Publisher of the Video Games Firewatch & Untitled Goose Game ⋆ Sometimes Writes Music",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 71496,
+				"friends_count": 540,
+				"listed_count": 1920,
+				"created_at": "Thu Mar 22 18:25:28 +0000 2007",
+				"favourites_count": 23900,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 32619,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "FF8269",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme10/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme10/bg.gif",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/763137848916660224/Llcjv9V5_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/763137848916660224/Llcjv9V5_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1919231/1557293651",
+				"profile_link_color": "19CF86",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "C1EC97",
+				"profile_text_color": "75D1A3",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "regular"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2,
+			"favorite_count": 25,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "TIL Bullwinkle’s still exist!\n\nI used to love that place when I was a kid.\n\nThey turned the one I always used to goto into a Chili’s or something a long time ago. https://t.co/P8bKBwQXci"
+	}
+]
