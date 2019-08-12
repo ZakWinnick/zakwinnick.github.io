6f660df8ffc239d4c5072768286dabe321b07a89
Zak Winnick <zakwinnick@protonmail.com>
2019-08-12 09:10:47 -0500

    Added 1 tweets

--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,323 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Mon Aug 12 13:30:35 +0000 2019",
+		"id": 1160906427503792000,
+		"id_str": "1160906427503792131",
+		"full_text": "Uh…I don’t know how I feel about this one… https://t.co/MVvAOhD1GY",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			42
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/MVvAOhD1GY",
+					"expanded_url": "https://twitter.com/Nickelodeon/status/1157705279187931136",
+					"display_url": "twitter.com/Nickelodeon/st…",
+					"indices": [
+						43,
+						66
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 9586552,
+			"id_str": "9586552",
+			"name": "Zak Winnick",
+			"screen_name": "ZakWinnick",
+			"location": "Nashville, TN",
+			"description": "Hotel Technology Guru | Electric Vehicle Enthusiast | Founder @SurfChaosMedia",
+			"url": "https://t.co/vGZZOYs8dO",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vGZZOYs8dO",
+							"expanded_url": "https://ZakWinnick.com",
+							"display_url": "ZakWinnick.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 58,
+			"friends_count": 188,
+			"listed_count": 18,
+			"created_at": "Sun Oct 21 22:28:31 +0000 2007",
+			"favourites_count": 147,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9238,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "3F5B66",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1149479819937103872/14NI-n9s_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1149479819937103872/14NI-n9s_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/9586552/1555366317",
+			"profile_link_color": "483D8B",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "858080",
+			"profile_text_color": "086660",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1157705279187931100,
+		"quoted_status_id_str": "1157705279187931136",
+		"quoted_status_permalink": {
+			"url": "https://t.co/MVvAOhD1GY",
+			"expanded": "https://twitter.com/Nickelodeon/status/1157705279187931136",
+			"display": "twitter.com/Nickelodeon/st…"
+		},
+		"quoted_status": {
+			"created_at": "Sat Aug 03 17:30:22 +0000 2019",
+			"id": 1157705279187931100,
+			"id_str": "1157705279187931136",
+			"full_text": "Do you like scary stories? All new Are You Afraid of the Dark, coming soon to Nickelodeon https://t.co/15sXxZ9c6o",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				89
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1157705210283864000,
+						"id_str": "1157705210283864064",
+						"indices": [
+							90,
+							113
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1157705210283864064/pu/img/9b_ZUiIMR9-B0FuF.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1157705210283864064/pu/img/9b_ZUiIMR9-B0FuF.jpg",
+						"url": "https://t.co/15sXxZ9c6o",
+						"display_url": "pic.twitter.com/15sXxZ9c6o",
+						"expanded_url": "https://twitter.com/Nickelodeon/status/1157705279187931136/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1024,
+								"h": 1024,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1024,
+								"h": 1024,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1157705210283864000,
+						"id_str": "1157705210283864064",
+						"indices": [
+							90,
+							113
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1157705210283864064/pu/img/9b_ZUiIMR9-B0FuF.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1157705210283864064/pu/img/9b_ZUiIMR9-B0FuF.jpg",
+						"url": "https://t.co/15sXxZ9c6o",
+						"display_url": "pic.twitter.com/15sXxZ9c6o",
+						"expanded_url": "https://twitter.com/Nickelodeon/status/1157705279187931136/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1024,
+								"h": 1024,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1024,
+								"h": 1024,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								1,
+								1
+							],
+							"duration_millis": 15057,
+							"variants": [
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/ext_tw_video/1157705210283864064/pu/pl/GFStJMMcJOISiqmI.m3u8?tag=10"
+								},
+								{
+									"bitrate": 432000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1157705210283864064/pu/vid/320x320/HaUr0mAZghmwg5so.mp4?tag=10"
+								},
+								{
+									"bitrate": 1280000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1157705210283864064/pu/vid/720x720/Wlgz3aEpkfzx7Dy9.mp4?tag=10"
+								},
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1157705210283864064/pu/vid/480x480/nnGn3s0CmFTm-nqi.mp4?tag=10"
+								}
+							]
+						},
+						"additional_media_info": {
+							"monetizable": false
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://www.spredfast.com/\" rel=\"nofollow\">Spredfast app</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 58309829,
+				"id_str": "58309829",
+				"name": "Nickelodeon",
+				"screen_name": "Nickelodeon",
+				"location": "USA",
+				"description": "The Official Twitter for Nickelodeon, USA!",
+				"url": "https://t.co/Lz9i6KW1cH",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/Lz9i6KW1cH",
+								"expanded_url": "http://www.nick.com",
+								"display_url": "nick.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 3882071,
+				"friends_count": 2655,
+				"listed_count": 3829,
+				"created_at": "Sun Jul 19 22:19:02 +0000 2009",
+				"favourites_count": 18176,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 50941,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": true,
+				"profile_background_color": "FA743E",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme4/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme4/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1135737831350050816/P9AOyUEm_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1135737831350050816/P9AOyUEm_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/58309829/1564755260",
+				"profile_link_color": "D1771E",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "F0F0F0",
+				"profile_text_color": "333333",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "regular"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2632,
+			"favorite_count": 11545,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Uh…I don’t know how I feel about this one… https://t.co/MVvAOhD1GY"
+	},
+	{
 		"created_at": "Mon Aug 12 00:17:54 +0000 2019",
 		"id": 1160706942257569800,
 		"id_str": "1160706942257569793",
