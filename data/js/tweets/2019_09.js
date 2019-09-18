eea150f5567d6ee9483480f41a6d2114638e985d
Zak Winnick <zakwinnick@protonmail.com>
2019-09-18 08:10:46 -0500

    Added 1 tweets

--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,6 +1,302 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Wed Sep 18 12:53:16 +0000 2019",
+		"id": 1174124906327330800,
+		"id_str": "1174305386796593153",
+		"full_text": "There’s a shortage of perfect movies in this world. It would be a pity to damage this one. https://t.co/5N8Q3P2e5G",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			90
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/5N8Q3P2e5G",
+					"expanded_url": "https://twitter.com/variety/status/1173954759071547392",
+					"display_url": "twitter.com/variety/status…",
+					"indices": [
+						91,
+						114
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 469706697,
+			"id_str": "469706697",
+			"name": "Cary Elwes",
+			"screen_name": "Cary_Elwes",
+			"location": "Los Angeles, CA",
+			"description": "Dad, Husband, Actor, Screenwriter, Author, Activist. Instagram: caryelwes",
+			"url": "https://t.co/yIsdO3zu8a",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/yIsdO3zu8a",
+							"expanded_url": "http://amzn.to/2sQr7BP",
+							"display_url": "amzn.to/2sQr7BP",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 89581,
+			"friends_count": 1673,
+			"listed_count": 589,
+			"created_at": "Fri Jan 20 22:07:57 +0000 2012",
+			"favourites_count": 2897,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 1391,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/913977162591604736/yY_8gA4l_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/913977162591604736/yY_8gA4l_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/469706697/1524091933",
+			"profile_link_color": "3B94D9",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1173954759071547400,
+		"quoted_status_id_str": "1173954759071547392",
+		"quoted_status_permalink": {
+			"url": "https://t.co/5N8Q3P2e5G",
+			"expanded": "https://twitter.com/variety/status/1173954759071547392",
+			"display": "twitter.com/variety/status…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Sep 17 13:40:00 +0000 2019",
+			"id": 1173954759071547400,
+			"id_str": "1173954759071547392",
+			"full_text": "Sony Pictures Entertainment CEO Tony Vinciquerra says that “very famous people whose names I won’t use” want to redo Norman Lear’s ‘The Princess Bride’ https://t.co/xGHdIxW2bf https://t.co/uvHjWzpj9t",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				175
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/xGHdIxW2bf",
+						"expanded_url": "http://bit.ly/30qHpQM",
+						"display_url": "bit.ly/30qHpQM",
+						"indices": [
+							152,
+							175
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1173950068472393700,
+						"id_str": "1173950068472393729",
+						"indices": [
+							176,
+							199
+						],
+						"media_url": "http://pbs.twimg.com/media/EEq1Xg9XsAEDdpj.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EEq1Xg9XsAEDdpj.jpg",
+						"url": "https://t.co/uvHjWzpj9t",
+						"display_url": "pic.twitter.com/uvHjWzpj9t",
+						"expanded_url": "https://twitter.com/Variety/status/1173954759071547392/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 382,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1000,
+								"h": 562,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1000,
+								"h": 562,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1173950068472393700,
+						"id_str": "1173950068472393729",
+						"indices": [
+							176,
+							199
+						],
+						"media_url": "http://pbs.twimg.com/media/EEq1Xg9XsAEDdpj.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EEq1Xg9XsAEDdpj.jpg",
+						"url": "https://t.co/uvHjWzpj9t",
+						"display_url": "pic.twitter.com/uvHjWzpj9t",
+						"expanded_url": "https://twitter.com/Variety/status/1173954759071547392/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 382,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1000,
+								"h": 562,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1000,
+								"h": 562,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 17525171,
+				"id_str": "17525171",
+				"name": "Variety",
+				"screen_name": "Variety",
+				"location": "Los Angeles, CA",
+				"description": "The business of entertainment.",
+				"url": "https://t.co/8QkvN6A5Nf",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/8QkvN6A5Nf",
+								"expanded_url": "http://www.variety.com",
+								"display_url": "variety.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 2309964,
+				"friends_count": 263038,
+				"listed_count": 18526,
+				"created_at": "Thu Nov 20 23:02:29 +0000 2008",
+				"favourites_count": 703,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 267484,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "FFFFFF",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1060236422500704257/jltNN1hQ_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1060236422500704257/jltNN1hQ_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/17525171/1568736049",
+				"profile_link_color": "60728A",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "FFFFFF",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 847,
+			"favorite_count": 3479,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 60122,
+		"favorite_count": 228438,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "There’s a shortage of perfect movies in this world. It would be a pity to damage this one. https://t.co/5N8Q3P2e5G"
+	},
+	{
 		"created_at": "Tue Sep 17 17:57:26 +0000 2019",
 		"id": 1174011455835795500,
 		"id_str": "1174019546476351488",
