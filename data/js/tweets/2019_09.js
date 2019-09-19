3eaa6f34cf43abbf82916ce105310350d3456030
Zak Winnick <zakwinnick@protonmail.com>
2019-09-19 13:10:46 -0500

    Added 1 tweets

--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,6 +1,111 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Thu Sep 19 17:39:31 +0000 2019",
+		"id": 1174736900990496800,
+		"id_str": "1174739813947322370",
+		"full_text": "Twenty five years ago today, Palm Computing announced the availability of the Graffiti “power writing technology” for handheld computers, including the Apple Newton.\n\nhttps://t.co/UO1CAVsP3U",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			190
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/UO1CAVsP3U",
+					"expanded_url": "http://www.grot.com/zoomer/zoomer-list/zoomer-list-3Q1994/0048.html",
+					"display_url": "grot.com/zoomer/zoomer-…",
+					"indices": [
+						167,
+						190
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://twitterrific.com/mac\" rel=\"nofollow\">Twitterrific for Mac</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14668722,
+			"id_str": "14668722",
+			"name": "NewtonTalk",
+			"screen_name": "newtontalk",
+			"location": "Canada",
+			"description": "Will the last person to leave the platform please turn off the backlight?",
+			"url": "http://t.co/uGBp5oZsbQ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/uGBp5oZsbQ",
+							"expanded_url": "http://newtontalk.net",
+							"display_url": "newtontalk.net",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 973,
+			"friends_count": 54,
+			"listed_count": 56,
+			"created_at": "Tue May 06 03:16:05 +0000 2008",
+			"favourites_count": 2610,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 7180,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EEEEEE",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/451385358052098048/BF9ITaXe_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/451385358052098048/BF9ITaXe_normal.png",
+			"profile_link_color": "FF9943",
+			"profile_sidebar_border_color": "112255",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "323232",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 7,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Twenty five years ago today, Palm Computing announced the availability of the Graffiti “power writing technology” for handheld computers, including the Apple Newton.\n\nhttps://t.co/UO1CAVsP3U"
+	},
+	{
 		"created_at": "Thu Sep 19 10:48:48 +0000 2019",
 		"id": 1174636451100840000,
 		"id_str": "1174636451100839941",
