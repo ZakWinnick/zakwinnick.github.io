340c09223db8b1d67f344578372799687d860e6d
Zak Winnick <zakwinnick@protonmail.com>
2019-09-20 20:10:46 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10389,
-	"created_at": "Fri Sep 20 17:10:46 +0000 2019",
+	"tweets": 10390,
+	"created_at": "Sat Sep 21 01:10:45 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,6 +1,324 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Sat Sep 21 00:29:42 +0000 2019",
+		"id": 1175113418543526000,
+		"id_str": "1175205424490004482",
+		"full_text": "Let's go with nope. https://t.co/6RhXUqcvJS",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			19
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/6RhXUqcvJS",
+					"expanded_url": "https://twitter.com/TrumpLasVegas/status/1175112544073109505",
+					"display_url": "twitter.com/TrumpLasVegas/…",
+					"indices": [
+						20,
+						43
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 514487309,
+			"id_str": "514487309",
+			"name": "Vital Vegas",
+			"screen_name": "VitalVegas",
+			"location": "Las Vegas, NV",
+			"description": "https://t.co/iNB8aPQlsS is your essential source for Las Vegas news, rumors, tips and WTF. #LasVegas",
+			"url": "https://t.co/qBgUfzLqfn",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/qBgUfzLqfn",
+							"expanded_url": "http://www.vitalvegas.com",
+							"display_url": "vitalvegas.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/iNB8aPQlsS",
+							"expanded_url": "http://VitalVegas.com",
+							"display_url": "VitalVegas.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 40818,
+			"friends_count": 11968,
+			"listed_count": 380,
+			"created_at": "Sun Mar 04 16:04:28 +0000 2012",
+			"favourites_count": 234549,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 53749,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/378800000477710502/0e173bc3f5353ff8861305b64f20b625_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/378800000477710502/0e173bc3f5353ff8861305b64f20b625_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/514487309/1510021234",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "893F0D",
+			"profile_text_color": "050404",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1175112544073109500,
+		"quoted_status_id_str": "1175112544073109505",
+		"quoted_status_permalink": {
+			"url": "https://t.co/6RhXUqcvJS",
+			"expanded": "https://twitter.com/TrumpLasVegas/status/1175112544073109505",
+			"display": "twitter.com/TrumpLasVegas/…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Sep 20 18:20:37 +0000 2019",
+			"id": 1175112544073109500,
+			"id_str": "1175112544073109505",
+			"full_text": "This is how we weekend ✅ With our new In-Suite Dining menu, you can enjoy breakfast, lunch, and dinner from within the comfort of your guestroom, bed, or even your jetted spa tub! How are you going to spend your weekend? #wewishyouwerehere #liveyourbestlife #trumplasvegas https://t.co/ej6sKbWDnG",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				272
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "wewishyouwerehere",
+						"indices": [
+							221,
+							239
+						]
+					},
+					{
+						"text": "liveyourbestlife",
+						"indices": [
+							240,
+							257
+						]
+					},
+					{
+						"text": "trumplasvegas",
+						"indices": [
+							258,
+							272
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1175112540168179700,
+						"id_str": "1175112540168179712",
+						"indices": [
+							273,
+							296
+						],
+						"media_url": "http://pbs.twimg.com/media/EE7WoRfU0AASgV1.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EE7WoRfU0AASgV1.jpg",
+						"url": "https://t.co/ej6sKbWDnG",
+						"display_url": "pic.twitter.com/ej6sKbWDnG",
+						"expanded_url": "https://twitter.com/TrumpLasVegas/status/1175112544073109505/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 750,
+								"h": 932,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 750,
+								"h": 932,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 547,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1175112540168179700,
+						"id_str": "1175112540168179712",
+						"indices": [
+							273,
+							296
+						],
+						"media_url": "http://pbs.twimg.com/media/EE7WoRfU0AASgV1.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EE7WoRfU0AASgV1.jpg",
+						"url": "https://t.co/ej6sKbWDnG",
+						"display_url": "pic.twitter.com/ej6sKbWDnG",
+						"expanded_url": "https://twitter.com/TrumpLasVegas/status/1175112544073109505/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 750,
+								"h": 932,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 750,
+								"h": 932,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 547,
+								"h": 680,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 82504071,
+				"id_str": "82504071",
+				"name": "Trump Vegas Hotel",
+				"screen_name": "TrumpLasVegas",
+				"location": "Las Vegas ",
+				"description": "The next generation of luxury hotel in Las Vegas.",
+				"url": "http://t.co/30YWwzTG3b",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/30YWwzTG3b",
+								"expanded_url": "http://www.trumplasvegashotel.com/",
+								"display_url": "trumplasvegashotel.com",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 133600,
+				"friends_count": 1246,
+				"listed_count": 1323,
+				"created_at": "Thu Oct 15 01:07:17 +0000 2009",
+				"favourites_count": 983,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 4522,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/694317826576748544/4I1x-_u7_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/694317826576748544/4I1x-_u7_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/82504071/1421960249",
+				"profile_link_color": "737373",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "F4ECC3",
+				"profile_text_color": "786B5F",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 5,
+			"favorite_count": 41,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 19,
+		"favorite_count": 148,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Let's go with nope. https://t.co/6RhXUqcvJS"
+	},
+	{
 		"created_at": "Fri Sep 20 16:46:13 +0000 2019",
 		"id": 1174774595800522800,
 		"id_str": "1175088786902007808",
