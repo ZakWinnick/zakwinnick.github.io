55b97caf1cc4ca93e942ef8ff2661ffefbc73b3f
Zak Winnick <zakwinnick@protonmail.com>
2019-09-30 21:10:47 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10406,
-	"created_at": "Tue Oct 01 00:10:46 +0000 2019",
+	"tweets": 10407,
+	"created_at": "Tue Oct 01 02:10:46 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_10.js
+++ data/js/tweets/2019_10.js
@@ -1,6 +1,254 @@
 Grailbird.data.tweets_2019_10 = 
 [
 	{
+		"created_at": "Tue Oct 01 01:22:32 +0000 2019",
+		"id": 1178842598942871600,
+		"id_str": "1178842598942871553",
+		"full_text": "This is heavy. https://t.co/C0pawYqoTQ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			14
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/C0pawYqoTQ",
+					"expanded_url": "https://twitter.com/NBCNightlyNews/status/1178832398940721152",
+					"display_url": "twitter.com/NBCNightlyNews…",
+					"indices": [
+						15,
+						38
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 9586552,
+			"id_str": "9586552",
+			"name": "Zak Winnick",
+			"screen_name": "ZakWinnick",
+			"location": "Nashville, TN",
+			"description": "Hotel Technology Guru | Electric Vehicle Enthusiast | Founder @SurfChaosMedia",
+			"url": "https://t.co/vGZZOYs8dO",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vGZZOYs8dO",
+							"expanded_url": "https://ZakWinnick.com",
+							"display_url": "ZakWinnick.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 56,
+			"friends_count": 195,
+			"listed_count": 18,
+			"created_at": "Sun Oct 21 22:28:31 +0000 2007",
+			"favourites_count": 164,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9307,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "3F5B66",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1177599412564123649/qbwiYp3N_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1177599412564123649/qbwiYp3N_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/9586552/1555366317",
+			"profile_link_color": "483D8B",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "858080",
+			"profile_text_color": "086660",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1178832398940721200,
+		"quoted_status_id_str": "1178832398940721152",
+		"quoted_status_permalink": {
+			"url": "https://t.co/C0pawYqoTQ",
+			"expanded": "https://twitter.com/NBCNightlyNews/status/1178832398940721152",
+			"display": "twitter.com/NBCNightlyNews…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Oct 01 00:42:00 +0000 2019",
+			"id": 1178832398940721200,
+			"id_str": "1178832398940721152",
+			"full_text": "California Gov. Gavin Newsom signed a first-in-the-nation bill Monday that clears the way for college players to be paid from endorsement deals: https://t.co/Pmspkjenue \n\n@Miguelnbc has the details now.",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				202
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "Miguelnbc",
+						"name": "Miguel Almaguer",
+						"id": 1220137028,
+						"id_str": "1220137028",
+						"indices": [
+							171,
+							181
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/Pmspkjenue",
+						"expanded_url": "https://nbcnews.to/2oGSbW7",
+						"display_url": "nbcnews.to/2oGSbW7",
+						"indices": [
+							145,
+							168
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 8839632,
+				"id_str": "8839632",
+				"name": "NBC Nightly News with Lester Holt",
+				"screen_name": "NBCNightlyNews",
+				"location": "New York",
+				"description": "Breaking news, in-depth reporting, context on news from around the world | Full episodes: https://t.co/xfi4YbHh0x | Newsletter: https://t.co/ACxkqB0LFh",
+				"url": "https://t.co/siVgviWIA1",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/siVgviWIA1",
+								"expanded_url": "http://NBCNightlyNews.com",
+								"display_url": "NBCNightlyNews.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/xfi4YbHh0x",
+								"expanded_url": "http://www.nbcnightlynews.com",
+								"display_url": "nbcnightlynews.com",
+								"indices": [
+									90,
+									113
+								]
+							},
+							{
+								"url": "https://t.co/ACxkqB0LFh",
+								"expanded_url": "https://nbcnews.to/2DFKQLR",
+								"display_url": "nbcnews.to/2DFKQLR",
+								"indices": [
+									128,
+									151
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 986464,
+				"friends_count": 479,
+				"listed_count": 11043,
+				"created_at": "Wed Sep 12 19:19:22 +0000 2007",
+				"favourites_count": 1753,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 139687,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/878311894418849792/jlk1k6MF_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/878311894418849792/jlk1k6MF_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/8839632/1561689902",
+				"profile_link_color": "124578",
+				"profile_sidebar_border_color": "DADADA",
+				"profile_sidebar_fill_color": "EEEEEE",
+				"profile_text_color": "000000",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 15,
+			"favorite_count": 40,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "This is heavy. https://t.co/C0pawYqoTQ"
+	},
+	{
 		"created_at": "Tue Oct 01 00:02:23 +0000 2019",
 		"id": 1178822431273869300,
 		"id_str": "1178822431273869314",
