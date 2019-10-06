2de8a8a0573b17343285f39c472af4c59c2447e3
Zak Winnick <zakwinnick@protonmail.com>
2019-10-06 16:10:46 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10412,
-	"created_at": "Sun Oct 06 03:10:45 +0000 2019",
+	"tweets": 10413,
+	"created_at": "Sun Oct 06 21:10:45 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_10.js
+++ data/js/tweets/2019_10.js
@@ -1,6 +1,279 @@
 Grailbird.data.tweets_2019_10 = 
 [
 	{
+		"created_at": "Sun Oct 06 20:43:20 +0000 2019",
+		"id": 1180946663357272000,
+		"id_str": "1180946663357272068",
+		"full_text": "Bonnaroo is going to be lit this year… https://t.co/FKVe9roj83",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			38
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/FKVe9roj83",
+					"expanded_url": "https://twitter.com/VengefulWalnut/status/1180943716443254784",
+					"display_url": "twitter.com/VengefulWalnut…",
+					"indices": [
+						39,
+						62
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 9586552,
+			"id_str": "9586552",
+			"name": "Zak Winnick",
+			"screen_name": "ZakWinnick",
+			"location": "Nashville, TN",
+			"description": "Hotel Technology Guru | Electric Vehicle Enthusiast | Founder @SurfChaosMedia",
+			"url": "https://t.co/vGZZOYs8dO",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vGZZOYs8dO",
+							"expanded_url": "https://ZakWinnick.com",
+							"display_url": "ZakWinnick.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 55,
+			"friends_count": 195,
+			"listed_count": 18,
+			"created_at": "Sun Oct 21 22:28:31 +0000 2007",
+			"favourites_count": 166,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9310,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "3F5B66",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1177599412564123649/qbwiYp3N_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1177599412564123649/qbwiYp3N_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/9586552/1555366317",
+			"profile_link_color": "483D8B",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "858080",
+			"profile_text_color": "086660",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1180943716443254800,
+		"quoted_status_id_str": "1180943716443254784",
+		"quoted_status_permalink": {
+			"url": "https://t.co/FKVe9roj83",
+			"expanded": "https://twitter.com/VengefulWalnut/status/1180943716443254784",
+			"display": "twitter.com/VengefulWalnut…"
+		},
+		"quoted_status": {
+			"created_at": "Sun Oct 06 20:31:37 +0000 2019",
+			"id": 1180943716443254800,
+			"id_str": "1180943716443254784",
+			"full_text": "This design fad needs to end... all I see is BNOR OJU https://t.co/qepCuifJOK",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				53
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1180943713712787500,
+						"id_str": "1180943713712787456",
+						"indices": [
+							54,
+							77
+						],
+						"media_url": "http://pbs.twimg.com/media/EGOODPuU0AAdyWH.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EGOODPuU0AAdyWH.jpg",
+						"url": "https://t.co/qepCuifJOK",
+						"display_url": "pic.twitter.com/qepCuifJOK",
+						"expanded_url": "https://twitter.com/VengefulWalnut/status/1180943716443254784/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 502,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 750,
+								"h": 554,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 750,
+								"h": 554,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1180943713712787500,
+						"id_str": "1180943713712787456",
+						"indices": [
+							54,
+							77
+						],
+						"media_url": "http://pbs.twimg.com/media/EGOODPuU0AAdyWH.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EGOODPuU0AAdyWH.jpg",
+						"url": "https://t.co/qepCuifJOK",
+						"display_url": "pic.twitter.com/qepCuifJOK",
+						"expanded_url": "https://twitter.com/VengefulWalnut/status/1180943716443254784/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 502,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 750,
+								"h": 554,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 750,
+								"h": 554,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 3009376464,
+				"id_str": "3009376464",
+				"name": "James Olivier",
+				"screen_name": "VengefulWalnut",
+				"location": "C-53",
+				"description": "not terrible writer person. has approximate knowledge of many things. possibly a velociraptor. pygmy ferret herder.",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 341,
+				"friends_count": 619,
+				"listed_count": 7,
+				"created_at": "Tue Feb 03 16:03:31 +0000 2015",
+				"favourites_count": 20108,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 22104,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1134003034093105153/Ys7y10QJ_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1134003034093105153/Ys7y10QJ_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/3009376464/1483719901",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 1,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Bonnaroo is going to be lit this year… https://t.co/FKVe9roj83"
+	},
+	{
 		"created_at": "Sun Oct 06 02:53:30 +0000 2019",
 		"id": 1180656429935595500,
 		"id_str": "1180677432807960576",
