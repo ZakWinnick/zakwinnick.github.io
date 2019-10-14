1acec9f0ba8ff2e58b5061a0c0881297f066bb6f
Zak Winnick <zakwinnick@protonmail.com>
2019-10-14 10:10:46 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10425,
-	"created_at": "Mon Oct 14 13:10:45 +0000 2019",
+	"tweets": 10426,
+	"created_at": "Mon Oct 14 15:10:45 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_10.js
+++ data/js/tweets/2019_10.js
@@ -1,6 +1,213 @@
 Grailbird.data.tweets_2019_10 = 
 [
 	{
+		"created_at": "Mon Oct 14 14:25:14 +0000 2019",
+		"id": 1183750614712500200,
+		"id_str": "1183750614712500225",
+		"full_text": "I dislike the whole changing your Twitter name thing to be clever, or to show that you’re at an event. \n\nKinda makes me wish Twitter didn’t allow you to have a longer profile name… https://t.co/0VVU3XTdNj",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			180
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/0VVU3XTdNj",
+					"expanded_url": "https://twitter.com/ismh/status/1183741073073553408",
+					"display_url": "twitter.com/ismh/status/11…",
+					"indices": [
+						181,
+						204
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 9586552,
+			"id_str": "9586552",
+			"name": "Zak Winnick",
+			"screen_name": "ZakWinnick",
+			"location": "Nashville, TN",
+			"description": "Hotel Technology Guru | Electric Vehicle Enthusiast | Drone Pilot",
+			"url": "https://t.co/vGZZOYJJCo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vGZZOYJJCo",
+							"expanded_url": "https://ZakWinnick.com",
+							"display_url": "ZakWinnick.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 55,
+			"friends_count": 194,
+			"listed_count": 18,
+			"created_at": "Sun Oct 21 22:28:31 +0000 2007",
+			"favourites_count": 169,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9321,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "3F5B66",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1177599412564123649/qbwiYp3N_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1177599412564123649/qbwiYp3N_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/9586552/1555366317",
+			"profile_link_color": "483D8B",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "858080",
+			"profile_text_color": "086660",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1183741073073553400,
+		"quoted_status_id_str": "1183741073073553408",
+		"quoted_status_permalink": {
+			"url": "https://t.co/0VVU3XTdNj",
+			"expanded": "https://twitter.com/ismh/status/1183741073073553408",
+			"display": "twitter.com/ismh/status/11…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Oct 14 13:47:19 +0000 2019",
+			"id": 1183741073073553400,
+			"id_str": "1183741073073553408",
+			"full_text": "I dislike the whole “spooky Twitter name” thing.",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				48
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1566661,
+				"id_str": "1566661",
+				"name": "Stephen Hackett",
+				"screen_name": "ismh",
+				"location": "Memphis, Tennessee",
+				"description": "Co-founder of the @_relayfm podcast network and the host of a few nerdy shows covering Apple and the space industry. Writer at @512px and @stjude supporter.",
+				"url": "https://t.co/7nn5wvZ2op",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/7nn5wvZ2op",
+								"expanded_url": "https://relay.fm",
+								"display_url": "relay.fm",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 22405,
+				"friends_count": 417,
+				"listed_count": 1209,
+				"created_at": "Tue Mar 20 00:37:22 +0000 2007",
+				"favourites_count": 21584,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 1458,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "1A1B1F",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1179371476207755264/399KYo7z_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1179371476207755264/399KYo7z_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1566661/1570019362",
+				"profile_link_color": "3B94D9",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "252429",
+				"profile_text_color": "666666",
+				"profile_use_background_image": false,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 63,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I dislike the whole changing your Twitter name thing to be clever, or to show that you’re at an event. \n\nKinda makes me wish Twitter didn’t allow you to have a longer profile name… https://t.co/0VVU3XTdNj"
+	},
+	{
 		"created_at": "Mon Oct 14 12:54:31 +0000 2019",
 		"id": 1183727787502461000,
 		"id_str": "1183727787502460928",
