8cc17b3b1b6ec1a6fe156048fc48dbbf463cae63
Zak Winnick <zakwinnick@protonmail.com>
2019-10-19 14:10:46 -0500

    Added 1 tweets

--- data/js/tweets/2019_10.js
+++ data/js/tweets/2019_10.js
@@ -1,6 +1,212 @@
 Grailbird.data.tweets_2019_10 = 
 [
 	{
+		"created_at": "Sat Oct 19 19:07:53 +0000 2019",
+		"id": 1185633684650647600,
+		"id_str": "1185633684650647552",
+		"full_text": "Only in Mountain View would something like this happen.  \n\nThe thieves probably drove off in their Model 3s… https://t.co/Fz8zyA4tak",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			108
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Fz8zyA4tak",
+					"expanded_url": "https://twitter.com/MerrittBaer/status/1184637149402021888",
+					"display_url": "twitter.com/MerrittBaer/st…",
+					"indices": [
+						109,
+						132
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 9586552,
+			"id_str": "9586552",
+			"name": "Zak Winnick",
+			"screen_name": "ZakWinnick",
+			"location": "Nashville, TN",
+			"description": "Hotel Technology Guru | Electric Vehicle Enthusiast | Action Cam Evangelist",
+			"url": "https://t.co/vGZZOYJJCo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vGZZOYJJCo",
+							"expanded_url": "https://ZakWinnick.com",
+							"display_url": "ZakWinnick.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 55,
+			"friends_count": 162,
+			"listed_count": 18,
+			"created_at": "Sun Oct 21 22:28:31 +0000 2007",
+			"favourites_count": 171,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9326,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "3F5B66",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1177599412564123649/qbwiYp3N_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1177599412564123649/qbwiYp3N_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/9586552/1555366317",
+			"profile_link_color": "483D8B",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "858080",
+			"profile_text_color": "086660",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1184637149402022000,
+		"quoted_status_id_str": "1184637149402021888",
+		"quoted_status_permalink": {
+			"url": "https://t.co/Fz8zyA4tak",
+			"expanded": "https://twitter.com/MerrittBaer/status/1184637149402021888",
+			"display": "twitter.com/MerrittBaer/st…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Oct 17 01:08:00 +0000 2019",
+			"id": 1184637149402022000,
+			"id_str": "1184637149402021888",
+			"full_text": "Colleagues had a car broken into and laptops stolen in downtown Mountain View last night while we were at dinner. We wondered how they knew to break into the hatchback when it is not see- through.\n\nThey turn on bluetooth scanners and follow the beacon to find electronics.",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				272
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 536586070,
+				"id_str": "536586070",
+				"name": "Merritt Baer",
+				"screen_name": "MerrittBaer",
+				"location": "Washington, DC",
+				"description": "Principal Security Architect at AWS. \n\nemerging tech, infosec, security as code, future of the internet.\n\namateur boxer. she/her.\n\nI work at AWS, tweets my own.",
+				"url": "https://t.co/JV8jouwbbQ",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/JV8jouwbbQ",
+								"expanded_url": "http://www.merrittrachelbaer.com",
+								"display_url": "merrittrachelbaer.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 3831,
+				"friends_count": 4903,
+				"listed_count": 180,
+				"created_at": "Sun Mar 25 19:40:13 +0000 2012",
+				"favourites_count": 22000,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 8131,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/786405944460713984/k6MdBuXe_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/786405944460713984/k6MdBuXe_normal.jpg",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1725,
+			"favorite_count": 3689,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Only in Mountain View would something like this happen.  \n\nThe thieves probably drove off in their Model 3s… https://t.co/Fz8zyA4tak"
+	},
+	{
 		"created_at": "Thu Oct 17 00:17:23 +0000 2019",
 		"id": 1184624410185076700,
 		"id_str": "1184624410185076737",
