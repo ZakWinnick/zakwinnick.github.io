79511ae3bca7b3fc28458376f58729380ec8f7c3
Zak Winnick <zakwinnick@protonmail.com>
2019-10-22 16:10:48 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10433,
-	"created_at": "Tue Oct 22 18:10:46 +0000 2019",
+	"tweets": 10434,
+	"created_at": "Tue Oct 22 21:10:46 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_10.js
+++ data/js/tweets/2019_10.js
@@ -1,6 +1,200 @@
 Grailbird.data.tweets_2019_10 = 
 [
 	{
+		"created_at": "Tue Oct 22 20:53:15 +0000 2019",
+		"id": 1186747365526528000,
+		"id_str": "1186747365526528007",
+		"full_text": "Sure, but let’s remember what they did to Robert Mueller, who was equally as decorated… https://t.co/c1W1VMNIEC",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			87
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/c1W1VMNIEC",
+					"expanded_url": "https://twitter.com/WillieGeist/status/1186744745613287424",
+					"display_url": "twitter.com/WillieGeist/st…",
+					"indices": [
+						88,
+						111
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 9586552,
+			"id_str": "9586552",
+			"name": "Zak Winnick",
+			"screen_name": "ZakWinnick",
+			"location": "Nashville, TN",
+			"description": "Hotel Technology Guru | Electric Vehicle Enthusiast | Action Cam Evangelist",
+			"url": "https://t.co/vGZZOYJJCo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vGZZOYJJCo",
+							"expanded_url": "https://ZakWinnick.com",
+							"display_url": "ZakWinnick.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 55,
+			"friends_count": 161,
+			"listed_count": 18,
+			"created_at": "Sun Oct 21 22:28:31 +0000 2007",
+			"favourites_count": 171,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9327,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "3F5B66",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1177599412564123649/qbwiYp3N_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1177599412564123649/qbwiYp3N_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/9586552/1555366317",
+			"profile_link_color": "483D8B",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "858080",
+			"profile_text_color": "086660",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1186744745613287400,
+		"quoted_status_id_str": "1186744745613287424",
+		"quoted_status_permalink": {
+			"url": "https://t.co/c1W1VMNIEC",
+			"expanded": "https://twitter.com/WillieGeist/status/1186744745613287424",
+			"display": "twitter.com/WillieGeist/st…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Oct 22 20:42:50 +0000 2019",
+			"id": 1186744745613287400,
+			"id_str": "1186744745613287424",
+			"full_text": "When the predictable accusations come about Taylor being a deep state traitor, consider that he graduated from West Point, served with the 101st Airborne in Vietnam, served in Jerusalem, Kabul, and Baghdad under Democrats and Republicans, and was hired again by Secretary Pompeo.",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				279
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": 1186744743650320400,
+			"in_reply_to_status_id_str": "1186744743650320384",
+			"in_reply_to_user_id": 17291393,
+			"in_reply_to_user_id_str": "17291393",
+			"in_reply_to_screen_name": "WillieGeist",
+			"user": {
+				"id": 17291393,
+				"id_str": "17291393",
+				"name": "Willie Geist",
+				"screen_name": "WillieGeist",
+				"location": "New York",
+				"description": "Host @NBC #SundayTODAY, Co-Host @Morning_Joe, “Sunday Sitdown” Podcast, @TODAYShow, Producer credit on Lucie & George Geist with EP @ChristinaGeist",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 830835,
+				"friends_count": 1716,
+				"listed_count": 3863,
+				"created_at": "Mon Nov 10 18:10:01 +0000 2008",
+				"favourites_count": 13357,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 18468,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F37125",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/779876086775767040/pArtXipF_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/779876086775767040/pArtXipF_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/17291393/1512361445",
+				"profile_link_color": "F37125",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "FFFCF0",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 190,
+			"favorite_count": 593,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Sure, but let’s remember what they did to Robert Mueller, who was equally as decorated… https://t.co/c1W1VMNIEC"
+	},
+	{
 		"created_at": "Tue Oct 22 17:32:05 +0000 2019",
 		"id": 1186696739656536000,
 		"id_str": "1186696739656536064",
