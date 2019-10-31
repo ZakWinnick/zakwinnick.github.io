ae76549de8fc0bd362a286dbe22c29abfe139d80
Zak Winnick <zakwinnick@protonmail.com>
2019-10-31 06:10:47 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10446,
-	"created_at": "Thu Oct 31 00:18:43 +0000 2019",
+	"tweets": 10447,
+	"created_at": "Thu Oct 31 11:10:46 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_10.js
+++ data/js/tweets/2019_10.js
@@ -1,6 +1,210 @@
 Grailbird.data.tweets_2019_10 = 
 [
 	{
+		"created_at": "Thu Oct 31 10:24:04 +0000 2019",
+		"id": 1189673320230326300,
+		"id_str": "1189850519226986497",
+		"full_text": "If things seem a little more dystopian than usual, it’s probably because BLADE RUNNER happens in two days. https://t.co/UtQkIsfyRv",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			106
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1189673316497449000,
+					"id_str": "1189673316497448961",
+					"indices": [
+						107,
+						130
+					],
+					"media_url": "http://pbs.twimg.com/media/EIKRlASXYAEMjnO.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EIKRlASXYAEMjnO.jpg",
+					"url": "https://t.co/UtQkIsfyRv",
+					"display_url": "pic.twitter.com/UtQkIsfyRv",
+					"expanded_url": "https://twitter.com/mcnees/status/1189673320230326275/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 259,
+							"h": 194,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 259,
+							"h": 194,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 259,
+							"h": 194,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"orig": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1189673316497449000,
+					"id_str": "1189673316497448961",
+					"indices": [
+						107,
+						130
+					],
+					"media_url": "http://pbs.twimg.com/media/EIKRlASXYAEMjnO.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EIKRlASXYAEMjnO.jpg",
+					"url": "https://t.co/UtQkIsfyRv",
+					"display_url": "pic.twitter.com/UtQkIsfyRv",
+					"expanded_url": "https://twitter.com/mcnees/status/1189673320230326275/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 259,
+							"h": 194,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 259,
+							"h": 194,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 259,
+							"h": 194,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"orig": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15127834,
+			"id_str": "15127834",
+			"name": "Grave Robbert McNees",
+			"screen_name": "mcnees",
+			"location": "Chicago, z=0",
+			"description": "Professor, physicist, sci comm. Black holes, quantum gravity, cosmology. Rocky Top. Tar Heel. RT = spooky action at a distance. Opinions are mine alone. He/him.",
+			"url": "https://t.co/lkEoQ2ZU7c",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/lkEoQ2ZU7c",
+							"expanded_url": "http://jacobi.luc.edu",
+							"display_url": "jacobi.luc.edu",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 16008,
+			"friends_count": 1057,
+			"listed_count": 550,
+			"created_at": "Sun Jun 15 20:49:47 +0000 2008",
+			"favourites_count": 64269,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 87492,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1121051529140744193/SgL-wCOe_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1121051529140744193/SgL-wCOe_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15127834/1544197516",
+			"profile_link_color": "56A0D3",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": false,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3523,
+		"favorite_count": 8387,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "If things seem a little more dystopian than usual, it’s probably because BLADE RUNNER happens in two days. https://t.co/UtQkIsfyRv"
+	},
+	{
 		"created_at": "Thu Oct 31 00:12:16 +0000 2019",
 		"id": 1189694774862925800,
 		"id_str": "1189696552421527552",
