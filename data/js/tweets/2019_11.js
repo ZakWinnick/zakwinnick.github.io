e357859f81ac92c2f6de3f9ef95826b3dcd3de27
Zak Winnick <zakwinnick@protonmail.com>
2019-11-09 14:10:46 -0600

    Added 1 tweets

--- data/js/tweets/2019_11.js
+++ data/js/tweets/2019_11.js
@@ -1,6 +1,333 @@
 Grailbird.data.tweets_2019_11 = 
 [
 	{
+		"created_at": "Sat Nov 09 19:40:37 +0000 2019",
+		"id": 959501972255772700,
+		"id_str": "1193252070398320645",
+		"full_text": "I found out I can put an ad on the screen in the elevator at work for $15. https://t.co/cdP3Kf3Y76",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			74
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 959501949975646200,
+					"id_str": "959501949975646208",
+					"indices": [
+						75,
+						98
+					],
+					"media_url": "http://pbs.twimg.com/media/DVDV8JmX0AA5x_I.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DVDV8JmX0AA5x_I.jpg",
+					"url": "https://t.co/cdP3Kf3Y76",
+					"display_url": "pic.twitter.com/cdP3Kf3Y76",
+					"expanded_url": "https://twitter.com/mattl/status/959501972255772675/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 408,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 896,
+							"h": 538,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 896,
+							"h": 538,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": [
+								{
+									"x": 94,
+									"y": 295,
+									"h": 57,
+									"w": 57
+								},
+								{
+									"x": 146,
+									"y": 161,
+									"h": 90,
+									"w": 90
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 124,
+									"y": 390,
+									"h": 76,
+									"w": 76
+								},
+								{
+									"x": 193,
+									"y": 213,
+									"h": 119,
+									"w": 119
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 124,
+									"y": 390,
+									"h": 76,
+									"w": 76
+								},
+								{
+									"x": 193,
+									"y": 213,
+									"h": 119,
+									"w": 119
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 124,
+									"y": 390,
+									"h": 76,
+									"w": 76
+								},
+								{
+									"x": 193,
+									"y": 213,
+									"h": 119,
+									"w": 119
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 959501949975646200,
+					"id_str": "959501949975646208",
+					"indices": [
+						75,
+						98
+					],
+					"media_url": "http://pbs.twimg.com/media/DVDV8JmX0AA5x_I.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DVDV8JmX0AA5x_I.jpg",
+					"url": "https://t.co/cdP3Kf3Y76",
+					"display_url": "pic.twitter.com/cdP3Kf3Y76",
+					"expanded_url": "https://twitter.com/mattl/status/959501972255772675/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 408,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 896,
+							"h": 538,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 896,
+							"h": 538,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": [
+								{
+									"x": 94,
+									"y": 295,
+									"h": 57,
+									"w": 57
+								},
+								{
+									"x": 146,
+									"y": 161,
+									"h": 90,
+									"w": 90
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 124,
+									"y": 390,
+									"h": 76,
+									"w": 76
+								},
+								{
+									"x": 193,
+									"y": 213,
+									"h": 119,
+									"w": 119
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 124,
+									"y": 390,
+									"h": 76,
+									"w": 76
+								},
+								{
+									"x": 193,
+									"y": 213,
+									"h": 119,
+									"w": 119
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 124,
+									"y": 390,
+									"h": 76,
+									"w": 76
+								},
+								{
+									"x": 193,
+									"y": 213,
+									"h": 119,
+									"w": 119
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 661723,
+			"id_str": "661723",
+			"name": "Matt Lee",
+			"screen_name": "mattl",
+			"location": "Boston, MA",
+			"description": "Public figure/idiot. British in USA. He/him. Artist/writer/director. Free software/culture/society. Me: https://t.co/OXD3oxfR3j Uses: https://t.co/jUNhsfUHx0",
+			"url": "https://t.co/aREDu5Lc7O",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/aREDu5Lc7O",
+							"expanded_url": "https://mat.tl",
+							"display_url": "mat.tl",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/OXD3oxfR3j",
+							"expanded_url": "http://bit.ly/2NylkOB",
+							"display_url": "bit.ly/2NylkOB",
+							"indices": [
+								104,
+								127
+							]
+						},
+						{
+							"url": "https://t.co/jUNhsfUHx0",
+							"expanded_url": "http://bit.ly/2kXBsc2",
+							"display_url": "bit.ly/2kXBsc2",
+							"indices": [
+								134,
+								157
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 6144,
+			"friends_count": 1154,
+			"listed_count": 100,
+			"created_at": "Thu Jan 18 21:05:24 +0000 2007",
+			"favourites_count": 38658,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 41136,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "666666",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/756219387749675008/by-ZdOc1_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/756219387749675008/by-ZdOc1_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/661723/1446576223",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": false,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3170,
+		"favorite_count": 10652,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I found out I can put an ad on the screen in the elevator at work for $15. https://t.co/cdP3Kf3Y76"
+	},
+	{
 		"created_at": "Wed Nov 06 13:21:03 +0000 2019",
 		"id": 1192029702744969200,
 		"id_str": "1192069386225553408",
