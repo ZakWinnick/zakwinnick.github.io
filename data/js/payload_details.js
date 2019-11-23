cda2ce7110e6afea167deafab61e40db1ceacaa4
Zak Winnick <zakwinnick@protonmail.com>
2019-11-22 20:10:47 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10467,
-	"created_at": "Fri Nov 22 14:10:46 +0000 2019",
+	"tweets": 10468,
+	"created_at": "Sat Nov 23 02:10:46 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_11.js
+++ data/js/tweets/2019_11.js
@@ -1,6 +1,140 @@
 Grailbird.data.tweets_2019_11 = 
 [
 	{
+		"created_at": "Sat Nov 23 02:08:40 +0000 2019",
+		"id": 1197971565658431500,
+		"id_str": "1198060769096740865",
+		"full_text": "56 Years ago today #JFKAssassination Find out more on #MysteryWire @g_knapp https://t.co/eTil6Qq0wU",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			99
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "JFKAssassination",
+					"indices": [
+						19,
+						36
+					]
+				},
+				{
+					"text": "MysteryWire",
+					"indices": [
+						54,
+						66
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "g_knapp",
+					"name": "George Knapp",
+					"id": 50117326,
+					"id_str": "50117326",
+					"indices": [
+						67,
+						75
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/eTil6Qq0wU",
+					"expanded_url": "https://www.mysterywire.com/mysteries/jfk-theories-persist-years-after-warren-commissions-missteps/",
+					"display_url": "mysterywire.com/mysteries/jfk-…",
+					"indices": [
+						76,
+						99
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1171217131108589600,
+			"id_str": "1171217131108589568",
+			"name": "Mystery Wire",
+			"screen_name": "MysteryWire",
+			"location": "Las Vegas, NV",
+			"description": "Your source for journalistically vetted stories on the unusual and unknown. George Knapp, 8 News Now, Las Vegas reports #MysteryWire",
+			"url": "https://t.co/6q68Mn7c3o",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/6q68Mn7c3o",
+							"expanded_url": "http://www.mysterywire.com",
+							"display_url": "mysterywire.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 534,
+			"friends_count": 28,
+			"listed_count": 4,
+			"created_at": "Tue Sep 10 00:21:54 +0000 2019",
+			"favourites_count": 76,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 75,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1171217341956255751/8qOvIQqS_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1171217341956255751/8qOvIQqS_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1171217131108589568/1573066208",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6,
+		"favorite_count": 13,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "56 Years ago today #JFKAssassination Find out more on #MysteryWire @g_knapp https://t.co/eTil6Qq0wU"
+	},
+	{
 		"created_at": "Fri Nov 22 14:04:17 +0000 2019",
 		"id": 1197740385994297300,
 		"id_str": "1197878472082952193",
