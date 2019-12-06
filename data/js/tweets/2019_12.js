eabd7cd2fa664e859f34b0e912961864d5345618
Zak Winnick <zakwinnick@protonmail.com>
2019-12-06 08:10:46 -0600

    Added 1 tweets

--- data/js/tweets/2019_12.js
+++ data/js/tweets/2019_12.js
@@ -1,6 +1,457 @@
 Grailbird.data.tweets_2019_12 = 
 [
 	{
+		"created_at": "Fri Dec 06 13:48:44 +0000 2019",
+		"id": 1202947987011461000,
+		"id_str": "1202947987011461125",
+		"full_text": "Looks like the one guy that works on this app had some free time... https://t.co/DcR0czc1r4",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			67
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/DcR0czc1r4",
+					"expanded_url": "https://twitter.com/macstoriesnet/status/1202942118727213058",
+					"display_url": "twitter.com/macstoriesnet/…",
+					"indices": [
+						68,
+						91
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 9586552,
+			"id_str": "9586552",
+			"name": "Zak Winnick",
+			"screen_name": "ZakWinnick",
+			"location": "Nashville, TN",
+			"description": "Hotel Technology Guru | Electric Vehicle Enthusiast | Action Cam Evangelist",
+			"url": "https://t.co/vGZZOYJJCo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vGZZOYJJCo",
+							"expanded_url": "https://ZakWinnick.com",
+							"display_url": "ZakWinnick.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 56,
+			"friends_count": 176,
+			"listed_count": 18,
+			"created_at": "Sun Oct 21 22:28:31 +0000 2007",
+			"favourites_count": 172,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9353,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "3F5B66",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1198692047584145413/ylCgOUxv_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1198692047584145413/ylCgOUxv_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/9586552/1572213500",
+			"profile_link_color": "483D8B",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "858080",
+			"profile_text_color": "086660",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": false,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1202942118727213000,
+		"quoted_status_id_str": "1202942118727213058",
+		"quoted_status_permalink": {
+			"url": "https://t.co/DcR0czc1r4",
+			"expanded": "https://twitter.com/macstoriesnet/status/1202942118727213058",
+			"display": "twitter.com/macstoriesnet/…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Dec 06 13:25:25 +0000 2019",
+			"id": 1202942118727213000,
+			"id_str": "1202942118727213058",
+			"full_text": "Hands-On with Clips 2.1: Memoji and Animoji Support, Plus New Sticker Face Tracking and More https://t.co/0kalMfEnpb https://t.co/jmcQVDYr3i",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				116
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/0kalMfEnpb",
+						"expanded_url": "https://www.macstories.net/news/hands-on-with-clips-21-memoji-and-animoji-support-plus-new-sticker-face-tracking-and-more/",
+						"display_url": "macstories.net/news/hands-on-…",
+						"indices": [
+							93,
+							116
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1202942116462301200,
+						"id_str": "1202942116462301184",
+						"indices": [
+							117,
+							140
+						],
+						"media_url": "http://pbs.twimg.com/media/ELG1e0EXkAAfVCV.png",
+						"media_url_https": "https://pbs.twimg.com/media/ELG1e0EXkAAfVCV.png",
+						"url": "https://t.co/jmcQVDYr3i",
+						"display_url": "pic.twitter.com/jmcQVDYr3i",
+						"expanded_url": "https://twitter.com/macstoriesnet/status/1202942118727213058/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 680,
+								"h": 434,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1024,
+								"h": 654,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1024,
+								"h": 654,
+								"resize": "fit"
+							}
+						},
+						"features": {
+							"small": {
+								"faces": [
+									{
+										"x": 85,
+										"y": 304,
+										"h": 51,
+										"w": 51
+									},
+									{
+										"x": 9,
+										"y": 56,
+										"h": 156,
+										"w": 156
+									}
+								]
+							},
+							"medium": {
+								"faces": [
+									{
+										"x": 128,
+										"y": 458,
+										"h": 77,
+										"w": 77
+									},
+									{
+										"x": 14,
+										"y": 85,
+										"h": 236,
+										"w": 236
+									}
+								]
+							},
+							"orig": {
+								"faces": [
+									{
+										"x": 128,
+										"y": 458,
+										"h": 77,
+										"w": 77
+									},
+									{
+										"x": 14,
+										"y": 85,
+										"h": 236,
+										"w": 236
+									}
+								]
+							},
+							"large": {
+								"faces": [
+									{
+										"x": 128,
+										"y": 458,
+										"h": 77,
+										"w": 77
+									},
+									{
+										"x": 14,
+										"y": 85,
+										"h": 236,
+										"w": 236
+									}
+								]
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1202942116462301200,
+						"id_str": "1202942116462301184",
+						"indices": [
+							117,
+							140
+						],
+						"media_url": "http://pbs.twimg.com/media/ELG1e0EXkAAfVCV.png",
+						"media_url_https": "https://pbs.twimg.com/media/ELG1e0EXkAAfVCV.png",
+						"url": "https://t.co/jmcQVDYr3i",
+						"display_url": "pic.twitter.com/jmcQVDYr3i",
+						"expanded_url": "https://twitter.com/macstoriesnet/status/1202942118727213058/photo/1",
+						"type": "photo",
+						"sizes": {
+							"small": {
+								"w": 680,
+								"h": 434,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1024,
+								"h": 654,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1024,
+								"h": 654,
+								"resize": "fit"
+							}
+						},
+						"features": {
+							"small": {
+								"faces": [
+									{
+										"x": 85,
+										"y": 304,
+										"h": 51,
+										"w": 51
+									},
+									{
+										"x": 9,
+										"y": 56,
+										"h": 156,
+										"w": 156
+									}
+								]
+							},
+							"medium": {
+								"faces": [
+									{
+										"x": 128,
+										"y": 458,
+										"h": 77,
+										"w": 77
+									},
+									{
+										"x": 14,
+										"y": 85,
+										"h": 236,
+										"w": 236
+									}
+								]
+							},
+							"orig": {
+								"faces": [
+									{
+										"x": 128,
+										"y": 458,
+										"h": 77,
+										"w": 77
+									},
+									{
+										"x": 14,
+										"y": 85,
+										"h": 236,
+										"w": 236
+									}
+								]
+							},
+							"large": {
+								"faces": [
+									{
+										"x": 128,
+										"y": 458,
+										"h": 77,
+										"w": 77
+									},
+									{
+										"x": 14,
+										"y": 85,
+										"h": 236,
+										"w": 236
+									}
+								]
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://publicize.wp.com/\" rel=\"nofollow\">WordPress.com</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 87077721,
+				"id_str": "87077721",
+				"name": "MacStories",
+				"screen_name": "macstoriesnet",
+				"location": "",
+				"description": "Apple news, app reviews, and stories. By @viticci and friends.  Advertise: https://t.co/bJlL8NAotW  Want more? Join @ClubMacStories: https://t.co/FMY97egcPc",
+				"url": "https://t.co/rz6ZEw9ok7",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/rz6ZEw9ok7",
+								"expanded_url": "http://www.macstories.net/",
+								"display_url": "macstories.net",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/bJlL8NAotW",
+								"expanded_url": "http://macstories.net/advertise",
+								"display_url": "macstories.net/advertise",
+								"indices": [
+									75,
+									98
+								]
+							},
+							{
+								"url": "https://t.co/FMY97egcPc",
+								"expanded_url": "http://club.macstories.net",
+								"display_url": "club.macstories.net",
+								"indices": [
+									133,
+									156
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 46554,
+				"friends_count": 9,
+				"listed_count": 3144,
+				"created_at": "Tue Nov 03 01:16:41 +0000 2009",
+				"favourites_count": 18,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 17711,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "EEEEEE",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/692115446456258560/HIjZquav_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/692115446456258560/HIjZquav_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/87077721/1398257369",
+				"profile_link_color": "545859",
+				"profile_sidebar_border_color": "5B5C5C",
+				"profile_sidebar_fill_color": "CDD0D1",
+				"profile_text_color": "333333",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"can_media_tag": false,
+				"followed_by": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 0,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Looks like the one guy that works on this app had some free time... https://t.co/DcR0czc1r4"
+	},
+	{
 		"created_at": "Fri Dec 06 01:13:01 +0000 2019",
 		"id": 1202757803963830300,
 		"id_str": "1202757803963830272",
