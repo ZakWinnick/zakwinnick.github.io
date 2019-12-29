98b460ebaa983f0b193d16f1ba0329330e76639a
Zak Winnick <zakwinnick@protonmail.com>
2019-12-29 06:10:46 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10498,
-	"created_at": "Sun Dec 29 01:10:46 +0000 2019",
+	"tweets": 10499,
+	"created_at": "Sun Dec 29 12:10:46 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_12.js
+++ data/js/tweets/2019_12.js
@@ -1,6 +1,210 @@
 Grailbird.data.tweets_2019_12 = 
 [
 	{
+		"created_at": "Sun Dec 29 11:24:00 +0000 2019",
+		"id": 1210920316131848200,
+		"id_str": "1211246484940820480",
+		"full_text": "Huh. News you can use. https://t.co/RRSWeq66dc",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			22
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1210920310154940400,
+					"id_str": "1210920310154940416",
+					"indices": [
+						23,
+						46
+					],
+					"media_url": "http://pbs.twimg.com/media/EM4NmzoXUAAE1Ut.png",
+					"media_url_https": "https://pbs.twimg.com/media/EM4NmzoXUAAE1Ut.png",
+					"url": "https://t.co/RRSWeq66dc",
+					"display_url": "pic.twitter.com/RRSWeq66dc",
+					"expanded_url": "https://twitter.com/Doctrine_Man/status/1210920316131848195/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 746,
+							"h": 480,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 438,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 746,
+							"h": 480,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					},
+					"features": {
+						"orig": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1210920310154940400,
+					"id_str": "1210920310154940416",
+					"indices": [
+						23,
+						46
+					],
+					"media_url": "http://pbs.twimg.com/media/EM4NmzoXUAAE1Ut.png",
+					"media_url_https": "https://pbs.twimg.com/media/EM4NmzoXUAAE1Ut.png",
+					"url": "https://t.co/RRSWeq66dc",
+					"display_url": "pic.twitter.com/RRSWeq66dc",
+					"expanded_url": "https://twitter.com/Doctrine_Man/status/1210920316131848195/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 746,
+							"h": 480,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 438,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 746,
+							"h": 480,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					},
+					"features": {
+						"orig": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 215099628,
+			"id_str": "215099628",
+			"name": "Doctrine Man",
+			"screen_name": "Doctrine_Man",
+			"location": "Lawrence, KS",
+			"description": "Jedi Knight, Senior Fellow at Modern @WarInstitute, Co-Founder/Board Member of @MilWritersGuild, Co-Founder of @DivergentOption. All-around nice guy.",
+			"url": "http://t.co/FVeWeHi6LH",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/FVeWeHi6LH",
+							"expanded_url": "http://www.facebook.com/doctrineman",
+							"display_url": "facebook.com/doctrineman",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 23861,
+			"friends_count": 1463,
+			"listed_count": 483,
+			"created_at": "Sat Nov 13 00:26:31 +0000 2010",
+			"favourites_count": 60431,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 101835,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1065247285976518659/aoWRz92e_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1065247285976518659/aoWRz92e_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/215099628/1488847189",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 59,
+		"favorite_count": 180,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Huh. News you can use. https://t.co/RRSWeq66dc"
+	},
+	{
 		"created_at": "Sun Dec 29 01:07:54 +0000 2019",
 		"id": 1211090328813326300,
 		"id_str": "1211091438710218752",
