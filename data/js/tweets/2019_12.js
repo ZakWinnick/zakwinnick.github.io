9dd8432bd3be81983bc4fdb8d0e7bc41794d3c52
Zak Winnick <zakwinnick@protonmail.com>
2019-12-29 17:10:46 -0600

    Added 1 tweets

--- data/js/tweets/2019_12.js
+++ data/js/tweets/2019_12.js
@@ -1,6 +1,235 @@
 Grailbird.data.tweets_2019_12 = 
 [
 	{
+		"created_at": "Sun Dec 29 23:04:18 +0000 2019",
+		"id": 1211422721403428900,
+		"id_str": "1211422721403428870",
+		"full_text": "This is truth for any Amy Adams movie... https://t.co/Bql1DfCpCb",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			40
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Bql1DfCpCb",
+					"expanded_url": "https://twitter.com/wesisrad/status/1211364083859017728",
+					"display_url": "twitter.com/wesisrad/statu…",
+					"indices": [
+						41,
+						64
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 9586552,
+			"id_str": "9586552",
+			"name": "Zak Winnick",
+			"screen_name": "ZakWinnick",
+			"location": "Nashville, TN",
+			"description": "Hotel Technology Guru | Electric Vehicle Enthusiast | Action Cam Evangelist",
+			"url": "https://t.co/vGZZOYJJCo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vGZZOYJJCo",
+							"expanded_url": "https://ZakWinnick.com",
+							"display_url": "ZakWinnick.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 56,
+			"friends_count": 177,
+			"listed_count": 18,
+			"created_at": "Sun Oct 21 22:28:31 +0000 2007",
+			"favourites_count": 175,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9370,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "3F5B66",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1210981468631191554/C5CAddRn_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1210981468631191554/C5CAddRn_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/9586552/1572213500",
+			"profile_link_color": "483D8B",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "858080",
+			"profile_text_color": "086660",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": false,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1211364083859017700,
+		"quoted_status_id_str": "1211364083859017728",
+		"quoted_status_permalink": {
+			"url": "https://t.co/Bql1DfCpCb",
+			"expanded": "https://twitter.com/wesisrad/status/1211364083859017728",
+			"display": "twitter.com/wesisrad/statu…"
+		},
+		"quoted_status": {
+			"created_at": "Sun Dec 29 19:11:18 +0000 2019",
+			"id": 1211364083859017700,
+			"id_str": "1211364083859017728",
+			"full_text": "the muppets (2011) keep amy adams am i doing this right https://t.co/5D2gK6HZkv",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				55
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/5D2gK6HZkv",
+						"expanded_url": "https://twitter.com/BeerBottleBlond/status/1210653864115998720",
+						"display_url": "twitter.com/BeerBottleBlon…",
+						"indices": [
+							56,
+							79
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 26287143,
+				"id_str": "26287143",
+				"name": "wes",
+				"screen_name": "wesisrad",
+				"location": "",
+				"description": "the agony and the irony, they’re killing me [producer/editor @ disney yellow shoes | views are my own]",
+				"url": "https://t.co/eB8BPe6MAU",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/eB8BPe6MAU",
+								"expanded_url": "http://weslag.com",
+								"display_url": "weslag.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 686,
+				"friends_count": 388,
+				"listed_count": 21,
+				"created_at": "Tue Mar 24 18:08:00 +0000 2009",
+				"favourites_count": 5808,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 4118,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "022330",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1207116526014234624/Qj7UfY7N_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1207116526014234624/Qj7UfY7N_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/26287143/1398214463",
+				"profile_link_color": "44616B",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "C0DFEC",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"can_media_tag": false,
+				"followed_by": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": true,
+			"quoted_status_id": 1210653864115998700,
+			"quoted_status_id_str": "1210653864115998720",
+			"quoted_status_permalink": {
+				"url": "https://t.co/5D2gK6HZkv",
+				"expanded": "https://twitter.com/BeerBottleBlond/status/1210653864115998720",
+				"display": "twitter.com/BeerBottleBlon…"
+			},
+			"retweet_count": 0,
+			"favorite_count": 14,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "This is truth for any Amy Adams movie... https://t.co/Bql1DfCpCb"
+	},
+	{
 		"created_at": "Sun Dec 29 11:24:00 +0000 2019",
 		"id": 1210920316131848200,
 		"id_str": "1211246484940820480",
