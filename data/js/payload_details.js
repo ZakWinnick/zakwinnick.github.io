35bd271fbbe98afe2c2f76c7dd8473199a777856
Zak Winnick <zakwinnick@protonmail.com>
2020-02-04 12:10:47 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10537,
-	"created_at": "Tue Feb 04 15:10:46 +0000 2020",
+	"tweets": 10538,
+	"created_at": "Tue Feb 04 18:10:46 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,329 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Tue Feb 04 18:08:17 +0000 2020",
+		"id": 1224756576881447000,
+		"id_str": "1224756576881446912",
+		"full_text": "The excitement of the coyote…❤️ https://t.co/VUuVBa1Gmp",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			31
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/VUuVBa1Gmp",
+					"expanded_url": "https://twitter.com/PeccaryNotPig/status/1224515892282740737",
+					"display_url": "twitter.com/PeccaryNotPig/…",
+					"indices": [
+						32,
+						55
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 9586552,
+			"id_str": "9586552",
+			"name": "Zak Winnick",
+			"screen_name": "ZakWinnick",
+			"location": "Nashville, TN",
+			"description": "Hotel Technology Guru | Electric Vehicle Enthusiast | Action Cam Evangelist",
+			"url": "https://t.co/vGZZOYJJCo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vGZZOYJJCo",
+							"expanded_url": "https://ZakWinnick.com",
+							"display_url": "ZakWinnick.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 58,
+			"friends_count": 185,
+			"listed_count": 18,
+			"created_at": "Sun Oct 21 22:28:31 +0000 2007",
+			"favourites_count": 144,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9413,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "3F5B66",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1210981468631191554/C5CAddRn_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1210981468631191554/C5CAddRn_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/9586552/1572213500",
+			"profile_link_color": "483D8B",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "858080",
+			"profile_text_color": "086660",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": false,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1224515892282740700,
+		"quoted_status_id_str": "1224515892282740737",
+		"quoted_status_permalink": {
+			"url": "https://t.co/VUuVBa1Gmp",
+			"expanded": "https://twitter.com/PeccaryNotPig/status/1224515892282740737",
+			"display": "twitter.com/PeccaryNotPig/…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Feb 04 02:11:53 +0000 2020",
+			"id": 1224515892282740700,
+			"id_str": "1224515892282740737",
+			"full_text": "A coyote and a badger use a culvert as a wildlife crossing to pass under a busy California highway together. Coyotes and badgers are known to hunt together. \n\n🎥Peninsula Open Space Trust https://t.co/oS9BL5JOoK",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				186
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1224515864201846800,
+						"id_str": "1224515864201846790",
+						"indices": [
+							187,
+							210
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1224515864201846790/pu/img/b2j3_FgMO3Fadb8q.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1224515864201846790/pu/img/b2j3_FgMO3Fadb8q.jpg",
+						"url": "https://t.co/oS9BL5JOoK",
+						"display_url": "pic.twitter.com/oS9BL5JOoK",
+						"expanded_url": "https://twitter.com/PeccaryNotPig/status/1224515892282740737/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 720,
+								"resize": "fit"
+							}
+						},
+						"features": {}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1224515864201846800,
+						"id_str": "1224515864201846790",
+						"indices": [
+							187,
+							210
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1224515864201846790/pu/img/b2j3_FgMO3Fadb8q.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1224515864201846790/pu/img/b2j3_FgMO3Fadb8q.jpg",
+						"url": "https://t.co/oS9BL5JOoK",
+						"display_url": "pic.twitter.com/oS9BL5JOoK",
+						"expanded_url": "https://twitter.com/PeccaryNotPig/status/1224515892282740737/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 720,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								16,
+								9
+							],
+							"duration_millis": 12584,
+							"variants": [
+								{
+									"bitrate": 256000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1224515864201846790/pu/vid/480x270/GDgEHrEy7yNIv47f.mp4?tag=10"
+								},
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1224515864201846790/pu/vid/640x360/iAtd2ZggmalUYviD.mp4?tag=10"
+								},
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/ext_tw_video/1224515864201846790/pu/pl/vEQb8Gd8hVHdTUyi.m3u8?tag=10"
+								},
+								{
+									"bitrate": 2176000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1224515864201846790/pu/vid/1280x720/h_MZRVTD0Ommva90.mp4?tag=10"
+								}
+							]
+						},
+						"features": {},
+						"additional_media_info": {
+							"monetizable": false
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 751474150414307300,
+				"id_str": "751474150414307328",
+				"name": "Russ McSpadden",
+				"screen_name": "PeccaryNotPig",
+				"location": "Tucson, AZ",
+				"description": "Writer, videographer, remote camera operator in the borderlands of Arizona & Mexico @CenterForBioDiv #NoBorderWall #NoMasters Typos mine. He/Him/Ya’ll",
+				"url": "https://t.co/7mm5S1p4ST",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/7mm5S1p4ST",
+								"expanded_url": "http://BorderViews.org",
+								"display_url": "BorderViews.org",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 4946,
+				"friends_count": 640,
+				"listed_count": 41,
+				"created_at": "Fri Jul 08 17:52:54 +0000 2016",
+				"favourites_count": 2179,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 931,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1202995454713065472/RL8Piafi_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1202995454713065472/RL8Piafi_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/751474150414307328/1515611665",
+				"profile_link_color": "ABB8C2",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"can_media_tag": true,
+				"followed_by": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 96071,
+			"favorite_count": 396651,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The excitement of the coyote…❤️ https://t.co/VUuVBa1Gmp"
+	},
+	{
 		"created_at": "Tue Feb 04 15:09:51 +0000 2020",
 		"id": 1224711671777808400,
 		"id_str": "1224711671777808385",
