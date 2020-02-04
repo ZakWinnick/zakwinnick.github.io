af6ce8e8e18d21145dff32eb7918bc164806e8c0
Zak Winnick <zakwinnick@protonmail.com>
2020-02-04 09:10:47 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10536,
-	"created_at": "Tue Feb 04 00:10:45 +0000 2020",
+	"tweets": 10537,
+	"created_at": "Tue Feb 04 15:10:46 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,353 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Tue Feb 04 15:09:51 +0000 2020",
+		"id": 1224711671777808400,
+		"id_str": "1224711671777808385",
+		"full_text": "Yes. Please. Put it out of its misery already. https://t.co/SJ6vXgpIMO",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			46
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/SJ6vXgpIMO",
+					"expanded_url": "https://twitter.com/screamscape/status/1224700680709058564",
+					"display_url": "twitter.com/screamscape/st…",
+					"indices": [
+						47,
+						70
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 9586552,
+			"id_str": "9586552",
+			"name": "Zak Winnick",
+			"screen_name": "ZakWinnick",
+			"location": "Nashville, TN",
+			"description": "Hotel Technology Guru | Electric Vehicle Enthusiast | Action Cam Evangelist",
+			"url": "https://t.co/vGZZOYJJCo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vGZZOYJJCo",
+							"expanded_url": "https://ZakWinnick.com",
+							"display_url": "ZakWinnick.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 58,
+			"friends_count": 185,
+			"listed_count": 18,
+			"created_at": "Sun Oct 21 22:28:31 +0000 2007",
+			"favourites_count": 144,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9411,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "3F5B66",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1210981468631191554/C5CAddRn_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1210981468631191554/C5CAddRn_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/9586552/1572213500",
+			"profile_link_color": "483D8B",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "858080",
+			"profile_text_color": "086660",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": false,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1224700680709058600,
+		"quoted_status_id_str": "1224700680709058564",
+		"quoted_status_permalink": {
+			"url": "https://t.co/SJ6vXgpIMO",
+			"expanded": "https://twitter.com/screamscape/status/1224700680709058564",
+			"display": "twitter.com/screamscape/st…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Feb 04 14:26:10 +0000 2020",
+			"id": 1224700680709058600,
+			"id_str": "1224700680709058564",
+			"full_text": "Detailed Desperado status update - Could this finally be the end of the Las Vegas giant? - https://t.co/evh0z3UKCR https://t.co/qswQAwb3Tc",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				114
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/evh0z3UKCR",
+						"expanded_url": "http://www.screamscape.com",
+						"display_url": "screamscape.com",
+						"indices": [
+							91,
+							114
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1224700662786789400,
+						"id_str": "1224700662786789384",
+						"indices": [
+							115,
+							138
+						],
+						"media_url": "http://pbs.twimg.com/media/EP8Cw5mXkAg8BDl.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EP8Cw5mXkAg8BDl.jpg",
+						"url": "https://t.co/qswQAwb3Tc",
+						"display_url": "pic.twitter.com/qswQAwb3Tc",
+						"expanded_url": "https://twitter.com/screamscape/status/1224700680709058564/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 600,
+								"h": 295,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 600,
+								"h": 295,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 600,
+								"h": 295,
+								"resize": "fit"
+							}
+						},
+						"features": {
+							"large": {
+								"faces": []
+							},
+							"small": {
+								"faces": []
+							},
+							"medium": {
+								"faces": []
+							},
+							"orig": {
+								"faces": []
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1224700662786789400,
+						"id_str": "1224700662786789384",
+						"indices": [
+							115,
+							138
+						],
+						"media_url": "http://pbs.twimg.com/media/EP8Cw5mXkAg8BDl.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EP8Cw5mXkAg8BDl.jpg",
+						"url": "https://t.co/qswQAwb3Tc",
+						"display_url": "pic.twitter.com/qswQAwb3Tc",
+						"expanded_url": "https://twitter.com/screamscape/status/1224700680709058564/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 600,
+								"h": 295,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 600,
+								"h": 295,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 600,
+								"h": 295,
+								"resize": "fit"
+							}
+						},
+						"features": {
+							"large": {
+								"faces": []
+							},
+							"small": {
+								"faces": []
+							},
+							"medium": {
+								"faces": []
+							},
+							"orig": {
+								"faces": []
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 11532872,
+				"id_str": "11532872",
+				"name": "Screamscape",
+				"screen_name": "screamscape",
+				"location": "ÜT: 35.36171,-81.0927",
+				"description": "https://t.co/pBJ024V7VI by Lance Hart: Theme Park and Entertainment news, odd humor, tech geek, husband and father of 3, Master of https://t.co/pBJ024V7VI",
+				"url": "http://t.co/dzwYCPAjgK",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/dzwYCPAjgK",
+								"expanded_url": "http://www.screamscape.com",
+								"display_url": "screamscape.com",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/pBJ024V7VI",
+								"expanded_url": "http://Screamscape.com",
+								"display_url": "Screamscape.com",
+								"indices": [
+									0,
+									23
+								]
+							},
+							{
+								"url": "https://t.co/pBJ024V7VI",
+								"expanded_url": "http://Screamscape.com",
+								"display_url": "Screamscape.com",
+								"indices": [
+									131,
+									154
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 23647,
+				"friends_count": 156,
+				"listed_count": 381,
+				"created_at": "Wed Dec 26 13:55:49 +0000 2007",
+				"favourites_count": 2104,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 20209,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C6E2EE",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/639423837545889792/gFCxqBcT_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/639423837545889792/gFCxqBcT_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/11532872/1441285608",
+				"profile_link_color": "1F98C7",
+				"profile_sidebar_border_color": "C6E2EE",
+				"profile_sidebar_fill_color": "DAECF4",
+				"profile_text_color": "663B12",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"can_media_tag": true,
+				"followed_by": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 5,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Yes. Please. Put it out of its misery already. https://t.co/SJ6vXgpIMO"
+	},
+	{
 		"created_at": "Tue Feb 04 00:00:31 +0000 2020",
 		"id": 1224432079632396300,
 		"id_str": "1224482832954863616",
