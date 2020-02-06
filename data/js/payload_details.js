6e75ca948c2d5092db773e0f0c6f9862cfba209b
Zak Winnick <zakwinnick@protonmail.com>
2020-02-06 08:10:47 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10541,
-	"created_at": "Thu Feb 06 12:18:42 +0000 2020",
+	"tweets": 10542,
+	"created_at": "Thu Feb 06 14:10:46 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,195 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Thu Feb 06 13:53:54 +0000 2020",
+		"id": 1225173552577552400,
+		"id_str": "1225417336162848772",
+		"full_text": "I feel old. #Alexa #VoiceFirst https://t.co/cynYeHHY6Z",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			30
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Alexa",
+					"indices": [
+						12,
+						18
+					]
+				},
+				{
+					"text": "VoiceFirst",
+					"indices": [
+						19,
+						30
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1225173548731392000,
+					"id_str": "1225173548731392001",
+					"indices": [
+						31,
+						54
+					],
+					"media_url": "http://pbs.twimg.com/media/EQCw2fBW4AELsjm.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EQCw2fBW4AELsjm.jpg",
+					"url": "https://t.co/cynYeHHY6Z",
+					"display_url": "pic.twitter.com/cynYeHHY6Z",
+					"expanded_url": "https://twitter.com/WayneAR19/status/1225173552577552392/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 800,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 800,
+							"h": 800,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1225173548731392000,
+					"id_str": "1225173548731392001",
+					"indices": [
+						31,
+						54
+					],
+					"media_url": "http://pbs.twimg.com/media/EQCw2fBW4AELsjm.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EQCw2fBW4AELsjm.jpg",
+					"url": "https://t.co/cynYeHHY6Z",
+					"display_url": "pic.twitter.com/cynYeHHY6Z",
+					"expanded_url": "https://twitter.com/WayneAR19/status/1225173552577552392/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 800,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 800,
+							"h": 800,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2571629532,
+			"id_str": "2571629532",
+			"name": "Wayne Richard",
+			"screen_name": "WayneAR19",
+			"location": "Loudonville, NY",
+			"description": "Owner and Founder, Voicebiz Technologies.    Real estate agent. Hiker. Backpacker.",
+			"url": "https://t.co/W8kim9oPNI",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/W8kim9oPNI",
+							"expanded_url": "http://www.VoicebizTechnologies.com",
+							"display_url": "VoicebizTechnologies.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 56,
+			"friends_count": 102,
+			"listed_count": 2,
+			"created_at": "Mon Jun 16 21:07:00 +0000 2014",
+			"favourites_count": 177,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 31,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1030177020611887106/peAiDOJF_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1030177020611887106/peAiDOJF_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2571629532/1534448412",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 12,
+		"favorite_count": 40,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I feel old. #Alexa #VoiceFirst https://t.co/cynYeHHY6Z"
+	},
+	{
 		"created_at": "Thu Feb 06 12:17:08 +0000 2020",
 		"id": 1225096786005676000,
 		"id_str": "1225392982611972097",
