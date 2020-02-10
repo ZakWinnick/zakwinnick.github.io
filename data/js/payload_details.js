f0a5fe3f670510f7c19c1848173d37a21f942f14
Zak Winnick <zakwinnick@protonmail.com>
2020-02-09 20:10:46 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10543,
-	"created_at": "Thu Feb 06 16:10:46 +0000 2020",
+	"tweets": 10544,
+	"created_at": "Mon Feb 10 02:10:45 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,112 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Mon Feb 10 01:51:40 +0000 2020",
+		"id": 1226682529379475500,
+		"id_str": "1226685130766520320",
+		"full_text": "My nine-year-old son, earnestly watching his first Oscars: \"This will be much more interesting next year when @BillandTed3 wins everything\" 😄🤘🏻",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			143
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "BillandTed3",
+					"name": "Bill & Ted 3",
+					"id": 284929130,
+					"id_str": "284929130",
+					"indices": [
+						110,
+						122
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 47475039,
+			"id_str": "47475039",
+			"name": "Sarah Kendzior",
+			"screen_name": "sarahkendzior",
+			"location": "St. Louis",
+			"description": "Writer and scholar. Co-host of @gaslitnation. Author of the bestseller THE VIEW FROM FLYOVER COUNTRY (2018) and HIDING IN PLAIN SIGHT (April 2020).",
+			"url": "https://t.co/D0d8h8jBBn",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/D0d8h8jBBn",
+							"expanded_url": "http://www.sarahkendzior.com",
+							"display_url": "sarahkendzior.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 518784,
+			"friends_count": 1832,
+			"listed_count": 8015,
+			"created_at": "Mon Jun 15 23:14:30 +0000 2009",
+			"favourites_count": 158110,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 110776,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1002524544878432256/sipdKDFZ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1002524544878432256/sipdKDFZ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/47475039/1572535371",
+			"profile_link_color": "16678A",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 26,
+		"favorite_count": 498,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "My nine-year-old son, earnestly watching his first Oscars: \"This will be much more interesting next year when @BillandTed3 wins everything\" 😄🤘🏻"
+	},
+	{
 		"created_at": "Thu Feb 06 15:49:43 +0000 2020",
 		"id": 1225048618069037000,
 		"id_str": "1225446482578464769",
