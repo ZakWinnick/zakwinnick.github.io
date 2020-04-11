52fb9b3a732290cb2ff8d7e74ef04937085cfb74
Zak Winnick <zakwinnick@protonmail.com>
2020-04-11 18:18:44 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10631,
-	"created_at": "Sat Apr 11 23:10:46 +0000 2020",
+	"tweets": 10632,
+	"created_at": "Sat Apr 11 23:18:43 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_04.js
+++ data/js/tweets/2020_04.js
@@ -1,6 +1,180 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Sat Apr 11 23:13:55 +0000 2020",
+		"id": 1248899387046219800,
+		"id_str": "1249113478465490945",
+		"full_text": "This little exchange is a gem https://t.co/hut6tonhHp",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			29
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1248899381216137200,
+					"id_str": "1248899381216137217",
+					"indices": [
+						30,
+						53
+					],
+					"media_url": "http://pbs.twimg.com/media/EVT7XsmX0AEXSuT.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EVT7XsmX0AEXSuT.jpg",
+					"url": "https://t.co/hut6tonhHp",
+					"display_url": "pic.twitter.com/hut6tonhHp",
+					"expanded_url": "https://twitter.com/alisongow/status/1248899387046219776/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 537,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 828,
+							"h": 1048,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 828,
+							"h": 1048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1248899381216137200,
+					"id_str": "1248899381216137217",
+					"indices": [
+						30,
+						53
+					],
+					"media_url": "http://pbs.twimg.com/media/EVT7XsmX0AEXSuT.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EVT7XsmX0AEXSuT.jpg",
+					"url": "https://t.co/hut6tonhHp",
+					"display_url": "pic.twitter.com/hut6tonhHp",
+					"expanded_url": "https://twitter.com/alisongow/status/1248899387046219776/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 537,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 828,
+							"h": 1048,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 828,
+							"h": 1048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 12792012,
+			"id_str": "12792012",
+			"name": "Alison Gow",
+			"screen_name": "alisongow",
+			"location": "Wales",
+			"description": "Publisher of @Laudablepods. Editor in Chief (digital) for Reach regionals. President, Society of Editors. Runner, skier, Cymru am byth 🏴󠁧󠁢󠁷󠁬󠁳󠁿",
+			"url": "https://t.co/YMAfrm7R6l",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/YMAfrm7R6l",
+							"expanded_url": "https://uk.linkedin.com/in/alisongow",
+							"display_url": "uk.linkedin.com/in/alisongow",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7633,
+			"friends_count": 1680,
+			"listed_count": 525,
+			"created_at": "Mon Jan 28 17:08:38 +0000 2008",
+			"favourites_count": 1648,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 26471,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1100330486914838528/KeousAaC_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1100330486914838528/KeousAaC_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/12792012/1470581953",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "E0FF92",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 51,
+		"favorite_count": 169,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "This little exchange is a gem https://t.co/hut6tonhHp"
+	},
+	{
 		"created_at": "Sat Apr 11 23:09:15 +0000 2020",
 		"id": 1248821594933399600,
 		"id_str": "1249112304077045761",
