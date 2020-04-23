3100cdf284a6489842433a8cec811697edf9291c
Zak Winnick <zakwinnick@protonmail.com>
2020-04-22 21:10:47 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10642,
-	"created_at": "Thu Apr 23 00:10:48 +0000 2020",
+	"tweets": 10643,
+	"created_at": "Thu Apr 23 02:10:46 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_04.js
+++ data/js/tweets/2020_04.js
@@ -1,6 +1,333 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Thu Apr 23 02:01:00 +0000 2020",
+		"id": 1253141795057303600,
+		"id_str": "1253141795057303553",
+		"full_text": "What the fuck is this, and why am I watching it before I go to bed? https://t.co/NTLLu5W6Dh",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			67
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/NTLLu5W6Dh",
+					"expanded_url": "https://twitter.com/jeremyjudkins/status/1253128373217431553",
+					"display_url": "twitter.com/jeremyjudkins/…",
+					"indices": [
+						68,
+						91
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 9586552,
+			"id_str": "9586552",
+			"name": "Zak Winnick",
+			"screen_name": "ZakWinnick",
+			"location": "Nashville, TN",
+			"description": "Hotel Technology Guru | Electric Vehicle Enthusiast",
+			"url": "https://t.co/vGZZOYJJCo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vGZZOYJJCo",
+							"expanded_url": "https://ZakWinnick.com",
+							"display_url": "ZakWinnick.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 56,
+			"friends_count": 202,
+			"listed_count": 18,
+			"created_at": "Sun Oct 21 22:28:31 +0000 2007",
+			"favourites_count": 157,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9518,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "3F5B66",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1252645188444524545/bHj34SzY_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1252645188444524545/bHj34SzY_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/9586552/1587488861",
+			"profile_link_color": "483D8B",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "858080",
+			"profile_text_color": "086660",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1253128373217431600,
+		"quoted_status_id_str": "1253128373217431553",
+		"quoted_status_permalink": {
+			"url": "https://t.co/NTLLu5W6Dh",
+			"expanded": "https://twitter.com/jeremyjudkins/status/1253128373217431553",
+			"display": "twitter.com/jeremyjudkins/…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Apr 23 01:07:40 +0000 2020",
+			"id": 1253128373217431600,
+			"id_str": "1253128373217431553",
+			"full_text": "The quarantine is affecting some of us worse than others...\n\nSource: idfk someone sent it to me. Probably originated on the dark web 🤣 https://t.co/DFaPUlaE39",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				134
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1253128277436350500,
+						"id_str": "1253128277436350466",
+						"indices": [
+							135,
+							158
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1253128277436350466/pu/img/QL7XZ6SIv5go3H8Y.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1253128277436350466/pu/img/QL7XZ6SIv5go3H8Y.jpg",
+						"url": "https://t.co/DFaPUlaE39",
+						"display_url": "pic.twitter.com/DFaPUlaE39",
+						"expanded_url": "https://twitter.com/jeremyjudkins/status/1253128373217431553/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 544,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 640,
+								"h": 800,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 640,
+								"h": 800,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1253128277436350500,
+						"id_str": "1253128277436350466",
+						"indices": [
+							135,
+							158
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1253128277436350466/pu/img/QL7XZ6SIv5go3H8Y.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1253128277436350466/pu/img/QL7XZ6SIv5go3H8Y.jpg",
+						"url": "https://t.co/DFaPUlaE39",
+						"display_url": "pic.twitter.com/DFaPUlaE39",
+						"expanded_url": "https://twitter.com/jeremyjudkins/status/1253128373217431553/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 544,
+								"h": 680,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 640,
+								"h": 800,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 640,
+								"h": 800,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								4,
+								5
+							],
+							"duration_millis": 36167,
+							"variants": [
+								{
+									"bitrate": 632000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1253128277436350466/pu/vid/320x400/-i2p6eOL69-Zsu46.mp4?tag=10"
+								},
+								{
+									"bitrate": 2176000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1253128277436350466/pu/vid/640x800/-DTmXk28qEm0sptZ.mp4?tag=10"
+								},
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1253128277436350466/pu/vid/360x450/iSR5GI9oGVnT4tHN.mp4?tag=10"
+								},
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/ext_tw_video/1253128277436350466/pu/pl/-PE2kgkS_eyetm1I.m3u8?tag=10"
+								}
+							]
+						},
+						"additional_media_info": {
+							"monetizable": false
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 84890791,
+				"id_str": "84890791",
+				"name": "Jeremy Judkins",
+				"screen_name": "jeremyjudkins",
+				"location": "Southwest Florida",
+				"description": "Tech Youtuber turned Tesla Youtuber. Florida Man. Tesla Model 3 Owner. Subscribe to my Youtube channel pleaaaassse: https://t.co/axudjXz7tn",
+				"url": "https://t.co/axudjXz7tn",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/axudjXz7tn",
+								"expanded_url": "http://youtube.com/jeremyjudkins",
+								"display_url": "youtube.com/jeremyjudkins",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/axudjXz7tn",
+								"expanded_url": "http://youtube.com/jeremyjudkins",
+								"display_url": "youtube.com/jeremyjudkins",
+								"indices": [
+									116,
+									139
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 4446,
+				"friends_count": 1971,
+				"listed_count": 32,
+				"created_at": "Sat Oct 24 16:55:02 +0000 2009",
+				"favourites_count": 41522,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 25444,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "022330",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1111762219853930496/m8VW_mqN_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1111762219853930496/m8VW_mqN_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/84890791/1577187759",
+				"profile_link_color": "0084B4",
+				"profile_sidebar_border_color": "A8C7F7",
+				"profile_sidebar_fill_color": "C0DFEC",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 17,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "What the fuck is this, and why am I watching it before I go to bed? https://t.co/NTLLu5W6Dh"
+	},
+	{
 		"created_at": "Wed Apr 22 23:29:55 +0000 2020",
 		"id": 1253103770659172400,
 		"id_str": "1253103770659172354",
