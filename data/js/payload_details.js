fe6e1419dbc870eb3b918a9d39b907de6b7b10cb
Zak Winnick <zakwinnick@protonmail.com>
2020-04-27 12:10:49 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10648,
-	"created_at": "Mon Apr 27 14:10:47 +0000 2020",
+	"tweets": 10649,
+	"created_at": "Mon Apr 27 17:10:48 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_04.js
+++ data/js/tweets/2020_04.js
@@ -1,6 +1,256 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Mon Apr 27 16:24:20 +0000 2020",
+		"id": 1254808609239773200,
+		"id_str": "1254808609239773185",
+		"full_text": "I think @ismh and @MKBHD should split this, and do a crossover video on each other’s YT channels. https://t.co/dYGuAN8ao1",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			97
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ismh",
+					"name": "Stephen Hackett",
+					"id": 1566661,
+					"id_str": "1566661",
+					"indices": [
+						8,
+						13
+					]
+				},
+				{
+					"screen_name": "MKBHD",
+					"name": "Marques Brownlee",
+					"id": 29873662,
+					"id_str": "29873662",
+					"indices": [
+						18,
+						24
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/dYGuAN8ao1",
+					"expanded_url": "https://twitter.com/ismh/status/1254782401500164098",
+					"display_url": "twitter.com/ismh/status/12…",
+					"indices": [
+						98,
+						121
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 9586552,
+			"id_str": "9586552",
+			"name": "Zak Winnick",
+			"screen_name": "ZakWinnick",
+			"location": "Nashville, TN",
+			"description": "Hotel Technology Guru | Electric Vehicle Enthusiast",
+			"url": "https://t.co/vGZZOYJJCo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vGZZOYJJCo",
+							"expanded_url": "https://ZakWinnick.com",
+							"display_url": "ZakWinnick.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 57,
+			"friends_count": 203,
+			"listed_count": 18,
+			"created_at": "Sun Oct 21 22:28:31 +0000 2007",
+			"favourites_count": 158,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9517,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "3F5B66",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1252645188444524545/bHj34SzY_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1252645188444524545/bHj34SzY_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/9586552/1587488861",
+			"profile_link_color": "483D8B",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "858080",
+			"profile_text_color": "086660",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1254782401500164000,
+		"quoted_status_id_str": "1254782401500164098",
+		"quoted_status_permalink": {
+			"url": "https://t.co/dYGuAN8ao1",
+			"expanded": "https://twitter.com/ismh/status/1254782401500164098",
+			"display": "twitter.com/ismh/status/12…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Apr 27 14:40:11 +0000 2020",
+			"id": 1254782401500164000,
+			"id_str": "1254782401500164098",
+			"full_text": "Yo @MKBHD https://t.co/sMWDrpC8fZ",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				33
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "MKBHD",
+						"name": "Marques Brownlee",
+						"id": 29873662,
+						"id_str": "29873662",
+						"indices": [
+							3,
+							9
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/sMWDrpC8fZ",
+						"expanded_url": "https://www.theverge.com/2020/4/27/21237393/apple-car-sale-vintage-race-porsche-price",
+						"display_url": "theverge.com/2020/4/27/2123…",
+						"indices": [
+							10,
+							33
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://tapbots.com/software/tweetbot/mac\" rel=\"nofollow\">Tweetbot for Mac</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1566661,
+				"id_str": "1566661",
+				"name": "Stephen Hackett",
+				"screen_name": "ismh",
+				"location": "Memphis, Tennessee",
+				"description": "Co-founder of the @_relayfm podcast network and the host of a few nerdy shows covering Apple and the space industry. Writer at @512px and @stjude supporter.",
+				"url": "https://t.co/7nn5wvZ2op",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/7nn5wvZ2op",
+								"expanded_url": "https://relay.fm",
+								"display_url": "relay.fm",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 24185,
+				"friends_count": 431,
+				"listed_count": 1297,
+				"created_at": "Tue Mar 20 00:37:22 +0000 2007",
+				"favourites_count": 21917,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 3188,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "1A1B1F",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1179371476207755264/399KYo7z_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1179371476207755264/399KYo7z_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1566661/1570019362",
+				"profile_link_color": "3B94D9",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "252429",
+				"profile_text_color": "666666",
+				"profile_use_background_image": false,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 30,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "und"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I think @ismh and @MKBHD should split this, and do a crossover video on each other’s YT channels. https://t.co/dYGuAN8ao1"
+	},
+	{
 		"created_at": "Mon Apr 27 13:22:44 +0000 2020",
 		"id": 1254762910418698200,
 		"id_str": "1254762910418698242",
