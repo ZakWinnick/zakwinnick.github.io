81ce2246c7cf54f2f3bab69852be497629391012
Zak Winnick <zakwinnick@protonmail.com>
2020-04-27 08:10:48 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10646,
-	"created_at": "Thu Apr 23 22:18:43 +0000 2020",
+	"tweets": 10647,
+	"created_at": "Mon Apr 27 13:10:46 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_04.js
+++ data/js/tweets/2020_04.js
@@ -1,6 +1,345 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Mon Apr 27 12:45:20 +0000 2020",
+		"id": 1254753495577755600,
+		"id_str": "1254753495577755648",
+		"full_text": "Yes…Please tell me how a product that hasn’t been announced yet is not shipping on the date that wasn’t promised by anyone…\n\nI seriously hate the tech news space for shit just like this (and all the ass kissing/ball licking, but that’s a different story). https://t.co/1VDIkfUm9Z",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			255
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/1VDIkfUm9Z",
+					"expanded_url": "https://twitter.com/AppleTerminal/status/1254729235731791872",
+					"display_url": "twitter.com/AppleTerminal/…",
+					"indices": [
+						256,
+						279
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 9586552,
+			"id_str": "9586552",
+			"name": "Zak Winnick",
+			"screen_name": "ZakWinnick",
+			"location": "Nashville, TN",
+			"description": "Hotel Technology Guru | Electric Vehicle Enthusiast",
+			"url": "https://t.co/vGZZOYJJCo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vGZZOYJJCo",
+							"expanded_url": "https://ZakWinnick.com",
+							"display_url": "ZakWinnick.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 57,
+			"friends_count": 203,
+			"listed_count": 18,
+			"created_at": "Sun Oct 21 22:28:31 +0000 2007",
+			"favourites_count": 158,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9515,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "3F5B66",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1252645188444524545/bHj34SzY_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1252645188444524545/bHj34SzY_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/9586552/1587488861",
+			"profile_link_color": "483D8B",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "858080",
+			"profile_text_color": "086660",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1254729235731792000,
+		"quoted_status_id_str": "1254729235731791872",
+		"quoted_status_permalink": {
+			"url": "https://t.co/1VDIkfUm9Z",
+			"expanded": "https://twitter.com/AppleTerminal/status/1254729235731791872",
+			"display": "twitter.com/AppleTerminal/…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Apr 27 11:08:56 +0000 2020",
+			"id": 1254729235731792000,
+			"id_str": "1254729235731791872",
+			"full_text": "JUST IN:\n\nThe @WSJ exclusively reports that @Apple has delayed mass production of the upcoming #iPhone12 by 1 month, causing further doubt on an on-time launch this fall.\n\nMore: https://t.co/PLGdNcUECC\n\n#Apple #iPhone https://t.co/T77ffKxJMU",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				217
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "iPhone12",
+						"indices": [
+							95,
+							104
+						]
+					},
+					{
+						"text": "Apple",
+						"indices": [
+							203,
+							209
+						]
+					},
+					{
+						"text": "iPhone",
+						"indices": [
+							210,
+							217
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "WSJ",
+						"name": "The Wall Street Journal",
+						"id": 3108351,
+						"id_str": "3108351",
+						"indices": [
+							14,
+							18
+						]
+					},
+					{
+						"screen_name": "Apple",
+						"name": "Apple",
+						"id": 380749300,
+						"id_str": "380749300",
+						"indices": [
+							44,
+							50
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/PLGdNcUECC",
+						"expanded_url": "http://appleterm.com/?p=8317",
+						"display_url": "appleterm.com/?p=8317",
+						"indices": [
+							178,
+							201
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1254728798223970300,
+						"id_str": "1254728798223970305",
+						"indices": [
+							218,
+							241
+						],
+						"media_url": "http://pbs.twimg.com/media/EWmxMbOXsAEs1rY.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EWmxMbOXsAEs1rY.jpg",
+						"url": "https://t.co/T77ffKxJMU",
+						"display_url": "pic.twitter.com/T77ffKxJMU",
+						"expanded_url": "https://twitter.com/AppleTerminal/status/1254729235731791872/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1254728798223970300,
+						"id_str": "1254728798223970305",
+						"indices": [
+							218,
+							241
+						],
+						"media_url": "http://pbs.twimg.com/media/EWmxMbOXsAEs1rY.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EWmxMbOXsAEs1rY.jpg",
+						"url": "https://t.co/T77ffKxJMU",
+						"display_url": "pic.twitter.com/T77ffKxJMU",
+						"expanded_url": "https://twitter.com/AppleTerminal/status/1254729235731791872/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1135468733957386200,
+				"id_str": "1135468733957386240",
+				"name": "Apple Terminal",
+				"screen_name": "AppleTerminal",
+				"location": "",
+				"description": "Your new favorite independent Apple news source. From beta, to jailbreak, to even Apple stock news, we cover it all. Follow us to keep up with the latest.",
+				"url": "https://t.co/5rdVAFBKu4",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/5rdVAFBKu4",
+								"expanded_url": "http://appleterm.com",
+								"display_url": "appleterm.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 7337,
+				"friends_count": 5,
+				"listed_count": 69,
+				"created_at": "Mon Jun 03 08:50:16 +0000 2019",
+				"favourites_count": 25,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 4713,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1176738887659610112/_nSS9d4e_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1176738887659610112/_nSS9d4e_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1135468733957386240/1586584179",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 13,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Yes…Please tell me how a product that hasn’t been announced yet is not shipping on the date that wasn’t promised by anyone…\n\nI seriously hate the tech news space for shit just like this (and all the ass kissing/ball licking, but that’s a different story). https://t.co/1VDIkfUm9Z"
+	},
+	{
 		"created_at": "Thu Apr 23 22:12:27 +0000 2020",
 		"id": 1253446666730066000,
 		"id_str": "1253446666730065921",
