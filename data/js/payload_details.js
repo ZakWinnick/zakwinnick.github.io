cd6923aeca6440decc4c9a323e08ad5fef3c801b
Zak Winnick <zakwinnick@protonmail.com>
2020-05-05 17:10:47 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10654,
-	"created_at": "Mon May 04 13:10:45 +0000 2020",
+	"tweets": 10655,
+	"created_at": "Tue May 05 22:10:46 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,303 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Tue May 05 21:55:03 +0000 2020",
+		"id": 1257790938883252200,
+		"id_str": "1257790938883252226",
+		"full_text": "Half expected to see @kurt_loder reply saying “75.”… https://t.co/haF7AMQFZO",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			52
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "kurt_loder",
+					"name": "Kurt Loder",
+					"id": 307259829,
+					"id_str": "307259829",
+					"indices": [
+						21,
+						32
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/haF7AMQFZO",
+					"expanded_url": "https://twitter.com/casspa/status/1257705470690766848",
+					"display_url": "twitter.com/casspa/status/…",
+					"indices": [
+						53,
+						76
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 9586552,
+			"id_str": "9586552",
+			"name": "Zak Winnick",
+			"screen_name": "ZakWinnick",
+			"location": "Nashville, TN",
+			"description": "Hotel Technology Guru | Electric Vehicle Enthusiast",
+			"url": "https://t.co/vGZZOYJJCo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vGZZOYJJCo",
+							"expanded_url": "https://ZakWinnick.com",
+							"display_url": "ZakWinnick.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 56,
+			"friends_count": 201,
+			"listed_count": 18,
+			"created_at": "Sun Oct 21 22:28:31 +0000 2007",
+			"favourites_count": 159,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9521,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "3F5B66",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1252645188444524545/bHj34SzY_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1252645188444524545/bHj34SzY_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/9586552/1587488861",
+			"profile_link_color": "483D8B",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "858080",
+			"profile_text_color": "086660",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1257705470690766800,
+		"quoted_status_id_str": "1257705470690766848",
+		"quoted_status_permalink": {
+			"url": "https://t.co/haF7AMQFZO",
+			"expanded": "https://twitter.com/casspa/status/1257705470690766848",
+			"display": "twitter.com/casspa/status/…"
+		},
+		"quoted_status": {
+			"created_at": "Tue May 05 16:15:25 +0000 2020",
+			"id": 1257705470690766800,
+			"id_str": "1257705470690766848",
+			"full_text": "Former MTV VJ Kurt Loder turns 75-years-old today.  How old do you feel right now? https://t.co/yhcuIAvGB8",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				82
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1257705466567721000,
+						"id_str": "1257705466567720963",
+						"indices": [
+							83,
+							106
+						],
+						"media_url": "http://pbs.twimg.com/media/EXREdU7WAAMlznd.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EXREdU7WAAMlznd.jpg",
+						"url": "https://t.co/yhcuIAvGB8",
+						"display_url": "pic.twitter.com/yhcuIAvGB8",
+						"expanded_url": "https://twitter.com/casspa/status/1257705470690766848/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1280,
+								"h": 720,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1257705466567721000,
+						"id_str": "1257705466567720963",
+						"indices": [
+							83,
+							106
+						],
+						"media_url": "http://pbs.twimg.com/media/EXREdU7WAAMlznd.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/EXREdU7WAAMlznd.jpg",
+						"url": "https://t.co/yhcuIAvGB8",
+						"display_url": "pic.twitter.com/yhcuIAvGB8",
+						"expanded_url": "https://twitter.com/casspa/status/1257705470690766848/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1280,
+								"h": 720,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 383,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 675,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 16535231,
+				"id_str": "16535231",
+				"name": "Cass Anderson",
+				"screen_name": "casspa",
+				"location": "Sarasota, FL",
+				"description": "Partner at @brobible. Phish addict. Work tweets and stuff about fishing/gear/outdoors but mostly shitposting. My Tottenham Hotspur takes are rarely good",
+				"url": "https://t.co/OtvAeUju23",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/OtvAeUju23",
+								"expanded_url": "http://www.brobible.com/author/cassfrombrobible",
+								"display_url": "brobible.com/author/cassfro…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 9147,
+				"friends_count": 1908,
+				"listed_count": 273,
+				"created_at": "Tue Sep 30 21:30:05 +0000 2008",
+				"favourites_count": 27882,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 60453,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme5/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme5/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1101928910206496768/2ckR1pua_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1101928910206496768/2ckR1pua_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/16535231/1515852578",
+				"profile_link_color": "B6B6B9",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "616161",
+				"profile_text_color": "000000",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2995,
+			"favorite_count": 11441,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Half expected to see @kurt_loder reply saying “75.”… https://t.co/haF7AMQFZO"
+	},
+	{
 		"created_at": "Mon May 04 12:47:20 +0000 2020",
 		"id": 1257290715664724000,
 		"id_str": "1257290715664723968",
