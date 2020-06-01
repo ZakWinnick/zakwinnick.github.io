caa33191640d2e0207f1cbb14a27ebbe61477901
Zak Winnick <zakwinnick@protonmail.com>
2020-06-01 08:10:47 -0500

    Added 1 tweets

---
+++ data/js/tweets/2020_06.js
@@ -0,0 +1,177 @@
+Grailbird.data.tweets_2020_06 = 
+[
+	{
+		"created_at": "Mon Jun 01 13:09:04 +0000 2020",
+		"id": 1267431805633921000,
+		"id_str": "1267443043264856065",
+		"full_text": "Today marks the start of hurricane season. Here's your annual reminder. https://t.co/FPkxUbnoDm",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			71
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1267431802689532000,
+					"id_str": "1267431802689531905",
+					"indices": [
+						72,
+						95
+					],
+					"media_url": "http://pbs.twimg.com/media/EZbSgmhXsAEApmN.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EZbSgmhXsAEApmN.jpg",
+					"url": "https://t.co/FPkxUbnoDm",
+					"display_url": "pic.twitter.com/FPkxUbnoDm",
+					"expanded_url": "https://twitter.com/wxrjm/status/1267431805633921026/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 562,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1125,
+							"h": 929,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1125,
+							"h": 929,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1267431802689532000,
+					"id_str": "1267431802689531905",
+					"indices": [
+						72,
+						95
+					],
+					"media_url": "http://pbs.twimg.com/media/EZbSgmhXsAEApmN.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EZbSgmhXsAEApmN.jpg",
+					"url": "https://t.co/FPkxUbnoDm",
+					"display_url": "pic.twitter.com/FPkxUbnoDm",
+					"expanded_url": "https://twitter.com/wxrjm/status/1267431805633921026/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 562,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1125,
+							"h": 929,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1125,
+							"h": 929,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 51261818,
+			"id_str": "51261818",
+			"name": "Ricky Matthews",
+			"screen_name": "wxrjm",
+			"location": "Hampton,VA - Bristol,TN",
+			"description": "Private Pilot. Meteorologist. UNC Charlotte alumni. I like clouds. #vawx #ncwx  #triwx",
+			"url": "http://t.co/8QzQQ3Y2E9",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/8QzQQ3Y2E9",
+							"expanded_url": "http://about.me/wxrjm",
+							"display_url": "about.me/wxrjm",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4084,
+			"friends_count": 2863,
+			"listed_count": 234,
+			"created_at": "Fri Jun 26 22:49:48 +0000 2009",
+			"favourites_count": 6467,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 68597,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/653337674699403264/WS7F14x5_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/653337674699403264/WS7F14x5_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/51261818/1481428142",
+			"profile_link_color": "009999",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6,
+		"favorite_count": 21,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Today marks the start of hurricane season. Here's your annual reminder. https://t.co/FPkxUbnoDm"
+	}
+]
