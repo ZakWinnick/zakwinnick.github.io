10c3443fd3e202e1743755a3d73ce50189a94df7
Zak Winnick <zakwinnick@protonmail.com>
2020-06-02 10:10:48 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10686,
-	"created_at": "Mon Jun 01 13:10:46 +0000 2020",
+	"tweets": 10687,
+	"created_at": "Tue Jun 02 15:10:46 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_06.js
+++ data/js/tweets/2020_06.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Tue Jun 02 14:47:37 +0000 2020",
+		"id": 1267830161652609000,
+		"id_str": "1267830234457362436",
+		"full_text": "Hey! We’re on iTunes now!\n\nhttps://t.co/X9OfnvsQ7q https://t.co/z2L6Dk8wju",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			50
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/X9OfnvsQ7q",
+					"expanded_url": "https://podcasts.apple.com/us/podcast/zaxtrax/id1516017602",
+					"display_url": "podcasts.apple.com/us/podcast/zax…",
+					"indices": [
+						27,
+						50
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1267830153343688700,
+					"id_str": "1267830153343688705",
+					"indices": [
+						51,
+						74
+					],
+					"media_url": "http://pbs.twimg.com/media/EZg8zqWUYAEOEkc.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EZg8zqWUYAEOEkc.jpg",
+					"url": "https://t.co/z2L6Dk8wju",
+					"display_url": "pic.twitter.com/z2L6Dk8wju",
+					"expanded_url": "https://twitter.com/TraxZax/status/1267830161652609034/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 338,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1019,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 597,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1267830153343688700,
+					"id_str": "1267830153343688705",
+					"indices": [
+						51,
+						74
+					],
+					"media_url": "http://pbs.twimg.com/media/EZg8zqWUYAEOEkc.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EZg8zqWUYAEOEkc.jpg",
+					"url": "https://t.co/z2L6Dk8wju",
+					"display_url": "pic.twitter.com/z2L6Dk8wju",
+					"expanded_url": "https://twitter.com/TraxZax/status/1267830161652609034/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 338,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1019,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 597,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1266482739253063700,
+			"id_str": "1266482739253063680",
+			"name": "ZaxTrax",
+			"screen_name": "TraxZax",
+			"location": "Nashville, TN",
+			"description": "A Curated Collection of Sonic Art | A Podcast Hosted by @ZakWinnick | @SurfChaosMedia",
+			"url": "https://t.co/bUWiO7CkW2",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/bUWiO7CkW2",
+							"expanded_url": "https://www.zaxtrax.com",
+							"display_url": "zaxtrax.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4,
+			"friends_count": 69,
+			"listed_count": 0,
+			"created_at": "Fri May 29 21:33:15 +0000 2020",
+			"favourites_count": 0,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 2,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1266482832689565698/qW-_W0se_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1266482832689565698/qW-_W0se_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1266482739253063680/1590788262",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Hey! We’re on iTunes now!\n\nhttps://t.co/X9OfnvsQ7q https://t.co/z2L6Dk8wju"
+	},
+	{
 		"created_at": "Mon Jun 01 13:09:04 +0000 2020",
 		"id": 1267431805633921000,
 		"id_str": "1267443043264856065",
