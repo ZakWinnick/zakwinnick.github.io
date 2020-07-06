c81839ce4bb536742b319a6b8f22e5c7c9cdc8e4
Zak Winnick <zakwinnick@protonmail.com>
2020-07-06 14:10:47 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10729,
-	"created_at": "Mon Jul 06 13:10:47 +0000 2020",
+	"tweets": 10730,
+	"created_at": "Mon Jul 06 19:10:46 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_07.js
+++ data/js/tweets/2020_07.js
@@ -1,6 +1,101 @@
 Grailbird.data.tweets_2020_07 = 
 [
 	{
+		"created_at": "Mon Jul 06 18:25:19 +0000 2020",
+		"id": 1280203313997181000,
+		"id_str": "1280206208197169152",
+		"full_text": "Kanye \n⬇️\nKan ye\n⬇️\nCan ye\n⬇️\nCan ya\n⬇️\nCan ya not?",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			51
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15296009,
+			"id_str": "15296009",
+			"name": "Brett Erlich",
+			"screen_name": "bretterlich",
+			"location": "Los Angeles, CA",
+			"description": "Supervising Producer, The Young Turks. Happy Half Hour. Viral Video Film School. World’s tallest human.",
+			"url": "https://t.co/O2PxbZEeU1",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/O2PxbZEeU1",
+							"expanded_url": "http://bretterlich.com",
+							"display_url": "bretterlich.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 36999,
+			"friends_count": 1624,
+			"listed_count": 845,
+			"created_at": "Wed Jul 02 07:16:19 +0000 2008",
+			"favourites_count": 18556,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 13532,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1200233729219653632/7e89UVro_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1200233729219653632/7e89UVro_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15296009/1564357602",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 14,
+		"favorite_count": 111,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "tr",
+		"text": "Kanye \n⬇️\nKan ye\n⬇️\nCan ye\n⬇️\nCan ya\n⬇️\nCan ya not?"
+	},
+	{
 		"created_at": "Mon Jul 06 12:49:57 +0000 2020",
 		"id": 1280121808835010600,
 		"id_str": "1280121808835010565",
