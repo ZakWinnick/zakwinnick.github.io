324dfa3017dcabb0f0036eae03280abd8729b7d9
Zak Winnick <zakwinnick@protonmail.com>
2020-08-25 14:10:49 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10763,
-	"created_at": "Wed Aug 19 17:10:49 +0000 2020",
+	"tweets": 10764,
+	"created_at": "Tue Aug 25 19:10:47 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_08.js
+++ data/js/tweets/2020_08.js
@@ -1,6 +1,210 @@
 Grailbird.data.tweets_2020_08 = 
 [
 	{
+		"created_at": "Tue Aug 25 18:29:40 +0000 2020",
+		"id": 1298326695708156000,
+		"id_str": "1298326695708155905",
+		"full_text": "I mean, in the sense that she’s married to the president? Yes, this would be a true statement. https://t.co/eiXv6iqaMy",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			94
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/eiXv6iqaMy",
+					"expanded_url": "https://twitter.com/osullivanauthor/status/1298302608759615489",
+					"display_url": "twitter.com/osullivanautho…",
+					"indices": [
+						95,
+						118
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 9586552,
+			"id_str": "9586552",
+			"name": "Zak Winnick",
+			"screen_name": "ZakWinnick",
+			"location": "Nashville, TN",
+			"description": "15+ Year Veteran Podcaster | Hotel Technology Guru | Amateur Social Media Professional | Host @TraxZax | 8w3",
+			"url": "https://t.co/vGZZOYJJCo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vGZZOYJJCo",
+							"expanded_url": "https://ZakWinnick.com",
+							"display_url": "ZakWinnick.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 59,
+			"friends_count": 206,
+			"listed_count": 18,
+			"created_at": "Sun Oct 21 22:28:31 +0000 2007",
+			"favourites_count": 175,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9541,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "3F5B66",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1283034053076299778/xcEYv_ml_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1283034053076299778/xcEYv_ml_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/9586552/1589495854",
+			"profile_link_color": "483D8B",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "858080",
+			"profile_text_color": "086660",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1298302608759615500,
+		"quoted_status_id_str": "1298302608759615489",
+		"quoted_status_permalink": {
+			"url": "https://t.co/eiXv6iqaMy",
+			"expanded": "https://twitter.com/osullivanauthor/status/1298302608759615489",
+			"display": "twitter.com/osullivanautho…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Aug 25 16:53:57 +0000 2020",
+			"id": 1298302608759615500,
+			"id_str": "1298302608759615489",
+			"full_text": "Peter Navarro just said \"Melania Trump is the Jackie Kennedy of our time.\"\n\nALL. \nOF. \nTHEM. \nARE. \nBATSHIT. \nCRAZY.",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				116
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 19558732,
+				"id_str": "19558732",
+				"name": "Brian O'Sullivan",
+				"screen_name": "osullivanauthor",
+				"location": "SF/LA, CA",
+				"description": "RESISTER!! If you like what I can do with 280 characters, imagine what I can do with 280 pages. Be honored if you checked out my books!   https://t.co/Yq6jovUeRZ",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/Yq6jovUeRZ",
+								"expanded_url": "http://tinyurl.com/y8lbyyz5",
+								"display_url": "tinyurl.com/y8lbyyz5",
+								"indices": [
+									138,
+									161
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 87517,
+				"friends_count": 87046,
+				"listed_count": 277,
+				"created_at": "Mon Jan 26 21:21:58 +0000 2009",
+				"favourites_count": 87133,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 29688,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1287882778961272832/ZqR5recr_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1287882778961272832/ZqR5recr_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/19558732/1586790216",
+				"profile_link_color": "1B95E0",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 842,
+			"favorite_count": 3737,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I mean, in the sense that she’s married to the president? Yes, this would be a true statement. https://t.co/eiXv6iqaMy"
+	},
+	{
 		"created_at": "Wed Aug 19 16:37:49 +0000 2020",
 		"id": 1296124220896944000,
 		"id_str": "1296124220896944129",
