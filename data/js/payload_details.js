bd9395c1785b6fe6629afc953216a0e6518624cd
Zak Winnick <zakwinnick@protonmail.com>
2020-09-15 12:10:47 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10790,
-	"created_at": "Tue Sep 15 13:10:45 +0000 2020",
+	"tweets": 10791,
+	"created_at": "Tue Sep 15 17:10:46 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_09.js
+++ data/js/tweets/2020_09.js
@@ -1,6 +1,180 @@
 Grailbird.data.tweets_2020_09 = 
 [
 	{
+		"created_at": "Tue Sep 15 16:53:21 +0000 2020",
+		"id": 1305404238869061600,
+		"id_str": "1305912602678226944",
+		"full_text": "Can't not see it. https://t.co/Tcgd2e595p",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			17
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1305404236679671800,
+					"id_str": "1305404236679671808",
+					"indices": [
+						18,
+						41
+					],
+					"media_url": "http://pbs.twimg.com/media/Eh26PKeU8AABC97.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Eh26PKeU8AABC97.jpg",
+					"url": "https://t.co/Tcgd2e595p",
+					"display_url": "pic.twitter.com/Tcgd2e595p",
+					"expanded_url": "https://twitter.com/GrumpyLibrary/status/1305404238869061632/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 604,
+							"h": 652,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 604,
+							"h": 652,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 604,
+							"h": 652,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1305404236679671800,
+					"id_str": "1305404236679671808",
+					"indices": [
+						18,
+						41
+					],
+					"media_url": "http://pbs.twimg.com/media/Eh26PKeU8AABC97.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Eh26PKeU8AABC97.jpg",
+					"url": "https://t.co/Tcgd2e595p",
+					"display_url": "pic.twitter.com/Tcgd2e595p",
+					"expanded_url": "https://twitter.com/GrumpyLibrary/status/1305404238869061632/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 604,
+							"h": 652,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 604,
+							"h": 652,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 604,
+							"h": 652,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 765986080160112600,
+			"id_str": "765986080160112640",
+			"name": "The Grumpy Librarian™",
+			"screen_name": "GrumpyLibrary",
+			"location": "Care-a-lot",
+			"description": "Public Librarian. Cantankerous. Mouthy Leftist. Cusser. Loyal Friend. Perfector of the Care Bear Stare of Disapproval™.  he/him and Soy-Boy extraordinaire. ACAB",
+			"url": "https://t.co/a1Hd8wHwhd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/a1Hd8wHwhd",
+							"expanded_url": "https://whatthefuckjusthappenedtoday.com/",
+							"display_url": "whatthefuckjusthappenedtoday.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 900,
+			"friends_count": 993,
+			"listed_count": 22,
+			"created_at": "Wed Aug 17 18:58:07 +0000 2016",
+			"favourites_count": 4911,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 3852,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1215502253903118337/HWtdjZ4N_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1215502253903118337/HWtdjZ4N_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/765986080160112640/1592169244",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 122,
+		"favorite_count": 541,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Can't not see it. https://t.co/Tcgd2e595p"
+	},
+	{
 		"created_at": "Tue Sep 15 12:50:18 +0000 2020",
 		"id": 1305809211029442600,
 		"id_str": "1305851434668261376",
