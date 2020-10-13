3b8b07a09e6c162ffba0e3243d98663a9b6fb657
Zak Winnick <zakwinnick@protonmail.com>
2020-10-13 16:10:48 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10811,
-	"created_at": "Tue Oct 13 17:18:43 +0000 2020",
+	"tweets": 10812,
+	"created_at": "Tue Oct 13 21:10:47 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,180 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Tue Oct 13 20:29:48 +0000 2020",
+		"id": 1316088790843154400,
+		"id_str": "1316113934966042624",
+		"full_text": "here's a shot of the back of the iPhone 12 clear case with MagSafe https://t.co/ZAYfNwi42U",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			66
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1316088786715992000,
+					"id_str": "1316088786715992064",
+					"indices": [
+						67,
+						90
+					],
+					"media_url": "http://pbs.twimg.com/media/EkOvx1EU4AAkUGC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EkOvx1EU4AAkUGC.jpg",
+					"url": "https://t.co/ZAYfNwi42U",
+					"display_url": "pic.twitter.com/ZAYfNwi42U",
+					"expanded_url": "https://twitter.com/jsnell/status/1316088790843154434/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1316088786715992000,
+					"id_str": "1316088786715992064",
+					"indices": [
+						67,
+						90
+					],
+					"media_url": "http://pbs.twimg.com/media/EkOvx1EU4AAkUGC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EkOvx1EU4AAkUGC.jpg",
+					"url": "https://t.co/ZAYfNwi42U",
+					"display_url": "pic.twitter.com/ZAYfNwi42U",
+					"expanded_url": "https://twitter.com/jsnell/status/1316088790843154434/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1600,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://twitterrific.com/mac\" rel=\"nofollow\">Twitterrific for Mac</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 784912,
+			"id_str": "784912",
+			"name": "Jason Snell",
+			"screen_name": "jsnell",
+			"location": "Monkeyhouse, Mill Valley, CA",
+			"description": "I write about tech and make podcasts about everything. @theincomparable @_upgradefm podcaster, @bleedsixcolors writer, @macworld EIC-turned-columnist.",
+			"url": "https://t.co/5ajg3A4SPy",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/5ajg3A4SPy",
+							"expanded_url": "https://sixcolors.com/",
+							"display_url": "sixcolors.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 76894,
+			"friends_count": 404,
+			"listed_count": 4874,
+			"created_at": "Wed Feb 21 00:04:08 +0000 2007",
+			"favourites_count": 14329,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 148095,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "061C69",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/539992431496667136/qblx7qeY_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/539992431496667136/qblx7qeY_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/784912/1481057522",
+			"profile_link_color": "000066",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "3399FF",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 38,
+		"favorite_count": 358,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "here's a shot of the back of the iPhone 12 clear case with MagSafe https://t.co/ZAYfNwi42U"
+	},
+	{
 		"created_at": "Tue Oct 13 17:16:27 +0000 2020",
 		"id": 1316062659327000600,
 		"id_str": "1316065275201159175",
