3121862d40dd1cc79d0104cd13ff556975322b9a
Zak Winnick <zakwinnick@protonmail.com>
2020-10-15 10:10:47 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10812,
-	"created_at": "Tue Oct 13 21:10:47 +0000 2020",
+	"tweets": 10813,
+	"created_at": "Thu Oct 15 15:10:46 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,120 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Thu Oct 15 14:58:07 +0000 2020",
+		"id": 1316743907225596000,
+		"id_str": "1316755238670544897",
+		"full_text": ".@JamesCarville is a national treasure. \n\nHear him in an fantastic interview on @MichaelCohen212’s podcast, Mea Culpa. \n\nhttps://t.co/XNiZCgbpzO",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			144
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "JamesCarville",
+					"name": "James Carville",
+					"id": 4211625232,
+					"id_str": "4211625232",
+					"indices": [
+						1,
+						15
+					]
+				},
+				{
+					"screen_name": "MichaelCohen212",
+					"name": "Michael Cohen",
+					"id": 52136185,
+					"id_str": "52136185",
+					"indices": [
+						80,
+						96
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/XNiZCgbpzO",
+					"expanded_url": "https://overcast.fm/+jtoUI14Wk",
+					"display_url": "overcast.fm/+jtoUI14Wk",
+					"indices": [
+						121,
+						144
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1189600242615890000,
+			"id_str": "1189600242615889922",
+			"name": "Zak Winnick",
+			"screen_name": "PolitiZak",
+			"location": "",
+			"description": "The political musings of @ZakWinnick",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4,
+			"friends_count": 539,
+			"listed_count": 0,
+			"created_at": "Wed Oct 30 17:49:40 +0000 2019",
+			"favourites_count": 9,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 93,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1210981653230825472/4CTiQItl_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1210981653230825472/4CTiQItl_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1189600242615889922/1574625510",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": ".@JamesCarville is a national treasure. \n\nHear him in an fantastic interview on @MichaelCohen212’s podcast, Mea Culpa. \n\nhttps://t.co/XNiZCgbpzO"
+	},
+	{
 		"created_at": "Tue Oct 13 20:29:48 +0000 2020",
 		"id": 1316088790843154400,
 		"id_str": "1316113934966042624",
