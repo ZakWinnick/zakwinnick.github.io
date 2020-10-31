30e5ca58773cf53cd673d80ddfb91c7fc6e29679
Zak Winnick <zakwinnick@protonmail.com>
2020-10-31 11:10:47 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10825,
-	"created_at": "Sat Oct 31 13:10:46 +0000 2020",
+	"tweets": 10826,
+	"created_at": "Sat Oct 31 16:10:46 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,99 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Sat Oct 31 15:45:31 +0000 2020",
+		"id": 1322564135779971000,
+		"id_str": "1322565372608942080",
+		"full_text": "https://t.co/LG3Z6SwVMF",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			23
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/LG3Z6SwVMF",
+					"expanded_url": "https://rip007.ytmnd.com/",
+					"display_url": "rip007.ytmnd.com",
+					"indices": [
+						0,
+						23
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://ibboard.co.uk/cawbird/\" rel=\"nofollow\">Cawbird</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 279390084,
+			"id_str": "279390084",
+			"name": "Anonymous🍏",
+			"screen_name": "YourAnonNews",
+			"location": "The Interwebs",
+			"description": "We are Anonymous, we are legion, we do not forgive, we do not forget. Expect us. \n\n#Anonymous #HackThePlanet",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7008395,
+			"friends_count": 748,
+			"listed_count": 15113,
+			"created_at": "Sat Apr 09 05:10:37 +0000 2011",
+			"favourites_count": 9360,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 179739,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1276653965531283457/IhAJeBDG_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1276653965531283457/IhAJeBDG_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/279390084/1459539143",
+			"profile_link_color": "000000",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 11,
+		"favorite_count": 215,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "https://t.co/LG3Z6SwVMF"
+	},
+	{
 		"created_at": "Sat Oct 31 13:04:35 +0000 2020",
 		"id": 1322522971408314400,
 		"id_str": "1322524872505991178",
