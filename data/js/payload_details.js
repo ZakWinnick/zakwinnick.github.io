b69a58cea105d7347e046a05fbf5358d6ddd72cf
Zak Winnick <zakwinnick@protonmail.com>
2020-11-01 11:10:47 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10826,
-	"created_at": "Sat Oct 31 16:10:46 +0000 2020",
+	"tweets": 10827,
+	"created_at": "Sun Nov 01 17:10:46 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_11.js
@@ -0,0 +1,149 @@
+Grailbird.data.tweets_2020_11 = 
+[
+	{
+		"created_at": "Sun Nov 01 16:35:07 +0000 2020",
+		"id": 1322919355135496200,
+		"id_str": "1322940241632788480",
+		"full_text": "My family’s wild origin story. The Trump Superfan Who Helped Two #Gay Men Adopt a Baby - via @TheAtlantic https://t.co/oLdojjrtI7 #maga #tcot #lgbtq",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			148
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Gay",
+					"indices": [
+						65,
+						69
+					]
+				},
+				{
+					"text": "maga",
+					"indices": [
+						130,
+						135
+					]
+				},
+				{
+					"text": "tcot",
+					"indices": [
+						136,
+						141
+					]
+				},
+				{
+					"text": "lgbtq",
+					"indices": [
+						142,
+						148
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "TheAtlantic",
+					"name": "The Atlantic",
+					"id": 35773039,
+					"id_str": "35773039",
+					"indices": [
+						93,
+						105
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/oLdojjrtI7",
+					"expanded_url": "https://www.theatlantic.com/family/archive/2020/11/trump-superfan-who-helped-two-gay-men-adopt-baby/616948/",
+					"display_url": "theatlantic.com/family/archive…",
+					"indices": [
+						106,
+						129
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://www.echofon.com/\" rel=\"nofollow\">Echofon</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 16248895,
+			"id_str": "16248895",
+			"name": "Steve Friess",
+			"screen_name": "SteveFriess",
+			"location": "Ann Arbor, MI",
+			"description": "Newsweek contract contributor + News/Features Editor at Hour Detroit. Also, a freelancer. '12 Knight-Wallace fellow & Vegas expatriate s.friess @ newsweek . com",
+			"url": "https://t.co/CZpMpl8ZKe",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/CZpMpl8ZKe",
+							"expanded_url": "http://bit.ly/1k9YlG6",
+							"display_url": "bit.ly/1k9YlG6",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6983,
+			"friends_count": 6168,
+			"listed_count": 370,
+			"created_at": "Thu Sep 11 22:31:54 +0000 2008",
+			"favourites_count": 2446,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 35235,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1DD9E6",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1298718035352535042/cdGU_1wO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1298718035352535042/cdGU_1wO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/16248895/1598473485",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "E0FF92",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "My family’s wild origin story. The Trump Superfan Who Helped Two #Gay Men Adopt a Baby - via @TheAtlantic https://t.co/oLdojjrtI7 #maga #tcot #lgbtq"
+	}
+]
