a39f87d220e60a3673e01821c1f99ea6ecdf0b2f
Zak Winnick <zakwinnick@protonmail.com>
2020-12-18 18:10:47 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10895,
-	"created_at": "Fri Dec 18 01:10:46 +0000 2020",
+	"tweets": 10896,
+	"created_at": "Sat Dec 19 00:10:46 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_12.js
+++ data/js/tweets/2020_12.js
@@ -1,6 +1,99 @@
 Grailbird.data.tweets_2020_12 = 
 [
 	{
+		"created_at": "Fri Dec 18 23:33:29 +0000 2020",
+		"id": 1340064685094101000,
+		"id_str": "1340077755719933956",
+		"full_text": "@Oriana0214 My pet raccoon just got a draft notice. WTF",
+		"truncated": false,
+		"display_text_range": [
+			12,
+			55
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Oriana0214",
+					"name": "Oriana Pawlyk",
+					"id": 36607254,
+					"id_str": "36607254",
+					"indices": [
+						0,
+						11
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1340053720562843600,
+		"in_reply_to_status_id_str": "1340053720562843648",
+		"in_reply_to_user_id": 36607254,
+		"in_reply_to_user_id_str": "36607254",
+		"in_reply_to_screen_name": "Oriana0214",
+		"user": {
+			"id": 48785884,
+			"id_str": "48785884",
+			"name": "Clark Gregg",
+			"screen_name": "clarkgregg",
+			"location": "All up in this place",
+			"description": "Actor, writer, lover of the dance...",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 707230,
+			"friends_count": 1119,
+			"listed_count": 4942,
+			"created_at": "Fri Jun 19 18:30:44 +0000 2009",
+			"favourites_count": 9743,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 17005,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/705527056767913984/VUxgq15K_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/705527056767913984/VUxgq15K_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/48785884/1430172096",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "A8C7F7",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 147,
+		"favorite_count": 1369,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "@Oriana0214 My pet raccoon just got a draft notice. WTF"
+	},
+	{
 		"created_at": "Fri Dec 18 00:18:42 +0000 2020",
 		"id": 1339726749710606300,
 		"id_str": "1339726749710606336",
