b1e633b21f8a739fe7b6ec11a3c143a073477602
Zak Winnick <zakwinnick@protonmail.com>
2020-12-23 21:10:47 -0600

    Added 1 tweets

--- data/js/tweets/2020_12.js
+++ data/js/tweets/2020_12.js
@@ -1,6 +1,101 @@
 Grailbird.data.tweets_2020_12 = 
 [
 	{
+		"created_at": "Thu Dec 24 02:39:39 +0000 2020",
+		"id": 1341904298112761900,
+		"id_str": "1341936547730436098",
+		"full_text": "Trump just pardoned the bees from My Girl",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			41
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 249346453,
+			"id_str": "249346453",
+			"name": "Matt Oswalt",
+			"screen_name": "MattOswaltVA",
+			"location": "",
+			"description": "I write comedy, occasionally date your aunt, and take photos of liquor stores which you can purchase on my website.",
+			"url": "https://t.co/4mbY1nuXip",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/4mbY1nuXip",
+							"expanded_url": "https://www.mattoswaltphoto.com",
+							"display_url": "mattoswaltphoto.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 354108,
+			"friends_count": 1052,
+			"listed_count": 1576,
+			"created_at": "Tue Feb 08 21:32:12 +0000 2011",
+			"favourites_count": 73899,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 16832,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/722184632288960512/ZUh_hO61_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/722184632288960512/ZUh_hO61_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/249346453/1535948757",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2339,
+		"favorite_count": 19033,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Trump just pardoned the bees from My Girl"
+	},
+	{
 		"created_at": "Tue Dec 22 19:20:18 +0000 2020",
 		"id": 1341463544844472300,
 		"id_str": "1341463595410989059",
