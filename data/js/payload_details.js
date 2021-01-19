74e52a5535dc34e39ef39ed9d2745c1b5dd9aade
Zak Winnick <zakwinnick@protonmail.com>
2021-01-19 10:10:47 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10943,
-	"created_at": "Tue Jan 19 11:18:43 +0000 2021",
+	"tweets": 10944,
+	"created_at": "Tue Jan 19 16:10:46 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_01.js
+++ data/js/tweets/2021_01.js
@@ -1,6 +1,256 @@
 Grailbird.data.tweets_2021_01 = 
 [
 	{
+		"created_at": "Tue Jan 19 15:36:24 +0000 2021",
+		"id": 1351554108684120000,
+		"id_str": "1351554108684120067",
+		"full_text": "Aww…I loved hearing them whenever they were played on @drdemento. \n\nVery sad… https://t.co/zcCB8g5PT3",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			77
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "drdemento",
+					"name": "drdemento",
+					"id": 1289134105578348500,
+					"id_str": "1289134105578348544",
+					"indices": [
+						54,
+						64
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/zcCB8g5PT3",
+					"expanded_url": "https://twitter.com/AndrewFeinberg/status/1351552012912099328",
+					"display_url": "twitter.com/AndrewFeinberg…",
+					"indices": [
+						78,
+						101
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 9586552,
+			"id_str": "9586552",
+			"name": "Zak Winnick",
+			"screen_name": "ZakWinnick",
+			"location": "Nashville, TN",
+			"description": "MacOS System Administrator | 15+ Year Veteran Podcaster | Amateur Social Media Professional | Host @_syncinprogress | 8w3",
+			"url": "https://t.co/vGZZOYJJCo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vGZZOYJJCo",
+							"expanded_url": "https://ZakWinnick.com",
+							"display_url": "ZakWinnick.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 63,
+			"friends_count": 261,
+			"listed_count": 18,
+			"created_at": "Sun Oct 21 22:28:31 +0000 2007",
+			"favourites_count": 174,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9642,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "3F5B66",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1283034053076299778/xcEYv_ml_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1283034053076299778/xcEYv_ml_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/9586552/1589495854",
+			"profile_link_color": "483D8B",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "858080",
+			"profile_text_color": "086660",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1351552012912099300,
+		"quoted_status_id_str": "1351552012912099328",
+		"quoted_status_permalink": {
+			"url": "https://t.co/zcCB8g5PT3",
+			"expanded": "https://twitter.com/AndrewFeinberg/status/1351552012912099328",
+			"display": "twitter.com/AndrewFeinberg…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Jan 19 15:28:05 +0000 2021",
+			"id": 1351552012912099300,
+			"id_str": "1351552012912099328",
+			"full_text": "Incredibly sad news.\n\nAfter nearly 40 years, the @Capsteps — the musical political satire act that was skewering politicians when @RandyRainbow was in diapers (literally) — is closing up shop due to the effects of the COVID-19 pandemic. https://t.co/YrJCusYgBi",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				260
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "Capsteps",
+						"name": "Capitol Steps",
+						"id": 68770853,
+						"id_str": "68770853",
+						"indices": [
+							49,
+							58
+						]
+					},
+					{
+						"screen_name": "RandyRainbow",
+						"name": "Randy Rainbow",
+						"id": 7786612,
+						"id_str": "7786612",
+						"indices": [
+							130,
+							143
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/YrJCusYgBi",
+						"expanded_url": "https://wtop.com/entertainment/2021/01/capitol-steps-leave-hilarious-legacy-after-four-decades-of-clever-political-comedy/",
+						"display_url": "wtop.com/entertainment/…",
+						"indices": [
+							237,
+							260
+						]
+					}
+				]
+			},
+			"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 2249531,
+				"id_str": "2249531",
+				"name": "Andrew Feinberg",
+				"screen_name": "AndrewFeinberg",
+				"location": "Washington, DC",
+				"description": "Reporter covering White House, Hill, 2020, etc | seen in @Independent, Breakfast Media and others | tips: andrew @ andrewfeinberg-dotcom |dm 4 Signal/ProtonMail",
+				"url": "https://t.co/86vCCURE8n",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/86vCCURE8n",
+								"expanded_url": "https://www.muckrack.com/andrewfeinberg",
+								"display_url": "muckrack.com/andrewfeinberg",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 114574,
+				"friends_count": 7468,
+				"listed_count": 1192,
+				"created_at": "Mon Mar 26 03:59:32 +0000 2007",
+				"favourites_count": 5139,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 59144,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "9AE4E8",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1188827397073854470/POwzzbMn_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1188827397073854470/POwzzbMn_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/2249531/1601078842",
+				"profile_link_color": "273A59",
+				"profile_sidebar_border_color": "87BC44",
+				"profile_sidebar_fill_color": "E0FF92",
+				"profile_text_color": "000000",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 11,
+			"favorite_count": 34,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Aww…I loved hearing them whenever they were played on @drdemento. \n\nVery sad… https://t.co/zcCB8g5PT3"
+	},
+	{
 		"created_at": "Tue Jan 19 11:13:23 +0000 2021",
 		"id": 1351487917580030000,
 		"id_str": "1351487917580029953",
