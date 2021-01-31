aae0ca46bdaca1126194a370c9405553d0b3d97f
Zak Winnick <zakwinnick@protonmail.com>
2021-01-31 15:10:47 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10956,
-	"created_at": "Sun Jan 31 01:10:46 +0000 2021",
+	"tweets": 10957,
+	"created_at": "Sun Jan 31 21:10:46 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_01.js
+++ data/js/tweets/2021_01.js
@@ -1,6 +1,167 @@
 Grailbird.data.tweets_2021_01 = 
 [
 	{
+		"created_at": "Sun Jan 31 20:43:06 +0000 2021",
+		"id": 1355979393471029200,
+		"id_str": "1355979944959086595",
+		"full_text": "BREAKING NEWS: President Biden signs executive order to increase free iCloud storage to 6GB by 2025 https://t.co/cb78BkfW11",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			99
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1355979334939500500,
+					"id_str": "1355979334939500548",
+					"indices": [
+						100,
+						123
+					],
+					"media_url": "http://pbs.twimg.com/media/EtFoBVXW4AQWFgk.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EtFoBVXW4AQWFgk.jpg",
+					"url": "https://t.co/cb78BkfW11",
+					"display_url": "pic.twitter.com/cb78BkfW11",
+					"expanded_url": "https://twitter.com/Creative_Rants/status/1355979393471029251/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1029,
+							"h": 677,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1029,
+							"h": 677,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 447,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1355979334939500500,
+					"id_str": "1355979334939500548",
+					"indices": [
+						100,
+						123
+					],
+					"media_url": "http://pbs.twimg.com/media/EtFoBVXW4AQWFgk.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EtFoBVXW4AQWFgk.jpg",
+					"url": "https://t.co/cb78BkfW11",
+					"display_url": "pic.twitter.com/cb78BkfW11",
+					"expanded_url": "https://twitter.com/Creative_Rants/status/1355979393471029251/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1029,
+							"h": 677,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1029,
+							"h": 677,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 447,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 824661439,
+			"id_str": "824661439",
+			"name": "Creative Rants",
+			"screen_name": "Creative_Rants",
+			"location": "Today at Apple sessions",
+			"description": "The personal rants of an Apple Store Creative",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8885,
+			"friends_count": 100,
+			"listed_count": 95,
+			"created_at": "Sat Sep 15 06:08:00 +0000 2012",
+			"favourites_count": 3882,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 6362,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "B2DFDA",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/2611871813/48h23l4h3wkw4v0sx5vw_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/2611871813/48h23l4h3wkw4v0sx5vw_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/824661439/1458601433",
+			"profile_link_color": "93A644",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 11,
+		"favorite_count": 103,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "BREAKING NEWS: President Biden signs executive order to increase free iCloud storage to 6GB by 2025 https://t.co/cb78BkfW11"
+	},
+	{
 		"created_at": "Sun Jan 31 01:01:39 +0000 2021",
 		"id": 1355668587516715000,
 		"id_str": "1355682625403432971",
