1555e677cb4795c4883adc5f829c99e297ea81be
Zak Winnick <zakwinnick@protonmail.com>
2021-02-03 08:10:47 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10958,
-	"created_at": "Mon Feb 01 00:10:46 +0000 2021",
+	"tweets": 10959,
+	"created_at": "Wed Feb 03 14:10:46 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_02.js
@@ -0,0 +1,196 @@
+Grailbird.data.tweets_2021_02 = 
+[
+	{
+		"created_at": "Wed Feb 03 13:54:47 +0000 2021",
+		"id": 1356962976004268000,
+		"id_str": "1356964353321369600",
+		"full_text": "Today we mark the 3rd anniversary of the #BowlingGreenMassacre\nResistors by the thousands bravely stood wholly unaware of the danger they weren't in.\nRT if you too survived that bloodless day &amp; maybe pour out a non fat almond latte for @KellyannePolls https://t.co/H4NPoje1Zi",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			255
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "BowlingGreenMassacre",
+					"indices": [
+						41,
+						62
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "KellyannePolls",
+					"name": "Kellyanne Conway",
+					"id": 471672239,
+					"id_str": "471672239",
+					"indices": [
+						240,
+						255
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1356962974540382200,
+					"id_str": "1356962974540382212",
+					"indices": [
+						256,
+						279
+					],
+					"media_url": "http://pbs.twimg.com/media/EtTmosbWQAQhmvY.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EtTmosbWQAQhmvY.jpg",
+					"url": "https://t.co/H4NPoje1Zi",
+					"display_url": "pic.twitter.com/H4NPoje1Zi",
+					"expanded_url": "https://twitter.com/AltSpaceForce1/status/1356962976004268039/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1079,
+							"h": 996,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 628,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1079,
+							"h": 996,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1356962974540382200,
+					"id_str": "1356962974540382212",
+					"indices": [
+						256,
+						279
+					],
+					"media_url": "http://pbs.twimg.com/media/EtTmosbWQAQhmvY.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EtTmosbWQAQhmvY.jpg",
+					"url": "https://t.co/H4NPoje1Zi",
+					"display_url": "pic.twitter.com/H4NPoje1Zi",
+					"expanded_url": "https://twitter.com/AltSpaceForce1/status/1356962976004268039/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1079,
+							"h": 996,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 628,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1079,
+							"h": 996,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1009553795918057500,
+			"id_str": "1009553795918057472",
+			"name": "AltSpaceForce 🚀🇺🇸",
+			"screen_name": "AltSpaceForce1",
+			"location": "The Final Frontier",
+			"description": "Boldly Going Where No Jedi Has Beamed Before\n\n\nProud member of #TheResistance #AltGov #AltFam #AltGovCares\nEarth is our spaceship.",
+			"url": "https://t.co/FXCQeL6PcG",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/FXCQeL6PcG",
+							"expanded_url": "https://www.altgovcares.com",
+							"display_url": "altgovcares.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10951,
+			"friends_count": 727,
+			"listed_count": 76,
+			"created_at": "Wed Jun 20 21:49:16 +0000 2018",
+			"favourites_count": 94344,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 32250,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1163929037070360576/UN4G2ISM_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1163929037070360576/UN4G2ISM_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1009553795918057472/1534039520",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 9,
+		"favorite_count": 33,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Today we mark the 3rd anniversary of the #BowlingGreenMassacre\nResistors by the thousands bravely stood wholly unaware of the danger they weren't in.\nRT if you too survived that bloodless day &amp; maybe pour out a non fat almond latte for @KellyannePolls https://t.co/H4NPoje1Zi"
+	}
+]
