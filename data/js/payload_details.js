61d3a808a1d91980ba84c75590148e509019de47
Zak Winnick <zakwinnick@protonmail.com>
2021-03-02 17:10:45 -0600

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10970,
-	"created_at": "Fri Feb 26 12:10:45 +0000 2021",
+	"tweets": 10971,
+	"created_at": "Tue Mar 02 23:10:45 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_03.js
@@ -0,0 +1,317 @@
+Grailbird.data.tweets_2021_03 = 
+[
+	{
+		"created_at": "Tue Mar 02 23:02:13 +0000 2021",
+		"id": 1366886591856324600,
+		"id_str": "1366886591856324612",
+		"full_text": "🤯\n\nThey truly were (are?) geniuses. Who thinks of something like this? 👽 https://t.co/WNq4xp7k3D",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			72
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/WNq4xp7k3D",
+					"expanded_url": "https://twitter.com/NehhLmao/status/1363925150958317568",
+					"display_url": "twitter.com/NehhLmao/statu…",
+					"indices": [
+						73,
+						96
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 9586552,
+			"id_str": "9586552",
+			"name": "Zak Winnick",
+			"screen_name": "ZakWinnick",
+			"location": "Nashville, TN",
+			"description": "Nashville-based MacOS System Administrator, 15+ Year Veteran Podcaster, and Electric Vehicle Evangelist | https://t.co/8yLe5VGR5c",
+			"url": "https://t.co/vGZZOYJJCo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vGZZOYJJCo",
+							"expanded_url": "https://ZakWinnick.com",
+							"display_url": "ZakWinnick.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/8yLe5VGR5c",
+							"expanded_url": "http://ts.la/zak8917",
+							"display_url": "ts.la/zak8917",
+							"indices": [
+								106,
+								129
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 63,
+			"friends_count": 262,
+			"listed_count": 18,
+			"created_at": "Sun Oct 21 22:28:31 +0000 2007",
+			"favourites_count": 176,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9668,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "3F5B66",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1357102829052309504/LTs-DnBU_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1357102829052309504/LTs-DnBU_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/9586552/1612393503",
+			"profile_link_color": "483D8B",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "858080",
+			"profile_text_color": "086660",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1363925150958317600,
+		"quoted_status_id_str": "1363925150958317568",
+		"quoted_status_permalink": {
+			"url": "https://t.co/WNq4xp7k3D",
+			"expanded": "https://twitter.com/NehhLmao/status/1363925150958317568",
+			"display": "twitter.com/NehhLmao/statu…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Feb 22 18:54:31 +0000 2021",
+			"id": 1363925150958317600,
+			"id_str": "1363925150958317568",
+			"full_text": "The way daft punk flipped the sample for one more time is still so crazy to me https://t.co/WIbKWKa1CD",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				78
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1363925098206478300,
+						"id_str": "1363925098206478337",
+						"indices": [
+							79,
+							102
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1363925098206478337/pu/img/QN7P5fUlREUkKKZD.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1363925098206478337/pu/img/QN7P5fUlREUkKKZD.jpg",
+						"url": "https://t.co/WIbKWKa1CD",
+						"display_url": "pic.twitter.com/WIbKWKa1CD",
+						"expanded_url": "https://twitter.com/NehhLmao/status/1363925150958317568/video/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 679,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 724,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 385,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1363925098206478300,
+						"id_str": "1363925098206478337",
+						"indices": [
+							79,
+							102
+						],
+						"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1363925098206478337/pu/img/QN7P5fUlREUkKKZD.jpg",
+						"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1363925098206478337/pu/img/QN7P5fUlREUkKKZD.jpg",
+						"url": "https://t.co/WIbKWKa1CD",
+						"display_url": "pic.twitter.com/WIbKWKa1CD",
+						"expanded_url": "https://twitter.com/NehhLmao/status/1363925150958317568/video/1",
+						"type": "video",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 679,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1280,
+								"h": 724,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 385,
+								"resize": "fit"
+							}
+						},
+						"video_info": {
+							"aspect_ratio": [
+								320,
+								181
+							],
+							"duration_millis": 47243,
+							"variants": [
+								{
+									"bitrate": 832000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1363925098206478337/pu/vid/636x360/CwlbIeDXDdjHzNv0.mp4?tag=10"
+								},
+								{
+									"bitrate": 256000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1363925098206478337/pu/vid/476x270/JB9xuYc_LIOPirMA.mp4?tag=10"
+								},
+								{
+									"content_type": "application/x-mpegURL",
+									"url": "https://video.twimg.com/ext_tw_video/1363925098206478337/pu/pl/lyMnFIwuXGD8ZjLj.m3u8?tag=10"
+								},
+								{
+									"bitrate": 2176000,
+									"content_type": "video/mp4",
+									"url": "https://video.twimg.com/ext_tw_video/1363925098206478337/pu/vid/1272x720/wh9wz7mtrV6zM5qv.mp4?tag=10"
+								}
+							]
+						},
+						"additional_media_info": {
+							"monetizable": false
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 386341016,
+				"id_str": "386341016",
+				"name": "Neh",
+				"screen_name": "NehhLmao",
+				"location": "",
+				"description": "The perfect combination of Carlton and Will",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1108,
+				"friends_count": 122,
+				"listed_count": 54,
+				"created_at": "Fri Oct 07 02:44:17 +0000 2011",
+				"favourites_count": 6968,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 4739,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "131516",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1356721749359030276/vCN91CmD_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1356721749359030276/vCN91CmD_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/386341016/1593722622",
+				"profile_link_color": "131516",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "EFEFEF",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 128163,
+			"favorite_count": 541167,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "🤯\n\nThey truly were (are?) geniuses. Who thinks of something like this? 👽 https://t.co/WNq4xp7k3D"
+	}
+]
