7f425e1418e0c497d45a6c4b53e0ebfefd11b2d3
Zak Winnick <zakwinnick@protonmail.com>
2021-03-15 09:10:46 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10976,
-	"created_at": "Fri Mar 12 00:10:46 +0000 2021",
+	"tweets": 10977,
+	"created_at": "Mon Mar 15 14:10:45 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_03.js
+++ data/js/tweets/2021_03.js
@@ -1,6 +1,101 @@
 Grailbird.data.tweets_2021_03 = 
 [
 	{
+		"created_at": "Mon Mar 15 13:26:00 +0000 2021",
+		"id": 1371451090659639300,
+		"id_str": "1371452624457842690",
+		"full_text": "\"Cinematography\" is frequently understood by Academy voters to be \"Landscapes and sunsets\" not \"filming a movie where backwards people are running around fighting forwards people and buildings are exploding and unexploding at once\" which is why TENET wasn't nominated",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			267
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 122231268,
+			"id_str": "122231268",
+			"name": "Jesse Hawken",
+			"screen_name": "jessehawken",
+			"location": "Toronto",
+			"description": "Son of Earth. Writer/filmmaker. Host and producer of @junkfilterpod. Resident of Bloordale.",
+			"url": "https://t.co/z7soJ71hce",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/z7soJ71hce",
+							"expanded_url": "https://www.youtube.com/user/telekino",
+							"display_url": "youtube.com/user/telekino",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11673,
+			"friends_count": 1949,
+			"listed_count": 165,
+			"created_at": "Fri Mar 12 01:38:52 +0000 2010",
+			"favourites_count": 71822,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 170689,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/559505710107459584/mG8rNZEr_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/559505710107459584/mG8rNZEr_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/122231268/1371175031",
+			"profile_link_color": "1A54B8",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "F3F3F3",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 30,
+		"favorite_count": 204,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "\"Cinematography\" is frequently understood by Academy voters to be \"Landscapes and sunsets\" not \"filming a movie where backwards people are running around fighting forwards people and buildings are exploding and unexploding at once\" which is why TENET wasn't nominated"
+	},
+	{
 		"created_at": "Thu Mar 11 23:20:37 +0000 2021",
 		"id": 1370152712050970600,
 		"id_str": "1370152712050970624",
