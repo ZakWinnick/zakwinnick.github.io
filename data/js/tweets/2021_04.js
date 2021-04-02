80ccd52ca774fb5cdaec313e1f1881a8dcad22d1
Zak Winnick <zakwinnick@protonmail.com>
2021-04-01 20:18:42 -0500

    Added 1 tweets

---
+++ data/js/tweets/2021_04.js
@@ -0,0 +1,425 @@
+Grailbird.data.tweets_2021_04 = 
+[
+	{
+		"created_at": "Fri Apr 02 01:13:00 +0000 2021",
+		"id": 1377791140246741000,
+		"id_str": "1377791140246740997",
+		"full_text": "I need that BART train, and Roosevelt Island tram car. 😍 https://t.co/AzqA27kl0p",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			56
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/AzqA27kl0p",
+					"expanded_url": "https://twitter.com/thrillgeek/status/1377781910617927684",
+					"display_url": "twitter.com/thrillgeek/sta…",
+					"indices": [
+						57,
+						80
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 9586552,
+			"id_str": "9586552",
+			"name": "Zak Winnick",
+			"screen_name": "ZakWinnick",
+			"location": "Nashville, TN",
+			"description": "Nashville-based MacOS System Administrator, 15+ Year Veteran Podcaster, and Electric Vehicle Evangelist | https://t.co/8yLe5VGR5c",
+			"url": "https://t.co/vGZZOYJJCo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vGZZOYJJCo",
+							"expanded_url": "https://ZakWinnick.com",
+							"display_url": "ZakWinnick.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/8yLe5VGR5c",
+							"expanded_url": "http://ts.la/zak8917",
+							"display_url": "ts.la/zak8917",
+							"indices": [
+								106,
+								129
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 62,
+			"friends_count": 234,
+			"listed_count": 18,
+			"created_at": "Sun Oct 21 22:28:31 +0000 2007",
+			"favourites_count": 180,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9679,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "3F5B66",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1357102829052309504/LTs-DnBU_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1357102829052309504/LTs-DnBU_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/9586552/1612393503",
+			"profile_link_color": "483D8B",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "858080",
+			"profile_text_color": "086660",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1377781910617927700,
+		"quoted_status_id_str": "1377781910617927684",
+		"quoted_status_permalink": {
+			"url": "https://t.co/AzqA27kl0p",
+			"expanded": "https://twitter.com/thrillgeek/status/1377781910617927684",
+			"display": "twitter.com/thrillgeek/sta…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Apr 02 00:36:20 +0000 2021",
+			"id": 1377781910617927700,
+			"id_str": "1377781910617927684",
+			"full_text": "If you visit the new Universal Studios Store, be sure to also check out the new Universal Legacy Store. And take your time. There’s TONS of theme park memorabilia to 👀 at! #UniversalOrlando https://t.co/GvwQRuxpLB",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				189
+			],
+			"entities": {
+				"hashtags": [
+					{
+						"text": "UniversalOrlando",
+						"indices": [
+							172,
+							189
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1377781896961220600,
+						"id_str": "1377781896961220612",
+						"indices": [
+							190,
+							213
+						],
+						"media_url": "http://pbs.twimg.com/media/Ex7dVeJWYAQhAy-.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Ex7dVeJWYAQhAy-.jpg",
+						"url": "https://t.co/GvwQRuxpLB",
+						"display_url": "pic.twitter.com/GvwQRuxpLB",
+						"expanded_url": "https://twitter.com/thrillgeek/status/1377781910617927684/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 900,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1536,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 510,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1377781896961220600,
+						"id_str": "1377781896961220612",
+						"indices": [
+							190,
+							213
+						],
+						"media_url": "http://pbs.twimg.com/media/Ex7dVeJWYAQhAy-.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Ex7dVeJWYAQhAy-.jpg",
+						"url": "https://t.co/GvwQRuxpLB",
+						"display_url": "pic.twitter.com/GvwQRuxpLB",
+						"expanded_url": "https://twitter.com/thrillgeek/status/1377781910617927684/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 900,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1536,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 510,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 1377781897179361300,
+						"id_str": "1377781897179361280",
+						"indices": [
+							190,
+							213
+						],
+						"media_url": "http://pbs.twimg.com/media/Ex7dVe9W8AAG2QA.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Ex7dVe9W8AAG2QA.jpg",
+						"url": "https://t.co/GvwQRuxpLB",
+						"display_url": "pic.twitter.com/GvwQRuxpLB",
+						"expanded_url": "https://twitter.com/thrillgeek/status/1377781910617927684/photo/1",
+						"type": "photo",
+						"sizes": {
+							"medium": {
+								"w": 1200,
+								"h": 900,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 510,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1536,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 1377781897435156500,
+						"id_str": "1377781897435156482",
+						"indices": [
+							190,
+							213
+						],
+						"media_url": "http://pbs.twimg.com/media/Ex7dVf6WEAIHrJy.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Ex7dVf6WEAIHrJy.jpg",
+						"url": "https://t.co/GvwQRuxpLB",
+						"display_url": "pic.twitter.com/GvwQRuxpLB",
+						"expanded_url": "https://twitter.com/thrillgeek/status/1377781910617927684/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 900,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1536,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 510,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 1377781897686880300,
+						"id_str": "1377781897686880259",
+						"indices": [
+							190,
+							213
+						],
+						"media_url": "http://pbs.twimg.com/media/Ex7dVg2XEAM9OnW.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/Ex7dVg2XEAM9OnW.jpg",
+						"url": "https://t.co/GvwQRuxpLB",
+						"display_url": "pic.twitter.com/GvwQRuxpLB",
+						"expanded_url": "https://twitter.com/thrillgeek/status/1377781910617927684/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 510,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 900,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1536,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1069027322,
+				"id_str": "1069027322",
+				"name": "🎢 ThrillGeek 🎥",
+				"screen_name": "thrillgeek",
+				"location": "Orlando, FL",
+				"description": "Theme park and pop culture updates from 🏰 Disney, 🌎 Universal and more - 📺 Come join over 50k YouTube subscribers 👉 https://t.co/NQ4SGHVhOi",
+				"url": "https://t.co/gZx3Q2k8JZ",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/gZx3Q2k8JZ",
+								"expanded_url": "http://thrillgeek.com",
+								"display_url": "thrillgeek.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/NQ4SGHVhOi",
+								"expanded_url": "http://YouTube.com/ThrillGeek",
+								"display_url": "YouTube.com/ThrillGeek",
+								"indices": [
+									116,
+									139
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 11674,
+				"friends_count": 821,
+				"listed_count": 167,
+				"created_at": "Mon Jan 07 19:06:15 +0000 2013",
+				"favourites_count": 88312,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 68421,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1345853847537909760/uYJh3ycq_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1345853847537909760/uYJh3ycq_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1069027322/1614259202",
+				"profile_link_color": "1B95E0",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2,
+			"favorite_count": 22,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I need that BART train, and Roosevelt Island tram car. 😍 https://t.co/AzqA27kl0p"
+	}
+]
