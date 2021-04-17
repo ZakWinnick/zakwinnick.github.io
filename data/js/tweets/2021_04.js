8c50b5502aff4bddb37dcd5a40943d79b079e2b3
Zak Winnick <zakwinnick@protonmail.com>
2021-04-16 21:10:46 -0500

    Added 1 tweets

--- data/js/tweets/2021_04.js
+++ data/js/tweets/2021_04.js
@@ -1,6 +1,102 @@
 Grailbird.data.tweets_2021_04 = 
 [
 	{
+		"created_at": "Sat Apr 17 02:03:27 +0000 2021",
+		"id": 1381760556110078000,
+		"id_str": "1383239654158213124",
+		"full_text": "women from Wisconsin use Easy Cheese in the bedroom instead of Redi-Whip",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			72
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 17925939,
+			"id_str": "17925939",
+			"name": "half-vacci Nate d ☕️ 🧙‍♂️",
+			"screen_name": "perlhack",
+			"location": "East Beep",
+			"description": "tweets about puns, portmanteaux, mental health, therapy, and pancakes vs waffles. Black Lives Matter. He/Him",
+			"url": "https://t.co/f1iDpZtKK7",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/f1iDpZtKK7",
+							"expanded_url": "https://twitter.com/search?q=(from%3Aperlhack)%20min_faves%3A50%20-filter%3Areplies&f=live",
+							"display_url": "twitter.com/search?q=(from…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11286,
+			"friends_count": 5810,
+			"listed_count": 116,
+			"created_at": "Sat Dec 06 18:16:42 +0000 2008",
+			"favourites_count": 343254,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 77199,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1370164334781984768/g-IlFHyu_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1370164334781984768/g-IlFHyu_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/17925939/1617911900",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 84,
+		"favorite_count": 210,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "women from Wisconsin use Easy Cheese in the bedroom instead of Redi-Whip"
+	},
+	{
 		"created_at": "Fri Apr 16 21:57:56 +0000 2021",
 		"id": 1383177869560529000,
 		"id_str": "1383177869560528910",
