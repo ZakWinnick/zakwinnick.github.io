b69f70b36e1632145e5feb5753ee1848aa38db76
Zak Winnick <zakwinnick@protonmail.com>
2021-05-29 12:10:45 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 11001,
-	"created_at": "Sat May 29 02:10:45 +0000 2021",
+	"tweets": 11002,
+	"created_at": "Sat May 29 17:10:45 +0000 2021",
 	"lang": "en"
 }
--- data/js/tweets/2021_05.js
+++ data/js/tweets/2021_05.js
@@ -1,6 +1,225 @@
 Grailbird.data.tweets_2021_05 = 
 [
 	{
+		"created_at": "Sat May 29 17:01:49 +0000 2021",
+		"id": 1398686027322175500,
+		"id_str": "1398686027322175491",
+		"full_text": "Aren’t they sterilized after every use anyway (even pre-pandemic)?\n\n👀 https://t.co/XO82kbQvbw",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			69
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/XO82kbQvbw",
+					"expanded_url": "https://twitter.com/UniStudios/status/1398663672935981060",
+					"display_url": "twitter.com/UniStudios/sta…",
+					"indices": [
+						70,
+						93
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://tapbots.com/tweetbot\" rel=\"nofollow\">Tweetbot for iΟS</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 9586552,
+			"id_str": "9586552",
+			"name": "Zak Winnick",
+			"screen_name": "ZakWinnick",
+			"location": "Nashville, TN",
+			"description": "Nashville-based MacOS System Administrator, 15+ Year Veteran Podcaster, and Electric Vehicle Evangelist | https://t.co/8yLe5VGR5c",
+			"url": "https://t.co/vGZZOYJJCo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/vGZZOYJJCo",
+							"expanded_url": "https://ZakWinnick.com",
+							"display_url": "ZakWinnick.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/8yLe5VGR5c",
+							"expanded_url": "http://ts.la/zak8917",
+							"display_url": "ts.la/zak8917",
+							"indices": [
+								106,
+								129
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 60,
+			"friends_count": 212,
+			"listed_count": 18,
+			"created_at": "Sun Oct 21 22:28:31 +0000 2007",
+			"favourites_count": 167,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 9700,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": true,
+			"profile_background_color": "3F5B66",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1395036528343199744/z2-qavA9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1395036528343199744/z2-qavA9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/9586552/1612393503",
+			"profile_link_color": "483D8B",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "858080",
+			"profile_text_color": "086660",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1398663672935981000,
+		"quoted_status_id_str": "1398663672935981060",
+		"quoted_status_permalink": {
+			"url": "https://t.co/XO82kbQvbw",
+			"expanded": "https://twitter.com/UniStudios/status/1398663672935981060",
+			"display": "twitter.com/UniStudios/sta…"
+		},
+		"quoted_status": {
+			"created_at": "Sat May 29 15:33:00 +0000 2021",
+			"id": 1398663672935981000,
+			"id_str": "1398663672935981060",
+			"full_text": "The CDC says don't put on the 3D glasses yet.",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				45
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"https://www.sprinklr.com\" rel=\"nofollow\">Sprinklr</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 33578424,
+				"id_str": "33578424",
+				"name": "Universal Studios Hollywood",
+				"screen_name": "UniStudios",
+				"location": "Universal City, CA",
+				"description": "We're sorry we can't come to the tweet right now, but if you leave your handle and a brief reply, we'll get back to you as soon as we can. Thanks! *BEEEEEEEP*",
+				"url": "https://t.co/lGEvzgwUVN",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/lGEvzgwUVN",
+								"expanded_url": "http://bit.ly/2SxKTwY",
+								"display_url": "bit.ly/2SxKTwY",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 352232,
+				"friends_count": 68396,
+				"listed_count": 1605,
+				"created_at": "Mon Apr 20 16:52:59 +0000 2009",
+				"favourites_count": 8875,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 21699,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "1A1B1F",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1228337756381814785/wTZ8UODR_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1228337756381814785/wTZ8UODR_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/33578424/1581696585",
+				"profile_link_color": "3B94D9",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "252429",
+				"profile_text_color": "666666",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 22,
+			"favorite_count": 265,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Aren’t they sterilized after every use anyway (even pre-pandemic)?\n\n👀 https://t.co/XO82kbQvbw"
+	},
+	{
 		"created_at": "Sat May 29 01:41:35 +0000 2021",
 		"id": 1398433429834801200,
 		"id_str": "1398454440085102597",
