fc83fb83618badb4592ef7cd4abc03a388812d9a
Zak Winnick <zakwinnick@protonmail.com>
2021-06-05 12:10:47 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 11005,
-	"created_at": "Sun May 30 17:10:45 +0000 2021",
+	"tweets": 11006,
+	"created_at": "Sat Jun 05 17:10:46 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_06.js
@@ -0,0 +1,290 @@
+Grailbird.data.tweets_2021_06 = 
+[
+	{
+		"created_at": "Sat Jun 05 16:52:21 +0000 2021",
+		"id": 1401077132554604500,
+		"id_str": "1401220356736794633",
+		"full_text": "https://t.co/HpPdvNhIvm https://t.co/TdXwwnK7fL",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			23
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/HpPdvNhIvm",
+					"expanded_url": "https://twitter.com/zproductionz/status/1400889914237980672",
+					"display_url": "twitter.com/zproductionz/s…",
+					"indices": [
+						0,
+						23
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1401077129413021700,
+					"id_str": "1401077129413021702",
+					"indices": [
+						24,
+						47
+					],
+					"media_url": "http://pbs.twimg.com/media/E3GgOdmWUAYKHcv.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E3GgOdmWUAYKHcv.jpg",
+					"url": "https://t.co/TdXwwnK7fL",
+					"display_url": "pic.twitter.com/TdXwwnK7fL",
+					"expanded_url": "https://twitter.com/AndreaMann/status/1401077132554604544/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 420,
+							"h": 319,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 420,
+							"h": 319,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 420,
+							"h": 319,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1401077129413021700,
+					"id_str": "1401077129413021702",
+					"indices": [
+						24,
+						47
+					],
+					"media_url": "http://pbs.twimg.com/media/E3GgOdmWUAYKHcv.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E3GgOdmWUAYKHcv.jpg",
+					"url": "https://t.co/TdXwwnK7fL",
+					"display_url": "pic.twitter.com/TdXwwnK7fL",
+					"expanded_url": "https://twitter.com/AndreaMann/status/1401077132554604544/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 420,
+							"h": 319,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 420,
+							"h": 319,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 420,
+							"h": 319,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 23286093,
+			"id_str": "23286093",
+			"name": "Andrea Mann",
+			"screen_name": "AndreaMann",
+			"location": "London SW1",
+			"description": "Writing, film, music, theatre, politics, cute animals (order may vary).",
+			"url": "https://t.co/W7uguG3CNX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/W7uguG3CNX",
+							"expanded_url": "http://www.andreamann.com",
+							"display_url": "andreamann.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 12605,
+			"friends_count": 13801,
+			"listed_count": 316,
+			"created_at": "Sun Mar 08 07:04:10 +0000 2009",
+			"favourites_count": 38110,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 76844,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1371431284816412672/K1e-Wew0_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1371431284816412672/K1e-Wew0_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/23286093/1620419873",
+			"profile_link_color": "52CCCC",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1400889914237980700,
+		"quoted_status_id_str": "1400889914237980672",
+		"quoted_status_permalink": {
+			"url": "https://t.co/HpPdvNhIvm",
+			"expanded": "https://twitter.com/zproductionz/status/1400889914237980672",
+			"display": "twitter.com/zproductionz/s…"
+		},
+		"quoted_status": {
+			"created_at": "Fri Jun 04 18:59:17 +0000 2021",
+			"id": 1400889914237980700,
+			"id_str": "1400889914237980672",
+			"full_text": "JOKER. CRUELLA. MALIFICENT. What’s the next villain solo film you’d like to see?",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				80
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1152419076,
+				"id_str": "1152419076",
+				"name": "Noah Levine",
+				"screen_name": "ZProductionz",
+				"location": "Camp Crystal Lake",
+				"description": "Filmmaker/Journalist. Creator of shockingly creepy horror shorts at ZOMBOY PRODUCTIONZ. Bylines: @TheDailyTexan @BDisgusting @fullcirclecine #UT22",
+				"url": "https://t.co/isy7uEXZEj",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/isy7uEXZEj",
+								"expanded_url": "http://www.youtube.com/user/ZomboyProductionz/featured",
+								"display_url": "youtube.com/user/ZomboyPro…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1339,
+				"friends_count": 699,
+				"listed_count": 32,
+				"created_at": "Wed Feb 06 00:20:31 +0000 2013",
+				"favourites_count": 158658,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 19842,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "1A1B1F",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1328457527722446848/Bt2jAO4v_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1328457527722446848/Bt2jAO4v_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1152419076/1595786344",
+				"profile_link_color": "981CEB",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "252429",
+				"profile_text_color": "666666",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none",
+				"withheld_in_countries": []
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 37,
+			"favorite_count": 614,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 777,
+		"favorite_count": 7303,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "https://t.co/HpPdvNhIvm https://t.co/TdXwwnK7fL"
+	}
+]
