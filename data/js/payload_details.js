903109109ef5052cd5f069bfca049ead6d7c96d0
Zak Winnick <zakwinnick@protonmail.com>
2021-07-03 10:10:46 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 11020,
-	"created_at": "Wed Jun 30 11:10:45 +0000 2021",
+	"tweets": 11021,
+	"created_at": "Sat Jul 03 15:10:45 +0000 2021",
 	"lang": "en"
 }
---
+++ data/js/tweets/2021_07.js
@@ -0,0 +1,186 @@
+Grailbird.data.tweets_2021_07 = 
+[
+	{
+		"created_at": "Sat Jul 03 14:38:44 +0000 2021",
+		"id": 1411019350774005800,
+		"id_str": "1411333593998106625",
+		"full_text": "63 years ago, a “star” was born- the Stardust to be exact!\nThe Stardust opened near the end of the era of atomic tourism. Sputnik orbited the Earth the year prior to the opening, heralding the dawn of the Space Age &amp; inspiring the theme of the casino resort. #NeonMuseumLasVegas https://t.co/qUXrZlI1FD",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			282
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "NeonMuseumLasVegas",
+					"indices": [
+						263,
+						282
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1411019348953682000,
+					"id_str": "1411019348953681929",
+					"indices": [
+						283,
+						306
+					],
+					"media_url": "http://pbs.twimg.com/media/E5TynzmWUAkTozF.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E5TynzmWUAkTozF.jpg",
+					"url": "https://t.co/qUXrZlI1FD",
+					"display_url": "pic.twitter.com/qUXrZlI1FD",
+					"expanded_url": "https://twitter.com/NeonMuseum/status/1411019350774005760/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 454,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 801,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 801,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1411019348953682000,
+					"id_str": "1411019348953681929",
+					"indices": [
+						283,
+						306
+					],
+					"media_url": "http://pbs.twimg.com/media/E5TynzmWUAkTozF.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E5TynzmWUAkTozF.jpg",
+					"url": "https://t.co/qUXrZlI1FD",
+					"display_url": "pic.twitter.com/qUXrZlI1FD",
+					"expanded_url": "https://twitter.com/NeonMuseum/status/1411019350774005760/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 454,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 801,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 801,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.hootsuite.com\" rel=\"nofollow\">Hootsuite Inc.</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 43383876,
+			"id_str": "43383876",
+			"name": "The Neon Museum",
+			"screen_name": "NeonMuseum",
+			"location": "Las Vegas, NV",
+			"description": "✨ The history of Las Vegas through signage ✨ #NeonMuseum #NeonBoneyard 501c3 non-profit 💡 Free admission for members!",
+			"url": "https://t.co/BpjXSrjXes",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/BpjXSrjXes",
+							"expanded_url": "https://www.neonmuseum.org",
+							"display_url": "neonmuseum.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 15818,
+			"friends_count": 1194,
+			"listed_count": 329,
+			"created_at": "Fri May 29 18:47:29 +0000 2009",
+			"favourites_count": 17281,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 11690,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1090773864244625409/40LH1XDn_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1090773864244625409/40LH1XDn_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/43383876/1608677728",
+			"profile_link_color": "E81C4F",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 14,
+		"favorite_count": 108,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "63 years ago, a “star” was born- the Stardust to be exact!\nThe Stardust opened near the end of the era of atomic tourism. Sputnik orbited the Earth the year prior to the opening, heralding the dawn of the Space Age &amp; inspiring the theme of the casino resort. #NeonMuseumLasVegas https://t.co/qUXrZlI1FD"
+	}
+]
